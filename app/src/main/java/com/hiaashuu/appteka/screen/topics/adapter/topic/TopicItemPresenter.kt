823cfde6c2472243df3ac7713ad22b20eb6894ab
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:29:24 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/topics/adapter/topic/TopicItemPresenter.kt
@@ -0,0 +1,39 @@
+package com.hiaashuu.appteka.screen.topics.adapter.topic
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.topics.adapter.ItemListener
+
+class TopicItemPresenter(
+    private val listener: ItemListener
+) : ItemPresenter<TopicItemView, TopicItem> {
+
+    override fun bindView(view: TopicItemView, item: TopicItem, position: Int) {
+        with(item) {
+            if (hasMore) {
+                hasMore = false
+                hasProgress = true
+                listener.onLoadMore(this)
+            }
+        }
+
+        view.setIcon(item.icon)
+        view.setTitle(item.title)
+        view.setMessageText(item.lastMsgText)
+        item.lastMsgUserIcon?.let(view::setMessageAvatar)
+        view.setMessageAvatarBadge(item.lastMsgUserBadge)
+        if (item.isPinned) {
+            view.showPin()
+        } else {
+            view.hidePin()
+        }
+        if (item.hasUnread) {
+            view.showUnread()
+        } else {
+            view.hideUnread()
+        }
+        if (item.hasProgress) view.showProgress() else view.hideProgress()
+        view.setOnClickListener { listener.onItemClick(item) }
+        view.setOnLongClickListener { listener.onItemLongClick(item) }
+    }
+
+}
