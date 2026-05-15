bcd586e7d47552a534db3041f40b489495d06316
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:26:38 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/adapter/reviews/ReviewsItemView.kt
@@ -0,0 +1,71 @@
+package com.hiaashuu.appteka.screen.profile.adapter.reviews
+
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.SimpleRecyclerAdapter
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.bind
+
+interface ReviewsItemView : ItemView {
+
+    fun setRatingsCount(count: String)
+
+    fun setOnClickListener(listener: (() -> Unit)?)
+
+    fun notifyChanged()
+
+}
+
+class ReviewsItemViewHolder(
+    view: View,
+    private val adapter: SimpleRecyclerAdapter,
+) : BaseItemViewHolder(view), ReviewsItemView {
+
+    private val ratingsBlock: View = view.findViewById(R.id.ratings_block)
+    private val ratingsCountText: TextView = view.findViewById(R.id.ratings_count)
+    private val recycler: RecyclerView = view.findViewById(R.id.recycler)
+    private val ratingsButton: View = view.findViewById(R.id.show_ratings_button)
+
+    private val layoutManager: LinearLayoutManager
+
+    private var clickListener: (() -> Unit)? = null
+
+    init {
+        val orientation = RecyclerView.VERTICAL
+        layoutManager = LinearLayoutManager(view.context, orientation, false)
+        adapter.setHasStableIds(true)
+        recycler.adapter = adapter
+        recycler.layoutManager = layoutManager
+        recycler.itemAnimator = DefaultItemAnimator()
+        recycler.itemAnimator?.changeDuration = DURATION_MEDIUM
+
+        ratingsBlock.setOnClickListener { clickListener?.invoke() }
+        ratingsButton.setOnClickListener { clickListener?.invoke() }
+    }
+
+    override fun setRatingsCount(count: String) {
+        ratingsCountText.bind(count)
+    }
+
+    override fun setOnClickListener(listener: (() -> Unit)?) {
+        this.clickListener = listener
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun notifyChanged() {
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun onUnbind() {
+        this.clickListener = null
+    }
+
+}
+
+private const val DURATION_MEDIUM = 300L
