5112fba4cb4a01dd81beeaff7f47dfbc9d1efb9f
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:18:39 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/rating/RatingItemView.kt
@@ -0,0 +1,79 @@
+package com.hiaashuu.appteka.screen.details.adapter.rating
+
+import android.view.View
+import android.widget.RatingBar
+import android.widget.TextView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.dto.BadgeMark
+import com.hiaashuu.appteka.dto.UserIcon
+import com.hiaashuu.appteka.util.bind
+import com.hiaashuu.appteka.view.UserIconView
+import com.hiaashuu.appteka.view.UserIconViewImpl
+
+interface RatingItemView : ItemView {
+
+    fun setUserIcon(userIcon: UserIcon)
+
+    fun setUserBadge(badge: BadgeMark?)
+
+    fun setUserName(name: String)
+
+    fun setRating(value: Float)
+
+    fun setDate(date: String)
+
+    fun setComment(text: String?)
+
+    fun setOnClickListener(listener: (() -> Unit)?)
+
+}
+
+class RatingItemViewHolder(view: View) : BaseItemViewHolder(view), RatingItemView {
+
+    private val userIconView: UserIconView = UserIconViewImpl(view.findViewById(R.id.member_icon))
+    private val userNameView: TextView = view.findViewById(R.id.user_name)
+    private val ratingView: RatingBar = view.findViewById(R.id.rating_view)
+    private val dateView: TextView = view.findViewById(R.id.date_view)
+    private val commentView: TextView = view.findViewById(R.id.comment_view)
+
+    private var clickListener: (() -> Unit)? = null
+
+    init {
+        view.setOnClickListener { clickListener?.invoke() }
+    }
+
+    override fun setUserIcon(userIcon: UserIcon) {
+        userIconView.bind(userIcon)
+    }
+
+    override fun setUserBadge(badge: BadgeMark?) {
+        userIconView.bindBadge(badge)
+    }
+
+    override fun setUserName(name: String) {
+        userNameView.bind(name)
+    }
+
+    override fun setRating(value: Float) {
+        ratingView.rating = value
+    }
+
+    override fun setDate(date: String) {
+        dateView.bind(date)
+    }
+
+    override fun setComment(text: String?) {
+        commentView.bind(text)
+    }
+
+    override fun setOnClickListener(listener: (() -> Unit)?) {
+        this.clickListener = listener
+    }
+
+    override fun onUnbind() {
+        this.clickListener = null
+    }
+
+}
