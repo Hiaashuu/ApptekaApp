32f5b712c5913254d4993e3eecae33918cee45da
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:22:46 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/feed/FeedResourceProvider.kt
@@ -0,0 +1,46 @@
+package com.hiaashuu.appteka.screen.feed
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.core.TimeProvider
+import java.util.Locale
+
+interface FeedResourceProvider {
+
+    fun formatTime(value: Long): String
+
+    fun prepareMenuActions(actions: List<String>, handler: (Int) -> Unit): List<MenuAction>
+
+}
+
+class FeedResourceProviderImpl(
+    private val resources: Resources,
+    private val locale: Locale,
+    private val timeProvider: TimeProvider,
+) : FeedResourceProvider {
+
+    override fun formatTime(value: Long): String {
+        return timeProvider.formatTimeDiff(value)
+    }
+
+    override fun prepareMenuActions(
+        actions: List<String>,
+        handler: (Int) -> Unit
+    ): List<MenuAction> {
+        return actions.mapIndexedNotNull { index, action ->
+            when (action) {
+                "delete" -> MenuAction(
+                    id = index,
+                    title = resources.getString(R.string.delete),
+                    icon = R.drawable.ic_delete,
+                    action = { handler.invoke(MENU_DELETE) }
+                )
+
+                else -> null
+            }
+        }
+    }
+
+}
+
+const val MENU_DELETE = 1
