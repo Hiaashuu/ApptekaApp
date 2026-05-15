7fb5ec28defcab19ce5b04ab662bae76fbde562b
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:33:36 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/users/adapter/publisher/PublisherItemPresenter.kt
@@ -0,0 +1,35 @@
+package com.hiaashuu.appteka.screen.users.adapter.publisher
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.categories.DEFAULT_LOCALE
+import com.hiaashuu.appteka.screen.users.adapter.ItemListener
+import com.hiaashuu.appteka.screen.users.adapter.UsersResourceProvider
+import java.util.Locale
+
+class PublisherItemPresenter(
+    private val locale: Locale,
+    private val resourceProvider: UsersResourceProvider,
+    private val listener: ItemListener,
+) : ItemPresenter<PublisherItemView, PublisherItem> {
+
+    override fun bindView(view: PublisherItemView, item: PublisherItem, position: Int) {
+        with(item) {
+            if (hasMore) {
+                hasMore = false
+                hasProgress = true
+                listener.onLoadMore(this)
+            }
+        }
+
+        val name = item.user.name.takeIf { !it.isNullOrBlank() }
+            ?: item.user.icon.label?.get(locale.language)
+            ?: item.user.icon.label?.get(DEFAULT_LOCALE).orEmpty()
+        view.setUserName(name)
+        view.setUserIcon(item.user.icon)
+        view.setUserBadge(item.user.primaryBadge)
+        view.setSubscribedDate(resourceProvider.formatSubscribedDate(item.time))
+        if (item.hasProgress) view.showProgress() else view.hideProgress()
+        view.setOnClickListener { listener.onItemClick(item) }
+    }
+
+}
