4a6b7abfa0af382fec1acdb99da3a3b3cde97dd2
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:29:41 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/unlink/UnlinkActivity.kt
@@ -0,0 +1,77 @@
+package com.hiaashuu.appteka.screen.unlink
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hiaashuu.appteka.appComponent
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.screen.unlink.di.UnlinkModule
+import javax.inject.Inject
+
+class UnlinkActivity : AppCompatActivity(), UnlinkPresenter.UnlinkRouter {
+
+    @Inject
+    lateinit var presenter: UnlinkPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val appId = intent.getStringExtra(EXTRA_APP_ID)
+            ?: throw IllegalArgumentException("App ID must be provided")
+        val label = intent.getStringExtra(EXTRA_LABEL).orEmpty()
+
+        val presenterState = savedInstanceState?.getBundle(KEY_PRESENTER_STATE)
+        appComponent
+                .unlinkComponent(UnlinkModule(this, appId, presenterState))
+                .inject(activity = this)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.unlink_activity)
+
+        val view = UnlinkViewImpl(window.decorView, title = label)
+
+        presenter.attachView(view)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachRouter(this)
+    }
+
+    override fun onStop() {
+        presenter.detachRouter()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        presenter.detachView()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBundle(KEY_PRESENTER_STATE, presenter.saveState())
+    }
+
+    override fun leaveScreen(success: Boolean) {
+        val result = when(success) {
+            true -> RESULT_OK
+            else -> RESULT_CANCELED
+        }
+        setResult(result)
+        finish()
+    }
+
+}
+
+fun createUnlinkActivityIntent(
+    context: Context,
+    appId: String,
+    label: String?,
+): Intent = Intent(context, UnlinkActivity::class.java)
+    .putExtra(EXTRA_APP_ID, appId)
+    .putExtra(EXTRA_LABEL, label)
+
+private const val EXTRA_APP_ID = "app_id"
+private const val EXTRA_LABEL = "label"
+
+private const val KEY_PRESENTER_STATE = "presenter_state"
