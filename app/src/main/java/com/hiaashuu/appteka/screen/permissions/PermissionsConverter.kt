3788999333a8f3bc82951c48a8fa20c788e0f881
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:24:37 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/permissions/PermissionsConverter.kt
@@ -0,0 +1,36 @@
+package com.hiaashuu.appteka.screen.permissions
+
+import com.hiaashuu.appteka.util.adapter.Item
+import com.hiaashuu.appteka.screen.permissions.adapter.safe.SafePermissionItem
+import com.hiaashuu.appteka.screen.permissions.adapter.unsafe.UnsafePermissionItem
+
+interface PermissionsConverter {
+
+    fun convert(permission: String): Item
+
+}
+
+class PermissionsConverterImpl(
+    private val permissionInfoProvider: PermissionInfoProvider
+) : PermissionsConverter {
+
+    private var id: Long = 1
+
+    override fun convert(permission: String): Item {
+        val permissionBrief = permissionInfoProvider.getPermissionBrief(permission)
+        return when (permissionBrief.dangerous) {
+            true -> UnsafePermissionItem(
+                id = id++,
+                permission = permissionBrief.permission,
+                description = permissionBrief.description
+            )
+
+            false -> SafePermissionItem(
+                id = id++,
+                permission = permissionBrief.permission,
+                description = permissionBrief.description
+            )
+        }
+    }
+
+}
