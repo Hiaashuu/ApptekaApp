316d59beb06f899aec035cda16e5fff48bb15eb3
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:20:00 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/distro/DistroAppEntity.kt
@@ -0,0 +1,17 @@
+package com.hiaashuu.appteka.screen.distro
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class DistroAppEntity(
+    val packageName: String,
+    val label: String,
+    val icon: String?,
+    val verName: String,
+    val verCode: Long,
+    val lastModified: Long,
+    val size: Long,
+    val path: String?,
+    val fileName: String,
+) : Parcelable
