53c1875174876aafbf6288519bea003b9c6ad5a4
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:30:16 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/upload/adapter/category/SelectCategoryItemView.kt
@@ -0,0 +1,72 @@
+package com.hiaashuu.appteka.screen.upload.adapter.category
+
+import android.view.View
+import android.widget.AutoCompleteTextView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.google.android.material.textfield.TextInputLayout
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.categories.CategoryItem
+import com.hiaashuu.appteka.util.hide
+import com.hiaashuu.appteka.util.show
+import com.hiaashuu.appteka.util.svgToDrawable
+
+interface SelectCategoryItemView : ItemView {
+
+    fun setSelectedCategory(category: CategoryItem?)
+
+    fun showRequiredFieldError()
+
+    fun hideRequiredFieldError()
+
+    fun setOnClickListener(listener: (() -> Unit)?)
+
+}
+
+class SelectCategoryItemViewHolder(view: View) : BaseItemViewHolder(view), SelectCategoryItemView {
+
+    private val context = view.context
+    private val selectorLayout: TextInputLayout = view.findViewById(R.id.selector_back)
+    private val categoryDropdown: AutoCompleteTextView = view.findViewById(R.id.category_dropdown)
+    private val requiredFieldErrorView: View = view.findViewById(R.id.required_field_error)
+
+    private var clickListener: (() -> Unit)? = null
+
+    init {
+        categoryDropdown.setOnClickListener { clickListener?.invoke() }
+        categoryDropdown.isFocusable = false
+        categoryDropdown.isFocusableInTouchMode = false
+    }
+
+    override fun setSelectedCategory(category: CategoryItem?) {
+        category?.let {
+            val icon = svgToDrawable(it.icon, context.resources)
+            selectorLayout.startIconDrawable = icon
+            categoryDropdown.setText(it.title, false)
+        } ?: run {
+            selectorLayout.setStartIconDrawable(R.drawable.ic_category)
+            categoryDropdown.setText(context.getString(R.string.category_not_defined), false)
+        }
+    }
+
+    override fun showRequiredFieldError() {
+        requiredFieldErrorView.show()
+        selectorLayout.error = " "
+        selectorLayout.isErrorEnabled = true
+    }
+
+    override fun hideRequiredFieldError() {
+        requiredFieldErrorView.hide()
+        selectorLayout.error = null
+        selectorLayout.isErrorEnabled = false
+    }
+
+    override fun setOnClickListener(listener: (() -> Unit)?) {
+        this.clickListener = listener
+    }
+
+    override fun onUnbind() {
+        this.clickListener = null
+    }
+
+}
