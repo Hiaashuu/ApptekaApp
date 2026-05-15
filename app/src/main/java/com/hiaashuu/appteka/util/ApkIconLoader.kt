24b40d9a0206b8e26abcc9caca2822609f447f3d
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:34:45 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/ApkIconLoader.kt
@@ -0,0 +1,52 @@
+package com.hiaashuu.appteka.util
+
+import android.content.Context
+import android.content.pm.PackageManager
+import com.tomclaw.imageloader.core.Loader
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URI
+import java.net.URLDecoder
+import java.net.URLEncoder
+
+class ApkIconLoader(
+    private val context: Context,
+    private val packageManager: PackageManager
+) : Loader {
+
+    override val schemes: List<String>
+        get() = listOf("apk")
+
+    override fun load(uriString: String, file: File): Boolean {
+        try {
+            val path = parseUri(uriString)
+            val info = packageManager.getPackageArchiveInfoCompat(path, 0)?.apply {
+                applicationInfo?.let {
+                    it.sourceDir = path
+                    it.publicSourceDir = path
+                } ?: return false
+            } ?: return false
+
+            val data = PackageHelper.getPackageIconPng(info.applicationInfo, packageManager, context)
+            FileOutputStream(file).use { output ->
+                output.write(data)
+                output.flush()
+            }
+            return true
+        } catch (ignored: Throwable) {
+        }
+        return false
+    }
+
+    private fun parseUri(s: String): String {
+        val uri = URI.create(s)
+        val path = URLDecoder.decode(uri.path.substring(1), "UTF-8")
+        return path
+    }
+
+}
+
+fun createApkIconURI(path: String): String {
+    val encodedPath = URLEncoder.encode(path, "UTF-8")
+    return "apk://path/$encodedPath"
+}
