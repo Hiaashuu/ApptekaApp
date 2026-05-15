73d273185d8c904c806007164aaf36189a2115b0
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:57 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/moderation/adapter/app/AppItemPresenter.kt
@@ -0,0 +1,35 @@
+package com.hiaashuu.appteka.screen.moderation.adapter.app
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.moderation.adapter.ItemListener
+
+class AppItemPresenter(
+    private val listener: ItemListener
+) : ItemPresenter<AppItemView, AppItem> {
+
+    override fun bindView(view: AppItemView, item: AppItem, position: Int) {
+        with(item) {
+            if (hasMore) {
+                hasMore = false
+                hasProgress = true
+                hasError = false
+                listener.onLoadMore(this)
+            }
+        }
+
+        view.setIcon(item.icon)
+        view.setTitle(item.title)
+        view.setVersion(item.version)
+        view.setSize(item.size)
+        view.setRating(item.rating.takeIf { it > 0 })
+        view.setDownloads(item.downloads)
+        view.setCategory(item.category)
+        if (item.openSource) view.showOpenSourceBadge() else view.hideOpenSourceBadge()
+        if (!item.isAbiCompatible) view.showAbiIncompatibleBadge() else view.hideAbiIncompatibleBadge()
+        if (item.hasProgress) view.showProgress() else view.hideProgress()
+        if (item.hasError) view.showError() else view.hideError()
+        view.setOnClickListener { listener.onItemClick(item) }
+        view.setOnRetryListener { listener.onRetryClick(item) }
+    }
+
+}
