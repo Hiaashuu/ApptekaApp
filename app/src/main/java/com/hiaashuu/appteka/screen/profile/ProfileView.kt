fd7f3efa3f7745577914269cba402b4867712fd8
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:25:41 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/ProfileView.kt
@@ -0,0 +1,227 @@
+package com.hiaashuu.appteka.screen.profile
+
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.hiaashuu.appteka.util.adapter.SimpleRecyclerAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.jakewharton.rxrelay3.PublishRelay
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.dto.Badge
+import com.hiaashuu.appteka.uikit.badges.BadgeBottomSheet
+import com.hiaashuu.appteka.util.hide
+import com.hiaashuu.appteka.util.hideWithAlphaAnimation
+import com.hiaashuu.appteka.util.show
+import com.hiaashuu.appteka.util.showWithAlphaAnimation
+import io.reactivex.rxjava3.core.Observable
+
+interface ProfileView {
+
+    fun showProgress()
+
+    fun showContent()
+
+    fun showMenu(canEliminate: Boolean)
+
+    fun hideMenu()
+
+    fun showToolbar()
+
+    fun hideToolbar()
+
+    fun showError()
+
+    fun hideError()
+
+    fun showEliminationDialog()
+
+    fun showEliminationDone(filesCount: Int, messagesCount: Int, ratingsCount: Int)
+
+    fun showEliminationFailed()
+
+    fun contentUpdated()
+
+    fun contentUpdated(position: Int)
+
+    fun showSubscriptionError()
+
+    fun showUnauthorizedError()
+
+    fun showBadgeDetails(badge: Badge)
+
+    fun navigationClicks(): Observable<Unit>
+
+    fun swipeRefresh(): Observable<Unit>
+
+    fun shareClicks(): Observable<Unit>
+
+    fun eliminateClicks(): Observable<Boolean>
+
+    fun retryClicks(): Observable<Unit>
+
+    fun loginClicks(): Observable<Unit>
+
+}
+
+class ProfileViewImpl(
+    view: View,
+    private val adapter: SimpleRecyclerAdapter
+) : ProfileView {
+
+    private val context = view.context
+    private val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+    private val swipeRefresh: SwipeRefreshLayout = view.findViewById(R.id.swipe_refresh)
+    private val recycler: RecyclerView = view.findViewById(R.id.recycler)
+    private val error: View = view.findViewById(R.id.error)
+    private val blockingProgress: View = view.findViewById(R.id.blocking_progress)
+    private val retryButton: View = view.findViewById(R.id.retry_button)
+
+    private val navigationRelay = PublishRelay.create<Unit>()
+    private val refreshRelay = PublishRelay.create<Unit>()
+    private val shareRelay = PublishRelay.create<Unit>()
+    private val eliminateRelay = PublishRelay.create<Boolean>()
+    private val retryRelay = PublishRelay.create<Unit>()
+    private val loginRelay = PublishRelay.create<Unit>()
+
+    private val layoutManager: LinearLayoutManager
+
+    init {
+        toolbar.setNavigationOnClickListener { navigationRelay.accept(Unit) }
+        toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.share -> shareRelay.accept(Unit)
+                R.id.eliminate -> eliminateRelay.accept(false)
+            }
+            true
+        }
+
+        swipeRefresh.setOnRefreshListener {
+            refreshRelay.accept(Unit)
+        }
+
+        retryButton.setOnClickListener { retryRelay.accept(Unit) }
+
+        val orientation = RecyclerView.VERTICAL
+        layoutManager = LinearLayoutManager(view.context, orientation, false)
+        adapter.setHasStableIds(true)
+        recycler.adapter = adapter
+        recycler.layoutManager = layoutManager
+        recycler.itemAnimator = DefaultItemAnimator()
+        recycler.itemAnimator?.changeDuration = DURATION_MEDIUM
+    }
+
+    override fun showProgress() {
+        blockingProgress.showWithAlphaAnimation(animateFully = true)
+    }
+
+    override fun showContent() {
+        blockingProgress.hideWithAlphaAnimation(animateFully = false)
+        swipeRefresh.isRefreshing = false
+    }
+
+    override fun showMenu(
+        canEliminate: Boolean,
+    ) {
+        toolbar.menu.clear()
+        toolbar.inflateMenu(R.menu.profile_menu)
+        if (!canEliminate) {
+            toolbar.menu.removeItem(R.id.eliminate)
+        }
+        toolbar.invalidateMenu()
+    }
+
+    override fun hideMenu() {
+        toolbar.menu.clear()
+        toolbar.invalidateMenu()
+    }
+
+    override fun showToolbar() {
+        toolbar.show()
+    }
+
+    override fun hideToolbar() {
+        toolbar.hide()
+    }
+
+    override fun showError() {
+        error.show()
+    }
+
+    override fun hideError() {
+        error.hide()
+    }
+
+    override fun showEliminationDialog() {
+        MaterialAlertDialogBuilder(context)
+            .setTitle(context.getString(R.string.eliminate_user_title))
+            .setMessage(context.getString(R.string.eliminate_user_message))
+            .setNegativeButton(R.string.yes) { _, _ ->
+                eliminateRelay.accept(true)
+            }
+            .setPositiveButton(R.string.no, null)
+            .show()
+    }
+
+    override fun showEliminationDone(filesCount: Int, messagesCount: Int, ratingsCount: Int) {
+        val message: String = context.getString(
+            R.string.eliminate_user_success,
+            filesCount,
+            messagesCount,
+            ratingsCount
+        )
+        Snackbar.make(recycler, message, Snackbar.LENGTH_LONG).show()
+    }
+
+    override fun showEliminationFailed() {
+        Toast.makeText(context, R.string.eliminate_user_failed, Toast.LENGTH_LONG).show()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun contentUpdated() {
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun contentUpdated(position: Int) {
+        adapter.notifyItemChanged(position)
+    }
+
+    override fun showSubscriptionError() {
+        Snackbar
+            .make(recycler, R.string.unable_to_change_subscription_state, Snackbar.LENGTH_LONG)
+            .show()
+    }
+
+    override fun showBadgeDetails(badge: Badge) {
+        BadgeBottomSheet.show(context, badge)
+    }
+
+    override fun showUnauthorizedError() {
+        Snackbar
+            .make(recycler, R.string.authorization_required_message, Snackbar.LENGTH_INDEFINITE)
+            .setAction(R.string.login_button) {
+                loginRelay.accept(Unit)
+            }
+            .show()
+    }
+
+    override fun navigationClicks(): Observable<Unit> = navigationRelay
+
+    override fun swipeRefresh(): Observable<Unit> = refreshRelay
+
+    override fun shareClicks(): Observable<Unit> = shareRelay
+
+    override fun eliminateClicks(): Observable<Boolean> = eliminateRelay
+
+    override fun retryClicks(): Observable<Unit> = retryRelay
+
+    override fun loginClicks(): Observable<Unit> = loginRelay
+
+}
+
+private const val DURATION_MEDIUM = 300L
