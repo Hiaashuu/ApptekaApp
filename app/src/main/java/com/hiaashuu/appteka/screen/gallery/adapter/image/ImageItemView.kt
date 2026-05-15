7bfa3d23d96e0cc5668c77407496d320daae8e1b
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:01 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/gallery/adapter/image/ImageItemView.kt
@@ -0,0 +1,45 @@
+package com.hiaashuu.appteka.screen.gallery.adapter.image
+
+import android.net.Uri
+import android.view.View
+import android.widget.ImageView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.view.ZoomImageView
+import com.tomclaw.imageloader.util.fetch
+
+interface ImageItemView : ItemView {
+
+    fun setUri(uri: Uri)
+
+}
+
+class ImageItemViewHolder(view: View) : BaseItemViewHolder(view), ImageItemView {
+
+    private val image: ZoomImageView = view.findViewById(R.id.gallery_image)
+
+    init {
+        image.disallowPagingWhenZoomed = true
+    }
+
+    override fun setUri(uri: Uri) {
+        image.setImageResource(R.drawable.ic_cloud)
+        image.scaleType = ImageView.ScaleType.CENTER_INSIDE
+        image.fetch(uri) {
+            onLoading { imageView ->
+                imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+                imageView.setImageResource(R.drawable.ic_cloud)
+            }
+            onSuccess { imageView, drawable ->
+                imageView.scaleType = ImageView.ScaleType.MATRIX
+                imageView.setImageDrawable(drawable)
+            }
+            onError { imageView, _ ->
+                imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+                imageView.setImageResource(R.drawable.ic_error)
+            }
+        }
+    }
+
+}
