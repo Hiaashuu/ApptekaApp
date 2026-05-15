cb63707498efbb5ee9b4275c1e2a882c95f1625b
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:25:50 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/adapter/app/AppItemView.kt
@@ -0,0 +1,67 @@
+package com.hiaashuu.appteka.screen.profile.adapter.app
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.bind
+import com.hiaashuu.appteka.util.hide
+import com.hiaashuu.appteka.util.show
+import com.tomclaw.imageloader.util.fetch
+
+interface AppItemView : ItemView {
+
+    fun setIcon(url: String?)
+
+    fun setTitle(title: String)
+
+    fun setRating(rating: Float?)
+
+    fun setOnClickListener(listener: (() -> Unit)?)
+
+}
+
+class AppItemViewHolder(view: View) : BaseItemViewHolder(view), AppItemView {
+
+    private val icon: ImageView = view.findViewById(R.id.app_icon)
+    private val title: TextView = view.findViewById(R.id.app_name)
+    private val rating: TextView = view.findViewById(R.id.app_rating)
+    private val ratingIcon: View = view.findViewById(R.id.rating_icon)
+
+    private var clickListener: (() -> Unit)? = null
+
+    init {
+        view.setOnClickListener { clickListener?.invoke() }
+    }
+
+    override fun setIcon(url: String?) {
+        icon.fetch(url.orEmpty()) {
+            centerCrop()
+            placeholder(R.drawable.app_placeholder)
+            onLoading { imageView ->
+                imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                imageView.setImageResource(R.drawable.app_placeholder)
+            }
+        }
+    }
+
+    override fun setTitle(title: String) {
+        this.title.bind(title)
+    }
+
+    override fun setRating(rating: Float?) {
+        this.rating.bind(rating?.toString())
+        rating?.let { this.ratingIcon.show() } ?: this.ratingIcon.hide()
+    }
+
+    override fun setOnClickListener(listener: (() -> Unit)?) {
+        this.clickListener = listener
+    }
+
+    override fun onUnbind() {
+        this.clickListener = null
+    }
+
+}
