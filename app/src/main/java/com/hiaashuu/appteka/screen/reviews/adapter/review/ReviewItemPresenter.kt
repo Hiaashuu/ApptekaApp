ec518444fb3d96b659edd3585fdd92f8906c260b
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:28:03 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/reviews/adapter/review/ReviewItemPresenter.kt
@@ -0,0 +1,39 @@
+package com.hiaashuu.appteka.screen.reviews.adapter.review
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.reviews.adapter.ItemListener
+import java.text.DateFormat
+
+class ReviewItemPresenter(
+    private val dateFormatter: DateFormat,
+    private val listener: ItemListener,
+) : ItemPresenter<ReviewItemView, ReviewItem> {
+
+    override fun bindView(view: ReviewItemView, item: ReviewItem, position: Int) {
+        with(item) {
+            if (hasMore) {
+                hasMore = false
+                hasProgress = true
+                hasError = false
+                listener.onLoadMore(this)
+            }
+        }
+
+        view.setIcon(item.icon)
+        view.setTitle(item.title)
+        view.setVersion(item.version)
+        view.setRating(item.rating)
+        val date: String = dateFormatter.format(item.time)
+        view.setDate(date)
+        view.setReview(item.text)
+        if (item.hasProgress) view.showProgress() else view.hideProgress()
+        if (item.showRatingMenu) {
+            view.showRatingMenu()
+        } else {
+            view.hideRatingMenu()
+        }
+        view.setOnReviewClickListener { listener.onItemClick(item) }
+        view.setOnDeleteClickListener { listener.onDeleteClick(item) }
+    }
+
+}
