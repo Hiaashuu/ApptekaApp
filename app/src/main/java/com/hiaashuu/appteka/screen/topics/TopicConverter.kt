8c53b99e22b1164244feb5912cd7ed36383a6610
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:29:16 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/topics/TopicConverter.kt
@@ -0,0 +1,60 @@
+package com.hiaashuu.appteka.screen.topics
+
+import com.hiaashuu.appteka.dto.MessageType
+import com.hiaashuu.appteka.dto.TopicEntity
+import com.hiaashuu.appteka.screen.topics.adapter.topic.TopicItem
+
+interface TopicConverter {
+
+    fun convert(entity: TopicEntity, translated: Boolean): TopicItem
+
+}
+
+class TopicConverterImpl(
+    private val resourceProvider: TopicsResourceProvider
+) : TopicConverter {
+
+    override fun convert(entity: TopicEntity, translated: Boolean): TopicItem {
+        val icon = when (entity.topicId) {
+            1 -> COMMON_QNA_TOPIC_ICON
+            else -> entity.icon.orEmpty()
+        }
+        val title = when (entity.topicId) {
+            1 -> resourceProvider.commonQuestionsTopicTitle()
+            else -> entity.title
+        }
+        val description = when (entity.topicId) {
+            1 -> resourceProvider.commonQuestionsTopicDescription()
+            else -> entity.description
+        }
+        entity.lastMsg ?: throw IllegalStateException("lastMsg must be specified")
+        val translation = entity.lastMsg.translation?.takeIf { it.isNotBlank() }
+        val showTranslation = translated && translation != null
+        val attachmentsCount = entity.lastMsg.attachments?.size ?: 0
+        val text = when {
+            showTranslation -> translation
+            entity.lastMsg.type == MessageType.TOPIC_CREATED -> resourceProvider.chatCreatedMessage()
+            entity.lastMsg.text.isNotBlank() -> entity.lastMsg.text
+            attachmentsCount > 0 -> resourceProvider.attachmentsPlaceholder(attachmentsCount)
+            else -> entity.lastMsg.text
+        }
+        return TopicItem(
+            id = entity.topicId.toLong(),
+            icon = icon,
+            title = title,
+            description = description,
+            packageName = entity.packageName,
+            isPinned = entity.isPinned,
+            hasUnread = entity.readMsgId?.let { it < entity.lastMsg.msgId } == true,
+            lastMsgId = entity.lastMsg.msgId,
+            lastMsgText = text,
+            lastMsgUserIcon = entity.lastMsg.author.icon,
+            lastMsgUserBadge = entity.lastMsg.author.primaryBadge,
+            hasTranslation = translation != null,
+            translated = showTranslation,
+        )
+    }
+
+}
+
+const val COMMON_QNA_TOPIC_ICON = "file:///android_asset/topic_common_qna.png"
