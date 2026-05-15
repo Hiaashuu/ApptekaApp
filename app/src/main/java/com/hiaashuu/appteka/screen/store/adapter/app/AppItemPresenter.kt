1ed6737182c7567043016c19b77d7b5d2ccc221f
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:28:45 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/store/adapter/app/AppItemPresenter.kt
@@ -0,0 +1,68 @@
+package com.hiaashuu.appteka.screen.store.adapter.app
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.core.FileStatus
+import com.hiaashuu.appteka.screen.store.AppsResourceProvider
+import com.hiaashuu.appteka.screen.store.adapter.ItemListener
+
+class AppItemPresenter(
+    private val listener: ItemListener,
+    private val resourceProvider: AppsResourceProvider
+) : ItemPresenter<AppItemView, AppItem> {
+
+    override fun bindView(view: AppItemView, item: AppItem, position: Int) {
+        with(item) {
+            if (hasMore) {
+                hasMore = false
+                hasProgress = true
+                listener.onLoadMore(this)
+            }
+        }
+
+        view.setIcon(item.icon)
+        view.setTitle(item.title)
+        view.setVersion(item.version)
+        view.setSize(item.size)
+        view.setRating(item.rating.takeIf { it > 0 })
+        view.setDownloads(item.downloads)
+        var statusText = ""
+        var isPublished = false
+        if (item.isInstalled && item.isUpdatable) {
+            statusText = resourceProvider.getStatusUpdatableString()
+            isPublished = true
+        } else if (item.isInstalled) {
+            statusText = resourceProvider.getStatusInstalledString()
+            isPublished = true
+        }
+        var clickable = true
+        when (item.status) {
+            FileStatus.UNLINKED -> {
+                statusText = resourceProvider.getStatusBlockedString()
+                isPublished = false
+                clickable = false
+            }
+
+            FileStatus.PRIVATE -> {
+                statusText = resourceProvider.getStatusPrivateString()
+                isPublished = false
+            }
+            FileStatus.MODERATION -> {
+                statusText = resourceProvider.getStatusModerationString()
+                isPublished = false
+            }
+        }
+        view.setStatus(statusText, isPublished)
+        if (item.isNew) view.showBadge() else view.hideBadge()
+        if (item.hasProgress) view.showProgress() else view.hideProgress()
+        view.setCategory(item.category)
+        if (item.openSource) view.showOpenSourceBadge() else view.hideOpenSourceBadge()
+        if (!item.isAbiCompatible) view.showAbiIncompatibleBadge() else view.hideAbiIncompatibleBadge()
+        view.setClickable(clickable)
+        if (clickable) {
+            view.setOnClickListener { listener.onItemClick(item) }
+        } else {
+            view.setOnClickListener(null)
+        }
+    }
+
+}
