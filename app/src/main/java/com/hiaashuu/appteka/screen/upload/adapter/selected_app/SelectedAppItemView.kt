fb5c3b49ecee05b74546bfd8739b1ca99d9a44dc
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:31:35 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/upload/adapter/selected_app/SelectedAppItemView.kt
@@ -0,0 +1,87 @@
+package com.hiaashuu.appteka.screen.upload.adapter.selected_app
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.bind
+import com.tomclaw.imageloader.util.fetch
+
+interface SelectedAppItemView : ItemView {
+
+    fun setAppIcon(url: String?)
+
+    fun setAppLabel(label: String?)
+
+    fun setAppPackage(packageName: String?)
+
+    fun setAppVersion(version: String?)
+
+    fun setAppSize(size: String?)
+
+    fun setOnClickListener(listener: (() -> Unit)?)
+
+    fun setOnDiscardListener(listener: (() -> Unit)?)
+
+}
+
+class SelectedAppItemViewHolder(private val cardView: View) : BaseItemViewHolder(cardView), SelectedAppItemView {
+
+    private val appIcon: ImageView = cardView.findViewById(R.id.app_icon)
+    private val appLabel: TextView = cardView.findViewById(R.id.app_label)
+    private val appPackage: TextView = cardView.findViewById(R.id.app_package)
+    private val appVersion: TextView = cardView.findViewById(R.id.app_version)
+    private val appSize: TextView = cardView.findViewById(R.id.app_size)
+    private val discardButton: View = cardView.findViewById(R.id.discard_button)
+
+    private var clickListener: (() -> Unit)? = null
+    private var discardListener: (() -> Unit)? = null
+
+    init {
+        cardView.setOnClickListener { clickListener?.invoke() }
+        discardButton.setOnClickListener { discardListener?.invoke() }
+    }
+
+    override fun setAppIcon(url: String?) {
+        appIcon.fetch(url.orEmpty()) {
+            centerCrop()
+            placeholder(R.drawable.app_placeholder)
+            onLoading { imageView ->
+                imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                imageView.setImageResource(R.drawable.app_placeholder)
+            }
+        }
+    }
+
+    override fun setAppLabel(label: String?) {
+        this.appLabel.bind(label)
+    }
+
+    override fun setAppPackage(packageName: String?) {
+        this.appPackage.bind(packageName)
+    }
+
+    override fun setAppVersion(version: String?) {
+        this.appVersion.bind(version)
+    }
+
+    override fun setAppSize(size: String?) {
+        this.appSize.bind(size)
+    }
+
+    override fun setOnClickListener(listener: (() -> Unit)?) {
+        this.clickListener = listener
+    }
+
+    override fun setOnDiscardListener(listener: (() -> Unit)?) {
+        this.discardListener = listener
+    }
+
+    override fun onUnbind() {
+        this.clickListener = null
+        this.discardListener = null
+    }
+
+}
