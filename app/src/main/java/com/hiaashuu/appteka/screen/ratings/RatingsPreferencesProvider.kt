28a7367b5b4d5655d5e42d116b92294edc960c85
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:27:28 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/ratings/RatingsPreferencesProvider.kt
@@ -0,0 +1,27 @@
+package com.hiaashuu.appteka.screen.ratings
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+
+interface RatingsPreferencesProvider {
+
+    fun isDarkTheme(): Boolean
+
+}
+
+class RatingsPreferencesProviderImpl(
+    context: Context
+) : RatingsPreferencesProvider {
+
+    private val preferences: SharedPreferences = context.getSharedPreferences(
+        context.packageName + "_preferences", MODE_PRIVATE
+    )
+
+    override fun isDarkTheme(): Boolean {
+        return preferences.getBoolean(DARK_THEME_PREF_KEY, false)
+    }
+
+}
+
+const val DARK_THEME_PREF_KEY = "pref_dark_theme"
