c019280371e00e0e9ee3173dc640e88237ae2046
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:32 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/api/AppVersion.kt
@@ -0,0 +1,21 @@
+package com.hiaashuu.appteka.screen.details.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@GsonModel
+@Parcelize
+data class AppVersion(
+    @SerializedName("app_id")
+    val appId: String,
+    @SerializedName("downloads")
+    val downloads: Int?,
+    @SerializedName("ver_code")
+    val verCode: Int,
+    @SerializedName("ver_name")
+    val verName: String,
+    @SerializedName("sdk_version")
+    val sdkVersion: Int,
+) : Parcelable
