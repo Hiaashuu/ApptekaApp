a5d27c7c970d7d7a0466614401603420a70cbb53
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:25:04 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/post/adapter/image/ImageItemView.kt
@@ -0,0 +1,68 @@
+package com.hiaashuu.appteka.screen.post.adapter.image
+
+import android.view.View
+import android.widget.ImageView
+import androidx.core.view.isVisible
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.tomclaw.imageloader.util.fetch
+
+interface ImageItemView : ItemView {
+
+    fun setImage(item: ImageItem)
+
+    fun setRemote(remote: Boolean)
+
+    fun setOnClickListener(listener: (() -> Unit)?)
+
+    fun setOnDeleteListener(listener: (() -> Unit)?)
+
+}
+
+class ImageItemViewHolder(view: View) : BaseItemViewHolder(view), ImageItemView {
+
+    private val card: View = view.findViewById(R.id.image_card)
+    private val image: ImageView = view.findViewById(R.id.image)
+    private val upload: View = view.findViewById(R.id.upload)
+    private val deleteButton: View = view.findViewById(R.id.delete_button)
+
+    private var clickListener: (() -> Unit)? = null
+    private var deleteListener: (() -> Unit)? = null
+
+    init {
+        card.setOnClickListener { clickListener?.invoke() }
+        deleteButton.setOnClickListener { deleteListener?.invoke() }
+    }
+
+    override fun setImage(item: ImageItem) {
+        val aspectRatio = item.width.toFloat() / item.height.toFloat()
+        val width = image.layoutParams.height * aspectRatio
+        image.layoutParams.width = width.toInt()
+
+        image.fetch(item.preview.toString()) {
+            centerCrop()
+            onLoading { imageView ->
+                imageView.setImageDrawable(null)
+            }
+        }
+    }
+
+    override fun setRemote(remote: Boolean) {
+        upload.isVisible = !remote
+    }
+
+    override fun setOnClickListener(listener: (() -> Unit)?) {
+        this.clickListener = listener
+    }
+
+    override fun setOnDeleteListener(listener: (() -> Unit)?) {
+        this.deleteListener = listener
+    }
+
+    override fun onUnbind() {
+        this.clickListener = null
+        this.deleteListener = null
+    }
+
+}
