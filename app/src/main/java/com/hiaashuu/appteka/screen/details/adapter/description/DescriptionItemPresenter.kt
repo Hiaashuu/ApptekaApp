5c3a560ff9f36d361a5e2a5a25de122e83084df1
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:18:08 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/description/DescriptionItemPresenter.kt
@@ -0,0 +1,40 @@
+package com.hiaashuu.appteka.screen.details.adapter.description
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.details.TRANSLATION_ORIGINAL
+import com.hiaashuu.appteka.screen.details.TRANSLATION_PROGRESS
+import com.hiaashuu.appteka.screen.details.TRANSLATION_TRANSLATED
+import com.hiaashuu.appteka.screen.details.adapter.ItemListener
+
+class DescriptionItemPresenter(
+    private val listener: ItemListener,
+    private val resourceProvider: DescriptionResourceProvider
+) : ItemPresenter<DescriptionItemView, DescriptionItem> {
+
+    override fun bindView(view: DescriptionItemView, item: DescriptionItem, position: Int) {
+        view.setText(item.text)
+        view.setAppVersion(resourceProvider.formatFileVersion(item.versionName, item.versionCode))
+        view.setVersionsCount(item.versionsCount)
+        view.setUploadDate(resourceProvider.formatDate(item.uploadDate))
+        view.setChecksum(item.checksum)
+        view.setSourceUrl(item.sourceUrl)
+        when (item.translationState) {
+            TRANSLATION_ORIGINAL -> {
+                view.enableTranslateButton()
+                view.showTranslateButton()
+            }
+
+            TRANSLATION_TRANSLATED -> {
+                view.enableTranslateButton()
+                view.showOriginalButton()
+            }
+
+            TRANSLATION_PROGRESS -> view.disableTranslateButton()
+        }
+
+        view.setOnTranslateClickListener { listener.onTranslateClick() }
+        view.setOnGooglePlayClickListener { listener.onGooglePlayClick() }
+        view.setOnVersionsClickListener { listener.onVersionsClick() }
+    }
+
+}
