5d5263f94f5894fcb28fa8fdb23f424707868460
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:14:45 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/categories/CategoryItem.kt
@@ -0,0 +1,17 @@
+package com.hiaashuu.appteka.categories
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@GsonModel
+data class CategoryItem(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("icon")
+    val icon: String,
+) : Parcelable
