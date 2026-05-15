48bad41d1b1b159c5397cb58ffab3aa0c000e62e
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:34:33 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/upload/UploadState.kt
@@ -0,0 +1,16 @@
+package com.hiaashuu.appteka.upload
+
+data class UploadState(
+    val status: UploadStatus,
+    val percent: Int = 0,
+    val result: UploadResponse? = null,
+)
+
+enum class UploadStatus {
+    IDLE,
+    AWAIT,
+    STARTED,
+    PROGRESS,
+    COMPLETED,
+    ERROR,
+}
