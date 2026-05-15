a05ea4bc9de6e260a7d816fd0cf6a7b8cee6c10a
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:30:22 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/upload/adapter/description/DescriptionItemView.kt
@@ -0,0 +1,72 @@
+package com.hiaashuu.appteka.screen.upload.adapter.description
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+
+interface DescriptionItemView : ItemView {
+
+    fun setText(text: String)
+
+    fun showRequiredFieldError()
+
+    fun showMinLengthError(minLength: Int)
+
+    fun hideError()
+
+    fun setOnTextChangedListener(listener: ((String) -> Unit)?)
+
+}
+
+class DescriptionItemViewHolder(view: View) : BaseItemViewHolder(view), DescriptionItemView {
+
+    private val resources = view.resources
+    private val descriptionEdit: EditText = view.findViewById(R.id.description)
+
+    private var textChangedListener: ((String) -> Unit)? = null
+
+    init {
+        descriptionEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                textChangedListener?.invoke(s.toString())
+            }
+        })
+    }
+
+    override fun setText(text: String) {
+        if (descriptionEdit.text.toString() != text) {
+            descriptionEdit.setText(text)
+        }
+    }
+
+    override fun showRequiredFieldError() {
+        descriptionEdit.error = resources.getString(R.string.required_field)
+    }
+
+    override fun showMinLengthError(minLength: Int) {
+        descriptionEdit.error = resources.getString(R.string.description_min_length_error, minLength)
+    }
+
+    override fun hideError() {
+        descriptionEdit.error = null
+    }
+
+    override fun setOnTextChangedListener(listener: ((String) -> Unit)?) {
+        this.textChangedListener = listener
+    }
+
+    override fun onUnbind() {
+        this.textChangedListener = null
+    }
+
+}
