37c618d6871581db832833b60c04ff99d67f28c3
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:23 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/installed/AppConverter.kt
@@ -0,0 +1,35 @@
+package com.hiaashuu.appteka.screen.installed
+
+import com.hiaashuu.appteka.screen.installed.adapter.app.AppItem
+import com.hiaashuu.appteka.screen.installed.api.UpdateEntity
+import java.util.concurrent.TimeUnit
+
+interface AppConverter {
+
+    fun convert(appEntity: InstalledAppEntity, update: UpdateEntity?): AppItem
+
+}
+
+class AppConverterImpl : AppConverter {
+
+    private var id: Long = 1
+
+    override fun convert(appEntity: InstalledAppEntity, update: UpdateEntity?): AppItem {
+        return AppItem(
+            id = id++,
+            icon = appEntity.icon,
+            title = appEntity.label,
+            version = appEntity.verName,
+            size = appEntity.size,
+            installTime = appEntity.firstInstallTime,
+            updateTime = appEntity.lastUpdateTime,
+            path = appEntity.path,
+            packageName = appEntity.packageName,
+            updateAppId = update?.appId,
+            isUserApp = appEntity.isUserApp,
+            isNew = (TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) - appEntity.firstInstallTime) <
+                    TimeUnit.DAYS.toSeconds(1),
+        )
+    }
+
+}
