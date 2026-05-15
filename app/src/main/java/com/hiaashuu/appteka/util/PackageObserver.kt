86242721ad38f0254db83f563ffbdb4640dfb6ab
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:36:22 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/PackageObserver.kt
@@ -0,0 +1,88 @@
+package com.hiaashuu.appteka.util
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
+import com.jakewharton.rxrelay3.BehaviorRelay
+import io.reactivex.rxjava3.core.Observable
+
+interface PackageObserver {
+
+    fun observe(packageName: String): Observable<Int>
+
+    fun pickInstalledVersionCode(packageName: String): Int
+
+}
+
+class PackageObserverImpl(
+    context: Context,
+    private val packageManager: PackageManager
+) : PackageObserver {
+
+    private val packages = HashMap<String, BehaviorRelay<Int>>()
+
+    init {
+        val filter = IntentFilter()
+        filter.addAction("android.intent.action.PACKAGE_ADDED")
+        filter.addAction("android.intent.action.PACKAGE_REMOVED")
+        filter.addAction("android.intent.action.PACKAGE_INSTALL")
+        filter.addAction("android.intent.action.PACKAGE_CHANGED")
+        filter.addAction("android.intent.action.PACKAGE_REPLACED")
+        filter.addDataScheme("package")
+        filter.addCategory("android.intent.category.DEFAULT")
+
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                println("[packages] Intent: " + intent.action + "/" + intent.dataString)
+                val packageName = intent.dataString?.replace("package:", "")
+                if (!packageName.isNullOrEmpty()) {
+                    packages[packageName]?.let { relay ->
+                        val versionCode = pickInstalledVersionCode(packageName)
+                        relay.accept(versionCode)
+                    }
+                }
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            context.registerReceiver(receiver, filter, Context.RECEIVER_NOT_EXPORTED)
+        } else {
+            @Suppress("DEPRECATION")
+            context.registerReceiver(receiver, filter)
+        }
+        println("[packages] Package observing started")
+    }
+
+    override fun observe(packageName: String): Observable<Int> {
+        val observable = packages[packageName] ?: let {
+            val versionCode = pickInstalledVersionCode(packageName)
+            val observable = BehaviorRelay.createDefault(versionCode)
+            packages[packageName] = observable
+            observable
+        }
+        return observable
+            .doOnSubscribe {
+                println("[packages] Package $packageName observer subscribed")
+            }
+            .doFinally {
+                println("[packages] Package $packageName observer disposed")
+                if (!observable.hasObservers()) {
+                    println("[packages] Package $packageName observer removed")
+                    packages.remove(packageName)
+                }
+            }
+    }
+
+    override fun pickInstalledVersionCode(packageName: String): Int {
+        return try {
+            packageManager.getPackageInfoCompat(packageName, 0).versionCodeCompat().toInt()
+        } catch (ex: Throwable) {
+            NOT_INSTALLED
+        }
+    }
+
+}
+
+const val NOT_INSTALLED = -1
