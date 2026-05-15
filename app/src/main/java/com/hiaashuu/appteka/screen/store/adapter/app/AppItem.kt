0f40b73986ec7b737c5abd0484a13f476ded3c49
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:28:42 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/store/adapter/app/AppItem.kt
@@ -0,0 +1,28 @@
+package com.hiaashuu.appteka.screen.store.adapter.app
+
+import android.os.Parcelable
+import com.hiaashuu.appteka.util.adapter.Item
+import com.hiaashuu.appteka.categories.CategoryItem
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class AppItem(
+    override val id: Long,
+    val appId: String,
+    val icon: String?,
+    val title: String,
+    val version: String,
+    val size: String,
+    val rating: Float,
+    val downloads: Int,
+    val status: Int,
+    val category: CategoryItem?,
+    val exclusive: Boolean,
+    val openSource: Boolean,
+    val isAbiCompatible: Boolean,
+    var isInstalled: Boolean = false,
+    var isUpdatable: Boolean = false,
+    var isNew: Boolean = false,
+    var hasMore: Boolean = false,
+    var hasProgress: Boolean = false,
+) : Item, Parcelable
