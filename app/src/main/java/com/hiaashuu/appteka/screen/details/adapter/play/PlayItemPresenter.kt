154c1f184d8dec0d4be5ca6ad6724a2e35b872c3
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:18:32 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/play/PlayItemPresenter.kt
@@ -0,0 +1,85 @@
+package com.hiaashuu.appteka.screen.details.adapter.play
+
+import android.os.Build
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.categories.DEFAULT_LOCALE
+import com.hiaashuu.appteka.screen.details.adapter.ItemListener
+import java.util.Locale
+
+class PlayItemPresenter(
+    private val locale: Locale,
+    private val resourceProvider: PlayResourceProvider,
+    private val listener: ItemListener,
+) : ItemPresenter<PlayItemView, PlayItem> {
+
+    override fun bindView(view: PlayItemView, item: PlayItem, position: Int) {
+        item.rating?.takeIf { it > 0 }?.let { view.showRating(it.toString()) } ?: view.hideRating()
+
+        view.setDownloads(item.downloads)
+
+        if (item.favorites > 0) {
+            view.showFavorites(item.favorites)
+        } else {
+            view.hideFavorites()
+        }
+
+        view.setSize(resourceProvider.formatFileSize(item.size))
+
+        if (item.exclusive) view.showExclusive() else view.hideExclusive()
+
+        if (item.openSource) view.showOpenSource() else view.hideOpenSource()
+
+        if (item.official) view.showOfficial() else view.hideOfficial()
+
+        item.category?.let {
+            val title = it.name[locale.language] ?: it.name[DEFAULT_LOCALE].orEmpty()
+            view.showCategory(it.icon, title)
+        } ?: view.hideCategory()
+
+        item.osVersion?.let { osVersion ->
+            if ((item.minSdk ?: 0) <= Build.VERSION.SDK_INT) {
+                view.showOsVersionCompatible(osVersion)
+            } else {
+                view.showOsVersionIncompatible(osVersion)
+            }
+        } ?: view.hideOsVersion()
+
+        when (item.securityStatus) {
+            PlaySecurityStatus.SCANNING -> {
+                view.showSecurityScanning(resourceProvider.securityScanningShort())
+                view.setOnSecurityClickListener {
+                    listener.onSecurityInfoClick(item.securityStatus, item.securityScore)
+                }
+            }
+            PlaySecurityStatus.SAFE -> {
+                view.showSecuritySafe(resourceProvider.securitySafeShort())
+                view.setOnSecurityClickListener {
+                    listener.onSecurityInfoClick(item.securityStatus, item.securityScore)
+                }
+            }
+            PlaySecurityStatus.SUSPICIOUS -> {
+                view.showSecuritySuspicious(resourceProvider.securitySuspiciousShort())
+                view.setOnSecurityClickListener {
+                    listener.onSecurityInfoClick(item.securityStatus, item.securityScore)
+                }
+            }
+            PlaySecurityStatus.MALWARE -> {
+                view.showSecurityMalware(resourceProvider.securityMalwareShort())
+                view.setOnSecurityClickListener {
+                    listener.onSecurityInfoClick(item.securityStatus, item.securityScore)
+                }
+            }
+            PlaySecurityStatus.NOT_CHECKED -> {
+                view.showSecurityNotChecked(resourceProvider.securityNotCheckedShort())
+                view.setOnSecurityClickListener {
+                    listener.onSecurityInfoClick(item.securityStatus, item.securityScore)
+                }
+            }
+            null -> {
+                view.hideSecurityStatus()
+                view.setOnSecurityClickListener(null)
+            }
+        }
+    }
+
+}
