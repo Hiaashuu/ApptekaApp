5e66b9268fc72f02b0ce6b7f8efd6ee916f63ff8
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:36:19 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/PackageInfos.kt
@@ -0,0 +1,18 @@
+package com.hiaashuu.appteka.util
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Build
+
+fun PackageInfo.versionCodeCompat(): Long {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        longVersionCode
+    } else {
+        @Suppress("DEPRECATION")
+        versionCode.toLong()
+    }
+}
+
+fun PackageInfo.getLabel(packageManager: PackageManager): String {
+    return applicationInfo?.loadLabel(packageManager)?.toString() ?: "Unknown"
+}
