53cf1993b253da1400f8abae0666a062b83b7bc0
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:31:46 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/upload/adapter/whats_new/WhatsNewItemBlueprint.kt
@@ -0,0 +1,19 @@
+package com.hiaashuu.appteka.screen.upload.adapter.whats_new
+
+import com.hiaashuu.appteka.util.adapter.Item
+import com.hiaashuu.appteka.util.adapter.ItemBlueprint
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.util.adapter.ViewHolderBuilder
+import com.hiaashuu.appteka.R
+
+class WhatsNewItemBlueprint(override val presenter: ItemPresenter<WhatsNewItemView, WhatsNewItem>) :
+    ItemBlueprint<WhatsNewItemView, WhatsNewItem> {
+
+    override val viewHolderProvider = ViewHolderBuilder.ViewHolderProvider(
+        layoutId = R.layout.upload_block_whats_new,
+        creator = { _, view -> WhatsNewItemViewHolder(view) }
+    )
+
+    override fun isRelevantItem(item: Item) = item is WhatsNewItem
+
+}
