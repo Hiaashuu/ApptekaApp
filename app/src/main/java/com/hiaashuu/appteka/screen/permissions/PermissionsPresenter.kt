d15f634b8aa221434a2413121d3f3119fdb10caa
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:24:39 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/permissions/PermissionsPresenter.kt
@@ -0,0 +1,81 @@
+package com.hiaashuu.appteka.screen.permissions
+
+import android.os.Bundle
+import com.hiaashuu.appteka.util.adapter.AdapterPresenter
+import com.hiaashuu.appteka.util.SchedulersFactory
+import dagger.Lazy
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.plusAssign
+
+interface PermissionsPresenter {
+
+    fun attachView(view: PermissionsView)
+
+    fun detachView()
+
+    fun attachRouter(router: PermissionsRouter)
+
+    fun detachRouter()
+
+    fun saveState(): Bundle
+
+    fun onBackPressed()
+
+    interface PermissionsRouter {
+
+        fun leaveScreen()
+
+    }
+
+}
+
+class PermissionsPresenterImpl(
+    private val permissions: List<String>,
+    private val adapterPresenter: Lazy<AdapterPresenter>,
+    private val converter: PermissionsConverter,
+    private val schedulers: SchedulersFactory,
+    state: Bundle?
+) : PermissionsPresenter {
+
+    private var view: PermissionsView? = null
+    private var router: PermissionsPresenter.PermissionsRouter? = null
+
+    private val subscriptions = CompositeDisposable()
+
+    override fun attachView(view: PermissionsView) {
+        this.view = view
+
+        subscriptions += view.navigationClicks().subscribe {
+            onBackPressed()
+        }
+
+        bindPermissions()
+    }
+
+    override fun detachView() {
+        subscriptions.clear()
+        this.view = null
+    }
+
+    override fun attachRouter(router: PermissionsPresenter.PermissionsRouter) {
+        this.router = router
+    }
+
+    override fun detachRouter() {
+        this.router = null
+    }
+
+    override fun saveState() = Bundle().apply {
+    }
+
+    override fun onBackPressed() {
+        router?.leaveScreen()
+    }
+
+    private fun bindPermissions() {
+        val items = permissions.map { converter.convert(it) }
+        adapterPresenter.get().onDataSourceChanged(items)
+        view?.contentUpdated()
+    }
+
+}
