723f3cda22beae836ed663241576fdb9634a151b
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:17:50 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/abi/AbiItemPresenter.kt
@@ -0,0 +1,17 @@
+package com.hiaashuu.appteka.screen.details.adapter.abi
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+
+class AbiItemPresenter(
+    private val resourceProvider: AbiResourceProvider,
+) : ItemPresenter<AbiItemView, AbiItem> {
+
+    override fun bindView(view: AbiItemView, item: AbiItem, position: Int) {
+        val formattedAbis = item.abiList.map { resourceProvider.formatAbiName(it) }
+        val compatibilityText = resourceProvider.getCompatibilityText(item.isCompatible)
+
+        view.showArchitectures(formattedAbis)
+        view.showCompatibility(compatibilityText, item.isCompatible)
+    }
+
+}
