0ac4cce0a9007bcb7cfa6df51829bf5b8722e201
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:48 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/api/TranslationResponse.kt
@@ -0,0 +1,19 @@
+package com.hiaashuu.appteka.screen.details.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@GsonModel
+@Parcelize
+data class TranslationResponse(
+    @SerializedName("whats_new")
+    val whatsNew: String?,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("whats_new_lang")
+    val whatsNewLang: String?,
+    @SerializedName("description_lang")
+    val descriptionLang: String?,
+) : Parcelable
