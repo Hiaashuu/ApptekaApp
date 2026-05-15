bdc37e2c51b773f0822e9add36df732c1d857a51
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:44 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/api/Scores.kt
@@ -0,0 +1,21 @@
+package com.hiaashuu.appteka.screen.details.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@GsonModel
+@Parcelize
+data class Scores(
+    @SerializedName("five")
+    val five: Int,
+    @SerializedName("four")
+    val four: Int,
+    @SerializedName("three")
+    val three: Int,
+    @SerializedName("two")
+    val two: Int,
+    @SerializedName("one")
+    val one: Int,
+) : Parcelable
