fc9783d3b5ad11230d6826da9761e3db7e2c9e90
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:34:28 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/upload/UploadResponse.kt
@@ -0,0 +1,15 @@
+package com.hiaashuu.appteka.upload
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@GsonModel
+@Parcelize
+data class UploadResponse(
+    @SerializedName("app_id")
+    val appId: String,
+    @SerializedName("file_status")
+    val fileStatus: Int,
+) : Parcelable
