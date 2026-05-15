64d01d2c2a00bdca993d467bf7b0492ca8181596
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:16:08 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/auth/request_code/RequestCodeResourceProvider.kt
@@ -0,0 +1,36 @@
+package com.hiaashuu.appteka.screen.auth.request_code
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+
+interface RequestCodeResourceProvider {
+
+    fun getRateLimitError(): String
+
+    fun getDomainBlockedError(): String
+
+    fun getServiceError(): String
+
+    fun getNetworkError(): String
+
+}
+
+class RequestCodeResourceProviderImpl(val resources: Resources) : RequestCodeResourceProvider {
+
+    override fun getRateLimitError(): String {
+        return resources.getString(R.string.error_rate_limit)
+    }
+
+    override fun getDomainBlockedError(): String {
+        return resources.getString(R.string.error_domain_blocked)
+    }
+
+    override fun getServiceError(): String {
+        return resources.getString(R.string.error_sending_request_code)
+    }
+
+    override fun getNetworkError(): String {
+        return resources.getString(R.string.network_error)
+    }
+
+}
