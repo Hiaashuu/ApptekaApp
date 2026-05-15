969977c4f6ca6a39ddd36412a0b4ee9fc8693fd7
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:26:30 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/adapter/review/ReviewItemPresenter.kt
@@ -0,0 +1,23 @@
+package com.hiaashuu.appteka.screen.profile.adapter.review
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.reviews.ReviewItemListener
+import java.text.DateFormat
+
+class ReviewItemPresenter(
+    private val dateFormatter: DateFormat,
+    private val listener: ReviewItemListener,
+) : ItemPresenter<ReviewItemView, ReviewItem> {
+
+    override fun bindView(view: ReviewItemView, item: ReviewItem, position: Int) {
+        view.setIcon(item.icon)
+        view.setTitle(item.title)
+        view.setVersion(item.version)
+        view.setRating(item.rating)
+        val date: String = dateFormatter.format(item.time)
+        view.setDate(date)
+        view.setReview(item.text)
+        view.setOnClickListener { listener.onRatingClick(item) }
+    }
+
+}
