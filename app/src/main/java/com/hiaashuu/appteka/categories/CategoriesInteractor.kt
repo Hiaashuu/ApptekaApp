eeb644225704c87e7d4351e639359e26cce32502
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:14:41 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/categories/CategoriesInteractor.kt
@@ -0,0 +1,38 @@
+package com.hiaashuu.appteka.categories
+
+import com.hiaashuu.appteka.core.StoreApi
+import com.hiaashuu.appteka.util.SchedulersFactory
+import io.reactivex.rxjava3.core.Single
+
+interface CategoriesInteractor {
+
+    fun getCategories(): Single<List<Category>>
+
+}
+
+class CategoriesInteractorImpl(
+    private val api: StoreApi,
+    private val schedulers: SchedulersFactory
+) : CategoriesInteractor {
+
+    private var categories: List<Category>? = null
+
+    override fun getCategories(): Single<List<Category>> {
+        return Single
+            .create { emitter ->
+                categories?.let {
+                    emitter.onSuccess(it)
+                } ?: emitter.onError(Exception("No cached categories"))
+            }
+            .onErrorResumeWith(
+                api.getCategories()
+                    .map {
+                        val categoriesSorted = it.result.categories
+                        categories = categoriesSorted
+                        categoriesSorted
+                    }
+                    .subscribeOn(schedulers.io())
+            )
+    }
+
+}
