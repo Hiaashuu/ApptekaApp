9039476c215a3a4fb14f87d01843951eb0fb037e
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:20:05 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/distro/adapter/apk/ApkItem.kt
@@ -0,0 +1,19 @@
+package com.hiaashuu.appteka.screen.distro.adapter.apk
+
+import android.os.Parcelable
+import com.hiaashuu.appteka.util.adapter.Item
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class ApkItem(
+    override val id: Long,
+    val icon: String?,
+    val title: String,
+    val version: String,
+    val size: Long,
+    val lastModified: Long,
+    val packageName: String,
+    val path: String?,
+    val fileName: String,
+    var isNew: Boolean,
+) : Item, Parcelable
