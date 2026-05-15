284044d24081567d8747bdbfb649f96998235af8
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:33:52 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/users/api/SubscriberEntity.kt
@@ -0,0 +1,18 @@
+package com.hiaashuu.appteka.screen.users.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.user.api.UserBrief
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@GsonModel
+@Parcelize
+data class SubscriberEntity(
+    @SerializedName("id")
+    val rowId: Int,
+    @SerializedName("time")
+    val time: Long,
+    @SerializedName("user")
+    val user: UserBrief,
+) : UserEntity, Parcelable
