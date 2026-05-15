4723d80e840dd65939daeec7c0a67fdd640ed1ab
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:22:51 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/gallery/GalleryView.kt
@@ -0,0 +1,100 @@
+package com.hiaashuu.appteka.screen.gallery
+
+import android.annotation.SuppressLint
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager2.widget.ViewPager2
+import com.hiaashuu.appteka.util.adapter.SimpleRecyclerAdapter
+import com.google.android.material.snackbar.Snackbar
+import com.jakewharton.rxrelay3.PublishRelay
+import com.hiaashuu.appteka.R
+import io.reactivex.rxjava3.core.Observable
+
+interface GalleryView {
+
+    fun setTitle(title: String)
+
+    fun contentUpdated()
+
+    fun setCurrentIndex(index: Int)
+
+    fun showError(text: String)
+
+    fun navigationClicks(): Observable<Unit>
+
+    fun pageChanged(): Observable<Int>
+
+    fun downloadClicks(): Observable<Unit>
+
+}
+
+class GalleryViewImpl(
+    view: View,
+    private val adapter: SimpleRecyclerAdapter
+) : GalleryView {
+
+    private val pager: ViewPager2 = view.findViewById(R.id.pager)
+    private val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+
+    private val navigationRelay = PublishRelay.create<Unit>()
+    private val pageRelay = PublishRelay.create<Int>()
+    private val downloadRelay = PublishRelay.create<Unit>()
+
+    private val callback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            pageRelay.accept(position)
+        }
+    }
+
+    init {
+        toolbar.setNavigationOnClickListener { navigationRelay.accept(Unit) }
+        toolbar.inflateMenu(R.menu.gallery_menu)
+        toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.download -> {
+                    downloadRelay.accept(Unit)
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        pager.adapter = adapter
+    }
+
+    override fun setTitle(title: String) {
+        toolbar.title = title
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun contentUpdated() {
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun setCurrentIndex(index: Int) {
+        pager.setCurrentItem(index, false)
+    }
+
+    override fun showError(text: String) {
+        Snackbar.make(pager, text, Snackbar.LENGTH_LONG).show()
+    }
+
+    override fun navigationClicks(): Observable<Unit> = navigationRelay
+
+    override fun pageChanged(): Observable<Int> = pageRelay
+        .doOnSubscribe {
+            if (!pageRelay.hasObservers()) {
+                pager.registerOnPageChangeCallback(callback)
+            }
+        }
+        .doFinally {
+            if (!pageRelay.hasObservers()) {
+                pager.unregisterOnPageChangeCallback(callback)
+            }
+        }
+
+    override fun downloadClicks(): Observable<Unit> = downloadRelay
+
+}
