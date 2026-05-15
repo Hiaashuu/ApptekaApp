a6d15ed315ae608ddef88d4f64425f94493ed147
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:35:20 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/Metrics.kt
@@ -0,0 +1,8 @@
+package com.hiaashuu.appteka.util
+
+import android.content.res.Resources
+import android.util.TypedValue
+
+fun dpToPx(px: Int, resources: Resources) = TypedValue
+    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, px.toFloat(), resources.displayMetrics)
+    .toInt()
