bf68661382df84153b6b986e1f74f9f7a770c29d
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:06 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/status/StatusItemView.kt
@@ -0,0 +1,103 @@
+package com.hiaashuu.appteka.screen.details.adapter.status
+
+import android.content.res.ColorStateList
+import android.graphics.PorterDuff
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.google.android.material.button.MaterialButton
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.bind
+import com.hiaashuu.appteka.util.hide
+import com.hiaashuu.appteka.util.show
+
+interface StatusItemView : ItemView {
+
+    fun setStatusTypeInfo()
+
+    fun setStatusTypeWarning()
+
+    fun setStatusTypeError()
+
+    fun hideActionButton()
+
+    fun showActionButton(label: String)
+
+    fun setStatusText(text: String)
+
+    fun setOnActionClickListener(listener: (() -> Unit)?)
+
+}
+
+class StatusItemViewHolder(view: View) : BaseItemViewHolder(view), StatusItemView {
+
+    private val context = view.context
+    private val background: View = view.findViewById(R.id.status_back)
+    private val icon: ImageView = view.findViewById(R.id.status_icon)
+    private val text: TextView = view.findViewById(R.id.status_text)
+    private val actionButton: MaterialButton = view.findViewById(R.id.action_button)
+
+    private var actionClickListener: (() -> Unit)? = null
+
+    init {
+        actionButton.setOnClickListener { actionClickListener?.invoke() }
+    }
+
+    override fun setStatusTypeInfo() {
+        setBackgroundColor(R.color.block_info_back_color)
+        icon.setImageResource(R.drawable.ic_info)
+        icon.setColorFilter(ContextCompat.getColor(context, R.color.block_info_color))
+        text.setTextColor(ContextCompat.getColor(context, R.color.block_info_text_color))
+        actionButton.setRippleColorResource(R.color.block_info_color)
+        actionButton.setTextColor(ContextCompat.getColor(context, R.color.block_info_text_color))
+    }
+
+    override fun setStatusTypeWarning() {
+        setBackgroundColor(R.color.block_warning_back_color)
+        icon.setImageResource(R.drawable.ic_warning)
+        icon.setColorFilter(ContextCompat.getColor(context, R.color.block_warning_color))
+        text.setTextColor(ContextCompat.getColor(context, R.color.block_warning_text_color))
+        actionButton.setRippleColorResource(R.color.block_warning_color)
+        actionButton.setTextColor(ContextCompat.getColor(context, R.color.block_warning_text_color))
+    }
+
+    override fun setStatusTypeError() {
+        setBackgroundColor(R.color.block_error_back_color)
+        icon.setImageResource(R.drawable.ic_error)
+        icon.setColorFilter(ContextCompat.getColor(context, R.color.block_error_color))
+        text.setTextColor(ContextCompat.getColor(context, R.color.block_error_text_color))
+        actionButton.setRippleColorResource(R.color.block_error_color)
+        actionButton.setTextColor(ContextCompat.getColor(context, R.color.block_error_text_color))
+    }
+
+    override fun hideActionButton() {
+        actionButton.hide()
+    }
+
+    override fun showActionButton(label: String) {
+        actionButton.text = label
+        actionButton.show()
+    }
+
+    override fun setStatusText(text: String) {
+        this.text.bind(text)
+    }
+
+    private fun setBackgroundColor(colorRes: Int) {
+        val backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(context, colorRes))
+        background.backgroundTintList = backgroundTintList
+        background.backgroundTintMode = PorterDuff.Mode.SRC_ATOP
+    }
+
+    override fun setOnActionClickListener(listener: (() -> Unit)?) {
+        this.actionClickListener = listener
+    }
+
+    override fun onUnbind() {
+        this.actionClickListener = null
+    }
+
+}
