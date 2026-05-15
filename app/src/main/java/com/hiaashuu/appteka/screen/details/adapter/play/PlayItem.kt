df5cee56819a894ca388008624234c7a03e4e185
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:18:29 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/play/PlayItem.kt
@@ -0,0 +1,31 @@
+package com.hiaashuu.appteka.screen.details.adapter.play
+
+import android.os.Parcelable
+import com.hiaashuu.appteka.util.adapter.Item
+import com.hiaashuu.appteka.categories.Category
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PlayItem(
+    override val id: Long,
+    val rating: Float?,
+    val downloads: Int,
+    val favorites: Int,
+    val size: Long,
+    val exclusive: Boolean,
+    val openSource: Boolean,
+    val official: Boolean,
+    val category: Category?,
+    val osVersion: String?,
+    val minSdk: Int?,
+    val securityStatus: PlaySecurityStatus?,
+    val securityScore: Int?,
+) : Item, Parcelable
+
+enum class PlaySecurityStatus {
+    SCANNING,
+    SAFE,
+    SUSPICIOUS,
+    MALWARE,
+    NOT_CHECKED
+}
