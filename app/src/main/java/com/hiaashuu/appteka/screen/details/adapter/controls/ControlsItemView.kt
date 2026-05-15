de05d716416adb1e78387809e95c3866b9f35290
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:18:04 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/controls/ControlsItemView.kt
@@ -0,0 +1,142 @@
+package com.hiaashuu.appteka.screen.details.adapter.controls
+
+import android.view.View
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.disable
+import com.hiaashuu.appteka.util.enable
+import com.hiaashuu.appteka.util.hide
+import com.hiaashuu.appteka.util.show
+
+interface ControlsItemView : ItemView {
+
+    fun showInstallButton()
+
+    fun disableInstallButton()
+
+    fun showUpdateButton()
+
+    fun disableUpdateButton()
+
+    fun showLaunchButton()
+
+    fun disableLaunchButton()
+
+    fun showRemoveButton()
+
+    fun showCancelButton()
+
+    fun hideButtons()
+
+    fun enableButtons()
+
+    fun setOnInstallClickListener(listener: (() -> Unit)?)
+
+    fun setOnUpdateClickListener(listener: (() -> Unit)?)
+
+    fun setOnLaunchClickListener(listener: (() -> Unit)?)
+
+    fun setOnRemoveClickListener(listener: (() -> Unit)?)
+
+    fun setOnCancelClickListener(listener: (() -> Unit)?)
+
+}
+
+class ControlsItemViewHolder(view: View) : BaseItemViewHolder(view), ControlsItemView {
+
+    private val installButton: View = view.findViewById(R.id.install_button)
+    private val updateButton: View = view.findViewById(R.id.update_button)
+    private val launchButton: View = view.findViewById(R.id.launch_button)
+    private val removeButton: View = view.findViewById(R.id.remove_button)
+    private val cancelButton: View = view.findViewById(R.id.cancel_button)
+
+    private var installClickListener: (() -> Unit)? = null
+    private var updateClickListener: (() -> Unit)? = null
+    private var launchClickListener: (() -> Unit)? = null
+    private var removeClickListener: (() -> Unit)? = null
+    private var cancelClickListener: (() -> Unit)? = null
+
+    init {
+        installButton.setOnClickListener { installClickListener?.invoke() }
+        updateButton.setOnClickListener { updateClickListener?.invoke() }
+        launchButton.setOnClickListener { launchClickListener?.invoke() }
+        removeButton.setOnClickListener { removeClickListener?.invoke() }
+        cancelButton.setOnClickListener { cancelClickListener?.invoke() }
+    }
+
+    override fun showInstallButton() {
+        installButton.show()
+    }
+
+    override fun disableInstallButton() {
+        installButton.disable()
+    }
+
+    override fun showUpdateButton() {
+        updateButton.show()
+    }
+
+    override fun disableUpdateButton() {
+        updateButton.disable()
+    }
+
+    override fun showLaunchButton() {
+        launchButton.show()
+    }
+
+    override fun disableLaunchButton() {
+        launchButton.disable()
+    }
+
+    override fun showRemoveButton() {
+        removeButton.show()
+    }
+
+    override fun showCancelButton() {
+        cancelButton.show()
+    }
+
+    override fun hideButtons() {
+        installButton.hide()
+        updateButton.hide()
+        launchButton.hide()
+        removeButton.hide()
+        cancelButton.hide()
+    }
+
+    override fun enableButtons() {
+        installButton.enable()
+        updateButton.enable()
+        launchButton.enable()
+    }
+
+    override fun setOnInstallClickListener(listener: (() -> Unit)?) {
+        this.installClickListener = listener
+    }
+
+    override fun setOnUpdateClickListener(listener: (() -> Unit)?) {
+        this.updateClickListener = listener
+    }
+
+    override fun setOnLaunchClickListener(listener: (() -> Unit)?) {
+        this.launchClickListener = listener
+    }
+
+    override fun setOnRemoveClickListener(listener: (() -> Unit)?) {
+        this.removeClickListener = listener
+    }
+
+    override fun setOnCancelClickListener(listener: (() -> Unit)?) {
+        this.cancelClickListener = listener
+    }
+
+    override fun onUnbind() {
+        this.installClickListener = null
+        this.updateClickListener = null
+        this.launchClickListener = null
+        this.removeClickListener = null
+        this.cancelClickListener = null
+    }
+
+}
