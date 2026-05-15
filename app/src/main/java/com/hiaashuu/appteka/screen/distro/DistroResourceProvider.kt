1280ffb0ca0498975e5dd50bcbb1838436baacef
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:20:04 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/distro/DistroResourceProvider.kt
@@ -0,0 +1,39 @@
+package com.hiaashuu.appteka.screen.distro
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.FileHelper
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+interface DistroResourceProvider {
+
+    fun formatAppVersion(verName: String, verCode: Int): String
+
+    fun formatFileSize(size: Long): String
+
+    fun formatDate(value: Long): String
+
+}
+
+class DistroResourceProviderImpl(
+    val resources: Resources,
+    val locale: Locale,
+) : DistroResourceProvider {
+
+    private val dateFormat: DateFormat = SimpleDateFormat("dd.MM.yy", locale)
+
+    override fun formatAppVersion(verName: String, verCode: Int): String {
+        return resources.getString(R.string.app_version_format, verName, verCode)
+    }
+
+    override fun formatFileSize(size: Long): String {
+        return FileHelper.formatBytes(resources, size)
+    }
+
+    override fun formatDate(value: Long): String {
+        return dateFormat.format(value)
+    }
+
+}
