bf316503c4fdd946252d0171df1a203a1eff1a58
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:18:11 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/description/DescriptionResourceProvider.kt
@@ -0,0 +1,32 @@
+package com.hiaashuu.appteka.screen.details.adapter.description
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+interface DescriptionResourceProvider {
+
+    fun formatFileVersion(versionName: String, versionCode: Int): String
+
+    fun formatDate(value: Long): String
+
+}
+
+class DescriptionResourceProviderImpl(
+    val resources: Resources,
+    val locale: Locale,
+) : DescriptionResourceProvider {
+
+    private val dateFormat: DateFormat = SimpleDateFormat("dd.MM.yy", locale)
+
+    override fun formatFileVersion(versionName: String, versionCode: Int): String {
+        return resources.getString(R.string.app_version_format, versionName, versionCode)
+    }
+
+    override fun formatDate(value: Long): String {
+        return dateFormat.format(value)
+    }
+
+}
