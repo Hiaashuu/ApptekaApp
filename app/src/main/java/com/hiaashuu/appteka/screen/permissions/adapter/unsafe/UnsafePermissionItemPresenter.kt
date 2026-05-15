c90c46f9a60708e9da3e4d7a5544769b6a326843
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:24:27 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/permissions/adapter/unsafe/UnsafePermissionItemPresenter.kt
@@ -0,0 +1,16 @@
+package com.hiaashuu.appteka.screen.permissions.adapter.unsafe
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.permissions.PermissionsResourceProvider
+
+class UnsafePermissionItemPresenter(
+    private val resourceProvider: PermissionsResourceProvider
+) : ItemPresenter<UnsafePermissionItemView, UnsafePermissionItem> {
+
+    override fun bindView(view: UnsafePermissionItemView, item: UnsafePermissionItem, position: Int) {
+        val description = item.description ?: resourceProvider.getUnknownPermissionString()
+        view.setDescription(description)
+        view.setPermission(item.permission)
+    }
+
+}
