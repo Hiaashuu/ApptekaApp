38aec24993721c626d1700bd275adea1362a2401
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:26:46 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/adapter/uploads/AppItemListener.kt
@@ -0,0 +1,9 @@
+package com.hiaashuu.appteka.screen.profile.adapter.uploads
+
+import com.hiaashuu.appteka.screen.profile.adapter.app.AppItem
+
+interface AppItemListener {
+
+    fun onAppClick(app: AppItem)
+
+}
