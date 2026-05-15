edb68e520b3b205a16fe5757f3223aaaf714560a
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:16:17 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/auth/verify_code/VerifyCodeActivity.kt
@@ -0,0 +1,109 @@
+package com.hiaashuu.appteka.screen.auth.verify_code
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hiaashuu.appteka.appComponent
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.screen.auth.verify_code.di.VerifyCodeModule
+import com.hiaashuu.appteka.util.Analytics
+import javax.inject.Inject
+
+class VerifyCodeActivity : AppCompatActivity(), VerifyCodePresenter.VerifyCodeRouter {
+
+    @Inject
+    lateinit var presenter: VerifyCodePresenter
+
+    @Inject
+    lateinit var analytics: Analytics
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val email = intent.getStringExtra(EXTRA_EMAIL)
+            ?: throw IllegalArgumentException("email must be provided")
+        val requestId = intent.getStringExtra(EXTRA_REQUEST_ID)
+            ?: throw IllegalArgumentException("requestId must be provided")
+        val registered = intent.getBooleanExtra(EXTRA_REGISTERED, false)
+        val codeRegex = intent.getStringExtra(EXTRA_CODE_REGEX)
+            ?: throw IllegalArgumentException("codeRegex must be provided")
+        val nameRegex = intent.getStringExtra(EXTRA_NAME_REGEX)
+            ?: throw IllegalArgumentException("nameRegex must be provided")
+
+        val presenterState = savedInstanceState?.getBundle(KEY_PRESENTER_STATE)
+        appComponent
+            .verifyCodeComponent(
+                VerifyCodeModule(
+                    this,
+                    email,
+                    requestId,
+                    registered,
+                    codeRegex,
+                    nameRegex,
+                    presenterState
+                )
+            )
+            .inject(activity = this)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.verify_code_activity)
+
+        val view = VerifyCodeViewImpl(window.decorView)
+
+        presenter.attachView(view)
+
+        if (savedInstanceState == null) {
+            analytics.trackEvent("open-verify-code-screen")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachRouter(this)
+    }
+
+    override fun onStop() {
+        presenter.detachRouter()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        presenter.detachView()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBundle(KEY_PRESENTER_STATE, presenter.saveState())
+    }
+
+    override fun leaveScreen(success: Boolean) {
+        if (success) {
+            setResult(RESULT_OK)
+        } else {
+            setResult(RESULT_CANCELED)
+        }
+        finish()
+    }
+
+}
+
+fun createVerifyCodeActivityIntent(
+    context: Context,
+    email: String,
+    requestId: String,
+    registered: Boolean,
+    codeRegex: String,
+    nameRegex: String,
+): Intent = Intent(context, VerifyCodeActivity::class.java)
+    .putExtra(EXTRA_EMAIL, email)
+    .putExtra(EXTRA_REQUEST_ID, requestId)
+    .putExtra(EXTRA_REGISTERED, registered)
+    .putExtra(EXTRA_CODE_REGEX, codeRegex)
+    .putExtra(EXTRA_NAME_REGEX, nameRegex)
+
+private const val EXTRA_EMAIL = "email"
+private const val EXTRA_REQUEST_ID = "request_id"
+private const val EXTRA_REGISTERED = "registered"
+private const val EXTRA_CODE_REGEX = "code_regex"
+private const val EXTRA_NAME_REGEX = "name_regex"
+private const val KEY_PRESENTER_STATE = "presenter_state"
