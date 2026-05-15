05fec236658251b31f836b204355808babb703ae
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:22:24 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/feed/adapter/upload/UploadItemView.kt
@@ -0,0 +1,225 @@
+package com.hiaashuu.appteka.screen.feed.adapter.upload
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.dto.Screenshot
+import com.hiaashuu.appteka.dto.BadgeMark
+import com.hiaashuu.appteka.dto.UserIcon
+import com.hiaashuu.appteka.screen.feed.adapter.ReactionsAdapter
+import com.hiaashuu.appteka.screen.feed.adapter.ScreenshotsAdapter
+import com.hiaashuu.appteka.screen.feed.api.Reaction
+import com.hiaashuu.appteka.util.bind
+import com.hiaashuu.appteka.util.hide
+import com.hiaashuu.appteka.util.show
+import com.hiaashuu.appteka.view.UserIconView
+import com.hiaashuu.appteka.view.UserIconViewImpl
+import com.tomclaw.imageloader.util.fetch
+
+interface UploadItemView : ItemView {
+
+    fun setUserIcon(userIcon: UserIcon)
+
+    fun setUserBadge(badge: BadgeMark?)
+
+    fun setUserName(name: String)
+
+    fun setIcon(url: String?)
+
+    fun setLabel(value: String)
+
+    fun setPackage(value: String)
+
+    fun setImages(screenshots: List<Screenshot>)
+
+    fun hideImage()
+
+    fun setText(text: String)
+
+    fun setTime(time: String)
+
+    fun showProgress()
+
+    fun hideProgress()
+
+    fun showMenu()
+
+    fun hideMenu()
+
+    fun setOnPostClickListener(listener: (() -> Unit)?)
+
+    fun setOnAppClickListener(listener: (() -> Unit)?)
+
+    fun setOnMenuClickListener(listener: (() -> Unit)?)
+
+    fun setReactions(reactions: List<Reaction>)
+
+    fun hideReactions()
+
+    fun setOnReactionClickListener(listener: ((Reaction) -> Unit)?)
+
+}
+
+class UploadItemViewHolder(
+    view: View,
+    private val adapter: ScreenshotsAdapter,
+    private val reactionsAdapter: ReactionsAdapter,
+) : BaseItemViewHolder(view), UploadItemView {
+
+    private val userIcon: UserIconView = UserIconViewImpl(view.findViewById(R.id.member_icon))
+    private val userName: TextView = view.findViewById(R.id.user_name)
+    private val time: TextView = view.findViewById(R.id.date_view)
+    private val appBlock: View = view.findViewById(R.id.app_block)
+    private val icon: ImageView = view.findViewById(R.id.app_icon)
+    private val label: TextView = view.findViewById(R.id.app_label)
+    private val packageName: TextView = view.findViewById(R.id.app_package)
+    private val text: TextView = view.findViewById(R.id.text)
+    private val images: RecyclerView = view.findViewById(R.id.images)
+    private val reactions: RecyclerView = view.findViewById(R.id.reactions)
+    private val menu: View = view.findViewById(R.id.post_menu)
+
+    private var postClickListener: (() -> Unit)? = null
+    private var appClickListener: (() -> Unit)? = null
+    private var menuClickListener: (() -> Unit)? = null
+    private var reactionClickListener: ((Reaction) -> Unit)? = null
+
+    init {
+        view.setOnClickListener { postClickListener?.invoke() }
+        appBlock.setOnClickListener { appClickListener?.invoke() }
+        menu.setOnClickListener { menuClickListener?.invoke() }
+
+        adapter.setHasStableIds(true)
+
+        val orientation = RecyclerView.HORIZONTAL
+        val layoutManager = LinearLayoutManager(view.context, orientation, false)
+        images.adapter = adapter
+        images.layoutManager = layoutManager
+        images.itemAnimator = DefaultItemAnimator()
+        images.itemAnimator?.changeDuration = DURATION_MEDIUM
+
+        val reactionsOrientation = RecyclerView.HORIZONTAL
+        val reactionsLayoutManager = LinearLayoutManager(view.context, reactionsOrientation, false)
+        reactions.adapter = reactionsAdapter
+        reactions.layoutManager = reactionsLayoutManager
+        reactions.itemAnimator = DefaultItemAnimator()
+        reactions.itemAnimator?.changeDuration = DURATION_MEDIUM
+    }
+
+    override fun setUserIcon(userIcon: UserIcon) {
+        this.userIcon.bind(userIcon)
+    }
+
+    override fun setUserBadge(badge: BadgeMark?) {
+        this.userIcon.bindBadge(badge)
+    }
+
+    override fun setUserName(name: String) {
+        userName.bind(name)
+    }
+
+    override fun setIcon(url: String?) {
+        icon.fetch(url.orEmpty()) {
+            centerCrop()
+            placeholder(R.drawable.app_placeholder)
+            onLoading { imageView ->
+                imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                imageView.setImageResource(R.drawable.app_placeholder)
+            }
+        }
+    }
+
+    override fun setLabel(value: String) {
+        label.bind(value)
+    }
+
+    override fun setPackage(value: String) {
+        packageName.bind(value)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun setImages(screenshots: List<Screenshot>) {
+        images.show()
+        with(adapter) {
+            dataSet.clear()
+            dataSet.addAll(screenshots)
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun hideImage() {
+        images.hide()
+    }
+
+    override fun setText(text: String) {
+        this.text.bind(text)
+    }
+
+    override fun setTime(time: String) {
+        this.time.bind(time)
+    }
+
+    override fun showProgress() {
+    }
+
+    override fun hideProgress() {
+    }
+
+    override fun showMenu() {
+        menu.show()
+    }
+
+    override fun hideMenu() {
+        menu.hide()
+    }
+
+    override fun setOnPostClickListener(listener: (() -> Unit)?) {
+        this.postClickListener = listener
+    }
+
+    override fun setOnAppClickListener(listener: (() -> Unit)?) {
+        this.appClickListener = listener
+    }
+
+    override fun setOnMenuClickListener(listener: (() -> Unit)?) {
+        this.menuClickListener = listener
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun setReactions(reactions: List<Reaction>) {
+        if (reactions.isNotEmpty()) {
+            this.reactions.show()
+            with(reactionsAdapter) {
+                dataSet.clear()
+                dataSet.addAll(reactions)
+                setClickListener(reactionClickListener)
+                notifyDataSetChanged()
+            }
+        } else {
+            this.reactions.hide()
+        }
+    }
+
+    override fun hideReactions() {
+        reactions.hide()
+    }
+
+    override fun setOnReactionClickListener(listener: ((Reaction) -> Unit)?) {
+        this.reactionClickListener = listener
+        reactionsAdapter.setClickListener(listener)
+    }
+
+    override fun onUnbind() {
+        this.postClickListener = null
+    }
+
+}
+
+private const val DURATION_MEDIUM = 300L
