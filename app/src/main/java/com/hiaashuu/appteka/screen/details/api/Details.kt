fdeb99104a09d185b500195c3c700072fd98e1ab
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:36 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/api/Details.kt
@@ -0,0 +1,44 @@
+package com.hiaashuu.appteka.screen.details.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.core.permissions.Capability
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@GsonModel
+@Parcelize
+data class Details(
+    @SerializedName("url")
+    val url: String,
+    @SerializedName("topic_id")
+    val topicId: Int?,
+    @SerializedName("msg_count")
+    val msgCount: Int?,
+    @SerializedName("actions")
+    val actions: ArrayList<String>?,
+    @SerializedName("rates")
+    val ratingsList: ArrayList<RatingEntity>?,
+    @SerializedName("user_rating")
+    val userRating: RatingEntity?,
+    @SerializedName("versions")
+    val versions: ArrayList<AppVersion>?,
+    @SerializedName("link")
+    val link: String,
+    @SerializedName("expires_in")
+    val expiresIn: Long,
+    @SerializedName("meta")
+    val meta: Meta?,
+    @SerializedName("info")
+    val info: AppInfo,
+    @SerializedName("is_favorite")
+    val isFavorite: Boolean?,
+    @SerializedName("security")
+    val security: Security?,
+    @SerializedName("translation")
+    val translation: TranslationResponse?,
+    @SerializedName("developer")
+    val developer: Developer?,
+    @SerializedName("capabilities")
+    val capabilities: Map<String, Capability>? = null,
+) : Parcelable
