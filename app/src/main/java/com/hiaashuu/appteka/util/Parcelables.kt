104a1e4e86c576aa22abf8eb3a38d73cd123ac18
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:36:23 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/Parcelables.kt
@@ -0,0 +1,69 @@
+package com.hiaashuu.appteka.util
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+
+fun Parcel.writeBool(value: Boolean) {
+    writeInt(if (value) 1 else 0)
+}
+
+fun Parcel.readBool(): Boolean {
+    return readInt() == 1
+}
+
+fun <T : Parcelable> Bundle.getParcelableCompat(key: String, clazz: Class<T>): T? {
+    return try {
+        classLoader = clazz.classLoader
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getParcelable(key, clazz)
+        } else {
+            @Suppress("DEPRECATION")
+            getParcelable(key)
+        }
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun <T : Parcelable> Bundle.getParcelableArrayListCompat(
+    key: String,
+    clazz: Class<T>
+): ArrayList<T>? {
+    return try {
+        classLoader = clazz.classLoader
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getParcelableArrayList(key, clazz)
+        } else {
+            @Suppress("DEPRECATION")
+            getParcelableArrayList(key)
+        }
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun <T : Parcelable> Intent.getParcelableExtraCompat(key: String, clazz: Class<T>): T? {
+    setExtrasClassLoader(clazz.classLoader)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(key, clazz)
+    } else {
+        @Suppress("DEPRECATION")
+        getParcelableExtra(key)
+    }
+}
+
+fun <T : Parcelable> Intent.getParcelableArrayListCompat(
+    key: String,
+    clazz: Class<T>
+): ArrayList<T>? {
+    setExtrasClassLoader(clazz.classLoader)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableArrayListExtra(key, clazz)
+    } else {
+        @Suppress("DEPRECATION")
+        getParcelableArrayListExtra(key)
+    }
+}
