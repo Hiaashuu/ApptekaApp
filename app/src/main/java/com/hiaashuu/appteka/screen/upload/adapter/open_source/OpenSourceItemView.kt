91955241bc8f7317a9c7d74ad733c7da741176cd
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:30:43 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/upload/adapter/open_source/OpenSourceItemView.kt
@@ -0,0 +1,67 @@
+package com.hiaashuu.appteka.screen.upload.adapter.open_source
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import androidx.core.view.isVisible
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.google.android.material.materialswitch.MaterialSwitch
+import com.google.android.material.textfield.TextInputLayout
+import com.hiaashuu.appteka.R
+
+interface OpenSourceItemView : ItemView {
+
+    fun setOpenSource(value: Boolean)
+
+    fun setUrl(uri: String)
+
+    fun setUrlVisible(visible: Boolean)
+
+    fun setOnOpenSourceChangedListener(listener: ((Boolean, String) -> Unit)?)
+
+}
+
+class OpenSourceItemViewHolder(view: View) : BaseItemViewHolder(view), OpenSourceItemView {
+
+    private val openSourceSwitch: MaterialSwitch = view.findViewById(R.id.open_source)
+    private val sourceUrlLayout: TextInputLayout = view.findViewById(R.id.source_url_layout)
+    private val sourceUrlEdit: EditText = view.findViewById(R.id.source_url)
+
+    private var openSourceChangedListener: ((Boolean, String) -> Unit)? = null
+
+    init {
+        openSourceSwitch.setOnCheckedChangeListener { _, isChecked ->
+            openSourceChangedListener?.invoke(isChecked, sourceUrlEdit.text.toString())
+        }
+        sourceUrlEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                openSourceChangedListener?.invoke(openSourceSwitch.isChecked, s.toString())
+            }
+        })
+    }
+
+    override fun setOpenSource(value: Boolean) {
+        openSourceSwitch.isChecked = value
+    }
+
+    override fun setUrl(uri: String) {
+        sourceUrlEdit.setText(uri)
+    }
+
+    override fun setUrlVisible(visible: Boolean) {
+        sourceUrlLayout.isVisible = visible
+    }
+
+    override fun setOnOpenSourceChangedListener(listener: ((Boolean, String) -> Unit)?) {
+        this.openSourceChangedListener = listener
+    }
+
+    override fun onUnbind() {
+        this.openSourceChangedListener = null
+    }
+
+}
