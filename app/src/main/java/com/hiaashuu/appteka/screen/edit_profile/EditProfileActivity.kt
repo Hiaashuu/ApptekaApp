ad2a866c165608a5b0c7d52f9939cb8c7a43aa1a
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:20:42 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/edit_profile/EditProfileActivity.kt
@@ -0,0 +1,106 @@
+package com.hiaashuu.appteka.screen.edit_profile
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.appComponent
+import com.hiaashuu.appteka.screen.avatar_crop.createAvatarCropActivityIntent
+import com.hiaashuu.appteka.screen.avatar_crop.extractAvatarCropResultUri
+import com.hiaashuu.appteka.screen.edit_profile.di.EditProfileModule
+import com.hiaashuu.appteka.util.Analytics
+import javax.inject.Inject
+
+class EditProfileActivity : AppCompatActivity(), EditProfilePresenter.EditProfileRouter {
+
+    @Inject
+    lateinit var presenter: EditProfilePresenter
+
+    @Inject
+    lateinit var analytics: Analytics
+
+    private val pickImageLauncher =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) presenter.onAvatarPicked(uri)
+        }
+
+    private val cropLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val cropped = extractAvatarCropResultUri(result.data)
+                if (cropped != null) {
+                    presenter.onAvatarCropped(cropped)
+                } else {
+                    presenter.onAvatarPickFailed()
+                }
+            }
+
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val presenterState = savedInstanceState?.getBundle(KEY_PRESENTER_STATE)
+        appComponent
+            .editProfileComponent(EditProfileModule(this, presenterState))
+            .inject(activity = this)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_profile_activity)
+
+        val view = EditProfileViewImpl(window.decorView)
+        presenter.attachView(view)
+
+        if (savedInstanceState == null) {
+            analytics.trackEvent("open-edit-profile-screen")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachRouter(this)
+    }
+
+    override fun onStop() {
+        presenter.detachRouter()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        presenter.detachView()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBundle(KEY_PRESENTER_STATE, presenter.saveState())
+    }
+
+    override fun openImagePicker() {
+        val request = PickVisualMediaRequest.Builder()
+            .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            .build()
+        pickImageLauncher.launch(request)
+    }
+
+    override fun openCropper(srcUri: Uri) {
+        cropLauncher.launch(createAvatarCropActivityIntent(this, srcUri))
+    }
+
+    override fun leaveScreen(success: Boolean) {
+        if (success) {
+            setResult(RESULT_OK)
+        } else {
+            setResult(RESULT_CANCELED)
+        }
+        finish()
+    }
+
+}
+
+fun createEditProfileActivityIntent(context: Context): Intent =
+    Intent(context, EditProfileActivity::class.java)
+
+private const val KEY_PRESENTER_STATE = "presenter_state"
