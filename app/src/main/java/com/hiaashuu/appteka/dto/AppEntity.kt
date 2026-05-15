084f4fe799bb9c43c2a9c38a054e8bb01a0ad866
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:15:36 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/dto/AppEntity.kt
@@ -0,0 +1,42 @@
+package com.hiaashuu.appteka.dto
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.categories.Category
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@GsonModel
+data class AppEntity(
+    @SerializedName("app_id")
+    val appId: String,
+    @SerializedName("package")
+    val packageName: String,
+    @SerializedName("icon")
+    val icon: String?,
+    @SerializedName("label")
+    val title: String,
+    @SerializedName("ver_name")
+    val verName: String,
+    @SerializedName("ver_code")
+    val verCode: Int,
+    @SerializedName("time")
+    val time: Long,
+    @SerializedName("size")
+    val size: Long,
+    @SerializedName("rating")
+    val rating: Float,
+    @SerializedName("downloads")
+    val downloads: Int,
+    @SerializedName("file_status")
+    val status: Int,
+    @SerializedName("category")
+    val category: Category?,
+    @SerializedName("exclusive")
+    val exclusive: Boolean,
+    @SerializedName("source_url")
+    val sourceUrl: String?,
+    @SerializedName("abi")
+    val abi: List<String>?,
+) : Parcelable
