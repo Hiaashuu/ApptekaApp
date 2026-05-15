c4e826e4e228f888e4175d7350da7c130e655d73
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:44 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/installed/api/UpdateEntity.kt
@@ -0,0 +1,31 @@
+package com.hiaashuu.appteka.screen.installed.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@GsonModel
+data class UpdateEntity(
+    @SerializedName("app_id")
+    val appId: String,
+    @SerializedName("size")
+    val size: Long,
+    @SerializedName("time")
+    val time: Long,
+    @SerializedName("label")
+    val title: String,
+    @SerializedName("package")
+    val packageName: String,
+    @SerializedName("ver_name")
+    val verName: String,
+    @SerializedName("ver_code")
+    val verCode: Int,
+    @SerializedName("downloads")
+    val downloads: Int,
+    @SerializedName("user_id")
+    val userId: String?,
+    @SerializedName("icon")
+    val icon: String?,
+) : Parcelable
