c6d96bc3f423eb9af58c1b780e28ca6cea932e0c
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:17:02 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/chat/adapter/outgoing/OutgoingMsgItemPresenter.kt
@@ -0,0 +1,33 @@
+package com.hiaashuu.appteka.screen.chat.adapter.outgoing
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.chat.adapter.ItemListener
+
+class OutgoingMsgItemPresenter(
+    private val listener: ItemListener
+) : ItemPresenter<OutgoingMsgItemView, OutgoingMsgItem> {
+
+    override fun bindView(view: OutgoingMsgItemView, item: OutgoingMsgItem, position: Int) {
+        listener.onLoadMore(item.msgId)
+
+        item.author.icon?.let(view::setUserIcon)
+        view.setUserBadge(item.author.primaryBadge)
+        view.setTime(item.time)
+        view.setDate(item.date)
+        view.setText(item.text)
+        view.setAttachments(item.attachments)
+        if (item.msgId == 0) {
+            if (item.sent) {
+                view.sentState()
+            } else {
+                view.sendingState()
+            }
+        } else {
+            view.deliveredState()
+        }
+
+        view.setOnClickListener { listener.onItemClick(item) }
+        view.setOnAttachmentClickListener { index -> listener.onAttachmentClick(item, index) }
+    }
+
+}
