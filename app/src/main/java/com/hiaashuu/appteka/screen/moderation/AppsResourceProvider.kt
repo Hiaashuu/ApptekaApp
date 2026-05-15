34cb63de7f426c91260e7433e6de6532a5073cb8
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:24:03 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/moderation/AppsResourceProvider.kt
@@ -0,0 +1,25 @@
+package com.hiaashuu.appteka.screen.moderation
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.FileHelper
+
+interface AppsResourceProvider {
+
+    fun formatAppVersion(verName: String, verCode: Int): String
+
+    fun formatFileSize(size: Long): String
+
+}
+
+class AppsResourceProviderImpl(val resources: Resources) : AppsResourceProvider {
+
+    override fun formatAppVersion(verName: String, verCode: Int): String {
+        return resources.getString(R.string.app_version_format, verName, verCode)
+    }
+
+    override fun formatFileSize(size: Long): String {
+        return FileHelper.formatBytes(resources, size)
+    }
+
+}
