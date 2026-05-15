0afb32b388d629b89e50f800d85262bfbba410e1
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:18 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/home/HomePresenter.kt
@@ -0,0 +1,395 @@
+package com.hiaashuu.appteka.screen.home
+
+import android.os.Bundle
+import com.hiaashuu.appteka.core.permissions.Capability
+import com.hiaashuu.appteka.core.permissions.CapabilityAction
+import com.hiaashuu.appteka.core.permissions.CapabilityPolicy
+import com.hiaashuu.appteka.core.permissions.CapabilityResult
+import com.hiaashuu.appteka.core.permissions.UserCapabilitiesInteractor
+import com.hiaashuu.appteka.core.permissions.UserCapabilitiesProvider
+import com.hiaashuu.appteka.dto.AppEntity
+import com.hiaashuu.appteka.screen.home.api.ModerationData
+import com.hiaashuu.appteka.screen.home.api.StartupResponse
+import com.hiaashuu.appteka.screen.home.api.StatusResponse
+import com.hiaashuu.appteka.user.ModerationProvider
+import com.hiaashuu.appteka.util.SchedulersFactory
+import com.hiaashuu.appteka.util.getParcelableCompat
+import com.tomclaw.bananalytics.Bananalytics
+import com.tomclaw.bananalytics.api.BreadcrumbCategory
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.plusAssign
+
+interface HomePresenter {
+
+    fun attachView(view: HomeView)
+
+    fun detachView()
+
+    fun attachRouter(router: HomeRouter)
+
+    fun detachRouter()
+
+    fun saveState(): Bundle
+
+    fun onBackPressed()
+
+    interface HomeRouter {
+
+        fun showStoreFragment()
+
+        fun showFeedFragment()
+
+        fun showTopicsFragment()
+
+        fun showProfileFragment()
+
+        fun openUploadScreen()
+
+        fun openPostScreen()
+
+        fun openCreateChatScreen()
+
+        fun openSearchScreen()
+
+        fun openInstalledScreen()
+
+        fun openDistroScreen()
+
+        fun openSettingsScreen()
+
+        fun openAboutScreen()
+
+        fun openAppScreen(appId: String, title: String)
+
+        fun openShareUrlDialog(text: String)
+
+        fun openBduiScreen(url: String, title: String?)
+
+        fun setBackCallbackEnabled(enabled: Boolean)
+
+        fun leaveScreen()
+
+        fun exitApp()
+
+        fun onTabReselected()
+
+        fun onCategoryMenuClicked()
+
+    }
+
+}
+
+class HomePresenterImpl(
+    startAction: String?,
+    private val bananalytics: Bananalytics,
+    private val interactor: HomeInteractor,
+    private val moderationProvider: ModerationProvider,
+    private val capabilitiesProvider: UserCapabilitiesProvider,
+    private val capabilitiesInteractor: UserCapabilitiesInteractor,
+    private val schedulers: SchedulersFactory,
+    state: Bundle?
+) : HomePresenter {
+
+    private var view: HomeView? = null
+    private var router: HomePresenter.HomeRouter? = null
+
+    private var action: String? = state?.getString(KEY_ACTION) ?: startAction
+    private var tabIndex: Int = state?.getInt(KEY_TAB_INDEX) ?: INDEX_STORE
+    private var startupLoaded: Boolean = state?.getBoolean(KEY_STARTUP_LOADED) == true
+    private var unread: Int = state?.getInt(KEY_UNREAD) ?: 0
+    private var feed: Int = state?.getInt(KEY_FEED) ?: 0
+    private var update: AppEntity? = state?.getParcelableCompat(KEY_UPDATE, AppEntity::class.java)
+    private var moderation: ModerationData? =
+        state?.getParcelableCompat(KEY_MODERATION, ModerationData::class.java)
+    private var status: StatusResponse? =
+        state?.getParcelableCompat(KEY_STATUS, StatusResponse::class.java)
+
+    private val subscriptions = CompositeDisposable()
+
+    override fun attachView(view: HomeView) {
+        this.view = view
+
+        subscriptions += view.storeClicks().subscribe { bindTab(index = INDEX_STORE) }
+        subscriptions += view.feedClicks().subscribe { bindTab(index = INDEX_FEED) }
+        subscriptions += view.discussClicks().subscribe { bindTab(index = INDEX_DISCUSS) }
+        subscriptions += view.profileClicks().subscribe { bindTab(index = INDEX_PROFILE) }
+        subscriptions += view.uploadClicks().subscribe {
+            onGuardedAction(CapabilityAction.APP_UPLOAD) { router?.openUploadScreen() }
+        }
+        subscriptions += view.postClicks().subscribe {
+            onGuardedAction(CapabilityAction.FEED_POST_CREATE) { router?.openPostScreen() }
+        }
+        subscriptions += view.createChatClicks().subscribe {
+            onGuardedAction(CapabilityAction.CHAT_TOPIC_CREATE) { router?.openCreateChatScreen() }
+        }
+        subscriptions += view.updateClicks().subscribe { onUpdateClicks() }
+        subscriptions += view.laterClicks().subscribe { onLaterClicks() }
+        subscriptions += view.searchClicks().subscribe { router?.openSearchScreen() }
+        subscriptions += view.categoryClicks().subscribe { router?.onCategoryMenuClicked() }
+        subscriptions += view.profileShareClicks().subscribe { onShareClicks() }
+        subscriptions += view.installedClicks().subscribe { router?.openInstalledScreen() }
+        subscriptions += view.distroClicks().subscribe { router?.openDistroScreen() }
+        subscriptions += view.settingsClicks().subscribe { router?.openSettingsScreen() }
+        subscriptions += view.aboutClicks().subscribe { router?.openAboutScreen() }
+        subscriptions += view.exitAppClicks().subscribe { router?.exitApp() }
+        subscriptions += view.tabReselectClicks().subscribe { router?.onTabReselected() }
+
+        subscriptions += moderationProvider.observeModerationData()
+            .observeOn(schedulers.mainThread())
+            .subscribe { data ->
+                moderation = data
+                bindModeration()
+            }
+
+        if (startupLoaded) {
+            bindUnread()
+            bindFeed()
+            bindUpdate()
+            bindModeration()
+            bindTab()
+        } else {
+            loadStartup()
+        }
+
+        loadCapabilities()
+
+        val status = status
+        if (status != null) {
+            onStatusLoaded(status)
+        } else {
+            loadStatus()
+        }
+    }
+
+    private fun bindTab(index: Int = tabIndex) {
+        tabIndex = index
+        val tabName = when (index) {
+            INDEX_STORE -> "Store"
+            INDEX_FEED -> "Feed"
+            INDEX_DISCUSS -> "Discuss"
+            INDEX_PROFILE -> "Profile"
+            else -> "Unknown"
+        }
+        bananalytics.leaveBreadcrumb("Tab: $tabName", BreadcrumbCategory.USER_ACTION)
+
+        router?.setBackCallbackEnabled(index != INDEX_STORE)
+        when (index) {
+            INDEX_STORE -> {
+                router?.showStoreFragment()
+                view?.showStoreToolbar()
+                view?.hideFabButtons()
+                view?.showUploadButton()
+            }
+
+            INDEX_FEED -> {
+                router?.showFeedFragment()
+                view?.showFeedToolbar()
+                view?.hideFabButtons()
+                view?.showPostButton()
+                bindFeed(count = 0)
+            }
+
+            INDEX_DISCUSS -> {
+                router?.showTopicsFragment()
+                view?.showDiscussToolbar()
+                view?.hideFabButtons()
+
+                if (CapabilityPolicy.isAllowed(
+                        action = CapabilityAction.CHAT_TOPIC_CREATE,
+                        capabilities = capabilitiesProvider.getCapabilities(),
+                        allowOnUnknown = false,
+                    )
+                ) {
+                    view?.showCreateChatButton()
+                }
+                bindUnread(count = 0)
+            }
+
+            INDEX_PROFILE -> {
+                router?.showProfileFragment()
+                view?.showProfileToolbar()
+                view?.hideFabButtons()
+            }
+        }
+    }
+
+    private fun loadStartup() {
+        subscriptions += interactor.loadStartup()
+            .observeOn(schedulers.mainThread())
+            .subscribe(
+                { onStartupLoaded(response = it) },
+                { }
+            )
+    }
+
+    private fun loadStatus() {
+        subscriptions += interactor.loadStatus()
+            .observeOn(schedulers.mainThread())
+            .subscribe(
+                { onStatusLoaded(response = it) },
+                { }
+            )
+    }
+
+    private fun loadCapabilities() {
+        subscriptions += capabilitiesInteractor.refresh()
+            .observeOn(schedulers.mainThread())
+            .subscribe(
+                {
+
+                    bindTab()
+                },
+                {  },
+            )
+    }
+
+    private fun onGuardedAction(action: String, onAllowed: () -> Unit) {
+        when (val result =
+            CapabilityPolicy.check(action, capabilitiesProvider.getCapabilities())) {
+            CapabilityResult.Allowed,
+            CapabilityResult.Unknown -> onAllowed()
+
+            is CapabilityResult.Denied -> view?.showCapabilityDenied(result.capability)
+        }
+    }
+
+    private fun onStartupLoaded(response: StartupResponse) {
+        startupLoaded = true
+        unread = response.unread
+        feed = response.feed
+        update = response.update
+        moderation = response.moderation
+
+        moderationProvider.setModerationData(response.moderation)
+
+        bindUnread()
+        bindFeed()
+        bindUpdate()
+        bindModeration()
+        bindTab()
+
+        response.bdui?.let { bdui ->
+            router?.openBduiScreen(bdui.url, bdui.title)
+        }
+    }
+
+    private fun onStatusLoaded(response: StatusResponse) {
+        status = response
+
+        response.bdui?.let { bdui ->
+            router?.openBduiScreen(bdui.url, bdui.title)
+        }
+    }
+
+    private fun bindUnread(count: Int = unread) {
+        unread = count
+        if (unread > 0) {
+            view?.showUnreadBadge(count)
+        } else {
+            view?.hideUnreadBadge()
+        }
+    }
+
+    private fun bindFeed(count: Int = feed) {
+        feed = count
+        if (feed > 0) {
+            view?.showFeedBadge(count)
+        } else {
+            view?.hideFeedBadge()
+        }
+    }
+
+    private fun bindModeration() {
+        val moderationData = moderation
+        if (moderationData != null && moderationData.moderator && moderationData.count > 0) {
+            view?.showModerationBadge(moderationData.count)
+        } else {
+            view?.hideModerationBadge()
+        }
+    }
+
+    private fun bindUpdate() {
+        view?.hideUpdateBlock()
+    }
+
+    private fun onUpdateClicks() {
+        val update = update ?: return
+        router?.openAppScreen(appId = update.appId, title = update.title)
+    }
+
+    private fun onLaterClicks() {
+        update = null
+        view?.hideUpdateBlock()
+    }
+
+    private fun onShareClicks() {
+        subscriptions += interactor.getUserBrief()
+            .observeOn(schedulers.mainThread())
+            .subscribe(
+                { it.url?.let { url -> router?.openShareUrlDialog(url) } },
+                { }
+            )
+    }
+
+    override fun detachView() {
+        this.view = null
+    }
+
+    override fun attachRouter(router: HomePresenter.HomeRouter) {
+        this.router = router
+        handleAction()
+        bindTab()
+    }
+
+    override fun detachRouter() {
+        this.router = null
+    }
+
+    override fun saveState(): Bundle = Bundle().apply {
+        putString(KEY_ACTION, action)
+        putInt(KEY_TAB_INDEX, tabIndex)
+        putBoolean(KEY_STARTUP_LOADED, startupLoaded)
+        putInt(KEY_UNREAD, unread)
+        putInt(KEY_FEED, feed)
+        putParcelable(KEY_UPDATE, update)
+        putParcelable(KEY_MODERATION, moderation)
+        putParcelable(KEY_STATUS, status)
+    }
+
+    override fun onBackPressed() {
+        if (tabIndex != INDEX_STORE) {
+            view?.selectStoreTab()
+        } else {
+            router?.leaveScreen()
+        }
+    }
+
+    private fun handleAction() {
+        when (action) {
+            ACTION_STORE -> view?.selectStoreTab()
+            ACTION_DISCUSS -> view?.selectDiscussTab()
+            ACTION_APPS -> router?.openInstalledScreen()
+            ACTION_INSTALL -> router?.openDistroScreen()
+        }
+        action = ""
+    }
+
+}
+
+private const val KEY_ACTION = "action"
+private const val KEY_TAB_INDEX = "current_tab"
+private const val KEY_STARTUP_LOADED = "startup_loaded"
+private const val KEY_UNREAD = "unread"
+private const val KEY_FEED = "feed"
+private const val KEY_UPDATE = "update"
+private const val KEY_MODERATION = "moderation"
+private const val KEY_STATUS = "status"
+private const val INDEX_STORE = 0
+private const val INDEX_FEED = 1
+private const val INDEX_DISCUSS = 2
+private const val INDEX_PROFILE = 3
+
+const val ACTION_STORE = "com.hiaashuu.appteka.cloud"
+const val ACTION_DISCUSS = "com.hiaashuu.appteka.discuss"
+const val ACTION_APPS = "com.hiaashuu.appteka.apps"
+const val ACTION_INSTALL = "com.hiaashuu.appteka.install"
