236aa225cd7a1a6e0beee782d9a55248de210887
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:22:49 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/gallery/GalleryResourceProvider.kt
@@ -0,0 +1,28 @@
+package com.hiaashuu.appteka.screen.gallery
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+import java.util.Locale
+
+interface GalleryResourceProvider {
+
+    fun formatTitle(current: Int, total: Int): String
+
+    fun errorSavingScreenshot(): String
+
+}
+
+class GalleryResourceProviderImpl(
+    val resources: Resources,
+    val locale: Locale,
+) : GalleryResourceProvider {
+
+    override fun formatTitle(current: Int, total: Int): String {
+        return resources.getString(R.string.gallery_title, current, total)
+    }
+
+    override fun errorSavingScreenshot(): String {
+        return resources.getString(R.string.error_saving_screenshot)
+    }
+
+}
