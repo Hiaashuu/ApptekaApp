9aef52a452e8ed129d2d5c49c03a434e2aca8925
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:26:50 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/adapter/uploads/UploadsItemPresenter.kt
@@ -0,0 +1,39 @@
+package com.hiaashuu.appteka.screen.profile.adapter.uploads
+
+import com.hiaashuu.appteka.util.adapter.AdapterPresenter
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.ItemListener
+import com.hiaashuu.appteka.screen.profile.adapter.app.AppItem
+
+class UploadsItemPresenter(
+    private val listener: ItemListener,
+    private val adapterPresenter: dagger.Lazy<AdapterPresenter>,
+) : ItemPresenter<UploadsItemView, UploadsItem>, AppItemListener {
+
+    private var uploads = emptyList<AppItem>()
+
+    override fun bindView(view: UploadsItemView, item: UploadsItem, position: Int) {
+        view.setUploadsCount(item.uploads.toString())
+        view.setDownloadsCount(item.downloads.toString())
+        view.setOnClickListener { listener.onUploadsClick(item.userId) }
+        view.setOnNextPageListener {
+            listener.onNextPage(uploads.last()) { items ->
+                uploads = uploads + items
+                onUploadsChanged(view)
+            }
+        }
+
+        uploads = item.items
+        onUploadsChanged(view)
+    }
+
+    private fun onUploadsChanged(view: UploadsItemView) {
+        adapterPresenter.get().onDataSourceChanged(uploads)
+        view.notifyChanged()
+    }
+
+    override fun onAppClick(app: AppItem) {
+        listener.onAppClick(app.appId, app.title)
+    }
+
+}
