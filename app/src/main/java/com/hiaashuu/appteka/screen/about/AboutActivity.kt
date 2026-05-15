b1c371c0163ad568540e5bb322588c12620929e1
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:15:53 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/about/AboutActivity.kt
@@ -0,0 +1,112 @@
+package com.hiaashuu.appteka.screen.about
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.hiaashuu.appteka.appComponent
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.screen.about.di.AboutModule
+import com.hiaashuu.appteka.util.Analytics
+import javax.inject.Inject
+import androidx.core.net.toUri
+
+class AboutActivity : AppCompatActivity(), AboutPresenter.AboutRouter {
+
+    @Inject
+    lateinit var presenter: AboutPresenter
+
+    @Inject
+    lateinit var analytics: Analytics
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val presenterState = savedInstanceState?.getBundle(KEY_PRESENTER_STATE)
+        appComponent
+            .aboutComponent(AboutModule(context = this, presenterState))
+            .inject(activity = this)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_activity)
+
+        val view = AboutViewImpl(window.decorView)
+
+        presenter.attachView(view)
+
+        if (savedInstanceState == null) {
+            analytics.trackEvent("open-about-screen")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachRouter(this)
+    }
+
+    override fun onStop() {
+        presenter.detachRouter()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        presenter.detachView()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBundle(KEY_PRESENTER_STATE, presenter.saveState())
+    }
+
+    override fun openFeedbackEmail(addr: String, subject: String, text: String) {
+        val uri = Uri.fromParts("mailto", addr, null)
+        val intent = Intent(Intent.ACTION_SENDTO, uri)
+            .putExtra(Intent.EXTRA_SUBJECT, subject)
+            .putExtra(Intent.EXTRA_TEXT, text)
+        try {
+            startActivity(Intent.createChooser(intent, getString(R.string.send_email)))
+        } catch (ex: Throwable) {
+            Toast.makeText(this, getString(R.string.no_email_clients), Toast.LENGTH_SHORT).show()
+        }
+        analytics.trackEvent("click-email-feedback")
+    }
+
+    override fun openSourceCodeLink() {
+        openUrl(url = "https://github.com/solkin/appteka-android")
+        analytics.trackEvent("click-source-code")
+    }
+
+    override fun openTelegramGroupLink() {
+        openUrl(url = getString(R.string.telegram_group_url))
+        analytics.trackEvent("click-telegram-group")
+    }
+
+    override fun openLegalInfoLink() {
+        openUrl(url = getString(R.string.legal_info_url))
+        analytics.trackEvent("click-legal-info")
+    }
+
+    override fun openContributorLink(url: String) {
+        openUrl(url = url)
+        analytics.trackEvent("click-contributor")
+    }
+
+    override fun leaveScreen() {
+        finish()
+    }
+
+    private fun openUrl(url: String) {
+        try {
+            startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
+        } catch (ignored: Throwable) {
+        }
+    }
+
+}
+
+fun createAboutActivityIntent(
+    context: Context,
+): Intent = Intent(context, AboutActivity::class.java)
+
+private const val KEY_PRESENTER_STATE = "presenter_state"
