93c2c94fbbcde19b2751c6fa7d317e1b8165aa6c
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:27:56 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/reviews/ReviewsActivity.kt
@@ -0,0 +1,107 @@
+package com.hiaashuu.appteka.screen.reviews
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
+import com.hiaashuu.appteka.util.adapter.ItemBinder
+import com.hiaashuu.appteka.util.adapter.AdapterPresenter
+import com.hiaashuu.appteka.util.adapter.SimpleRecyclerAdapter
+import com.hiaashuu.appteka.appComponent
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.screen.details.createDetailsActivityIntent
+import com.hiaashuu.appteka.screen.reviews.di.ReviewsModule
+import com.hiaashuu.appteka.util.Analytics
+import com.hiaashuu.appteka.util.ZipParcelable
+import com.hiaashuu.appteka.util.getParcelableCompat
+import javax.inject.Inject
+
+class ReviewsActivity : AppCompatActivity(), ReviewsPresenter.ReviewsRouter {
+
+    @Inject
+    lateinit var presenter: ReviewsPresenter
+
+    @Inject
+    lateinit var adapterPresenter: AdapterPresenter
+
+    @Inject
+    lateinit var binder: ItemBinder
+
+    @Inject
+    lateinit var analytics: Analytics
+
+    private val invalidateDetailsResultLauncher =
+        registerForActivityResult(StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                presenter.invalidateApps()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val userId = intent.getIntExtra(EXTRA_USER_ID, 0)
+        val presenterState = savedInstanceState
+            ?.getParcelableCompat(KEY_PRESENTER_STATE, ZipParcelable::class.java)
+            ?.restore<Bundle>()
+        appComponent
+            .reviewsComponent(ReviewsModule(this, userId, presenterState))
+            .inject(activity = this)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reviews_activity)
+
+        val adapter = SimpleRecyclerAdapter(adapterPresenter, binder)
+        val view = ReviewsViewImpl(window.decorView, adapter)
+
+        presenter.attachView(view)
+
+        if (savedInstanceState == null) {
+            analytics.trackEvent("open-reviews-screen")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachRouter(this)
+    }
+
+    override fun onStop() {
+        presenter.detachRouter()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        presenter.detachView()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(KEY_PRESENTER_STATE, ZipParcelable(presenter.saveState()))
+    }
+
+    override fun openAppScreen(appId: String, title: String) {
+        val intent = createDetailsActivityIntent(
+            context = this,
+            appId = appId,
+            label = title,
+            moderation = false,
+            finishOnly = true
+        )
+        invalidateDetailsResultLauncher.launch(intent)
+    }
+
+    override fun leaveScreen() {
+        finish()
+    }
+
+}
+
+fun createReviewsActivityIntent(
+    context: Context,
+    userId: Int
+): Intent = Intent(context, ReviewsActivity::class.java)
+    .putExtra(EXTRA_USER_ID, userId)
+
+private const val EXTRA_USER_ID = "user_id"
+private const val KEY_PRESENTER_STATE = "presenter_state"
