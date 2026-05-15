adf76a3274058c58353e682e26735cc5a8740e78
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:15:51 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/about/AboutResourceProvider.kt
@@ -0,0 +1,50 @@
+package com.hiaashuu.appteka.screen.about
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.core.AppInfoProvider
+
+data class Contributor(
+    val name: String,
+    val url: String
+)
+
+interface AboutResourceProvider {
+
+    fun getAppVersion(): String
+
+    fun getContributors(): List<Contributor>
+
+}
+
+class AboutResourceProviderImpl(
+    private val infoProvider: AppInfoProvider,
+    private val resources: Resources,
+) : AboutResourceProvider {
+
+    override fun getAppVersion(): String {
+        return resources.getString(
+            R.string.app_version,
+            infoProvider.getVersionName(),
+            infoProvider.getVersionCode()
+        )
+    }
+
+    override fun getContributors(): List<Contributor> {
+        return listOf(
+            Contributor(
+                name = "Igor Solkin",
+                url = "https://github.com/solkin"
+            ),
+            Contributor(
+                name = "Ameer Muawiya",
+                url = "https://github.com/ameermuawiya"
+            ),
+            Contributor(
+                name = "tojik_proof_93",
+                url = "https://github.com/FlutterGenerator"
+            )
+        )
+    }
+
+}
