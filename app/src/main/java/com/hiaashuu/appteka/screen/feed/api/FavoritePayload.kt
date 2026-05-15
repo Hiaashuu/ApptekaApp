74013b245acf91b392df21bb3eef0534d574ae44
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:22:27 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/feed/api/FavoritePayload.kt
@@ -0,0 +1,40 @@
+package com.hiaashuu.appteka.screen.feed.api
+
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.categories.Category
+import com.hiaashuu.appteka.dto.Screenshot
+import com.hiaashuu.appteka.util.GsonModel
+
+@GsonModel
+data class FavoritePayload(
+    @SerializedName("app_id")
+    val appId: String,
+    @SerializedName("package")
+    val packageName: String,
+    @SerializedName("icon")
+    val icon: String?,
+    @SerializedName("label")
+    val title: String,
+    @SerializedName("ver_name")
+    val verName: String,
+    @SerializedName("ver_code")
+    val verCode: Int,
+    @SerializedName("size")
+    val size: Long,
+    @SerializedName("rating")
+    val rating: Float,
+    @SerializedName("downloads")
+    val downloads: Int,
+    @SerializedName("file_status")
+    val status: Int,
+    @SerializedName("category")
+    val category: Category?,
+    @SerializedName("exclusive")
+    val exclusive: Boolean,
+    @SerializedName("open_source")
+    val openSource: Boolean,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("screenshots")
+    val screenshots: List<Screenshot>?,
+): PostPayload
