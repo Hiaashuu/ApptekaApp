8e2a6acf591eca9b32a78f35cdffd8bc0190c768
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:18 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/user_review/UserReviewItemView.kt
@@ -0,0 +1,85 @@
+package com.hiaashuu.appteka.screen.details.adapter.user_review
+
+import android.view.View
+import android.widget.RatingBar
+import android.widget.TextView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.dto.BadgeMark
+import com.hiaashuu.appteka.dto.UserIcon
+import com.hiaashuu.appteka.util.bind
+import com.hiaashuu.appteka.view.UserIconView
+import com.hiaashuu.appteka.view.UserIconViewImpl
+
+interface UserReviewItemView : ItemView {
+
+    fun setMemberName(name: String)
+
+    fun setMemberIcon(userIcon: UserIcon)
+
+    fun setMemberBadge(badge: BadgeMark?)
+
+    fun setRating(value: Float)
+
+    fun setDate(date: String)
+
+    fun setReview(text: String?)
+
+    fun setOnEditListener(listener: (() -> Unit)?)
+
+}
+
+class UserReviewItemViewHolder(view: View) : BaseItemViewHolder(view), UserReviewItemView {
+
+    private val memberIcon: UserIconView = UserIconViewImpl(view.findViewById(R.id.member_icon))
+    private val memberName: TextView = view.findViewById(R.id.member_name)
+    private val ratingView: RatingBar = view.findViewById(R.id.rating_view)
+    private val dateView: TextView = view.findViewById(R.id.date_view)
+    private val reviewView: TextView = view.findViewById(R.id.review_view)
+    private val feedbackButton: View = view.findViewById(R.id.feedback_button)
+
+    private var editListener: (() -> Unit)? = null
+
+    init {
+        feedbackButton.setOnClickListener { editListener?.invoke() }
+        ratingView.setOnRatingBarChangeListener { _, _, fromUser ->
+            if (fromUser) {
+                editListener?.invoke()
+            }
+        }
+    }
+
+    override fun setMemberIcon(userIcon: UserIcon) {
+        this.memberIcon.bind(userIcon)
+    }
+
+    override fun setMemberBadge(badge: BadgeMark?) {
+        this.memberIcon.bindBadge(badge)
+    }
+
+    override fun setMemberName(name: String) {
+        this.memberName.bind(name)
+    }
+
+    override fun setRating(value: Float) {
+        ratingView.rating = value
+    }
+
+    override fun setDate(date: String) {
+        dateView.bind(date)
+    }
+
+    override fun setReview(text: String?) {
+        reviewView.bind(text)
+    }
+
+    override fun setOnEditListener(listener: (() -> Unit)?) {
+        this.editListener = listener
+    }
+
+    override fun onUnbind() {
+        this.editListener = null
+    }
+
+}
