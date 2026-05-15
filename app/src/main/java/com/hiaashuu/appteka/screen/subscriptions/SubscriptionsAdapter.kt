08747d26f8606b8aa6e3bf88c361410bf19c14d8
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:29:08 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/subscriptions/SubscriptionsAdapter.kt
@@ -0,0 +1,38 @@
+package com.hiaashuu.appteka.screen.subscriptions
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.screen.profile.createProfileFragment
+import com.hiaashuu.appteka.screen.users.UsersType
+import com.hiaashuu.appteka.screen.users.createUsersFragment
+
+class SubscriptionsAdapter internal constructor(
+    fm: FragmentManager,
+    lifecycle: Lifecycle,
+    val userId: Int,
+) : FragmentStateAdapter(fm, lifecycle) {
+
+    fun getItemTitle(position: Int): Int {
+        return when (position) {
+            0 -> R.string.subscribers
+            1 -> R.string.publishers
+            else -> throw Exception("Invalid item index")
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        val fragment = when (position) {
+            0 -> createUsersFragment(userId, UsersType.SUBSCRIBERS)
+            1 -> createUsersFragment(userId, UsersType.PUBLISHERS)
+            else -> throw IllegalStateException("Invalid fragment index")
+        }
+        return fragment
+    }
+}
