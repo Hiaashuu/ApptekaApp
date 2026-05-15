8ccc0b1895cede33d0c2fa0df60da6a234ea8e0d
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:17:23 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/chat/ChatResourceProvider.kt
@@ -0,0 +1,36 @@
+package com.hiaashuu.appteka.screen.chat
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+
+interface ChatResourceProvider {
+
+    fun unsupportedMessageText(): String
+
+    fun commonQuestionsTopicTitle(): String
+
+    fun commonQuestionsTopicDescription(): String
+
+    fun chatCreatedMessage(): String
+
+}
+
+class ChatResourceProviderImpl(val resources: Resources) : ChatResourceProvider {
+
+    override fun unsupportedMessageText(): String {
+        return resources.getString(R.string.unsupported_message)
+    }
+
+    override fun commonQuestionsTopicTitle(): String {
+        return resources.getString(R.string.topic_common_qna_title)
+    }
+
+    override fun commonQuestionsTopicDescription(): String {
+        return resources.getString(R.string.topic_common_qna_description)
+    }
+
+    override fun chatCreatedMessage(): String {
+        return resources.getString(R.string.system_message_chat_created)
+    }
+
+}
