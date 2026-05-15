1f1ce7168af49acfcf831fac1fb01bc580862497
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:35 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/installed/adapter/app/AppItemPresenter.kt
@@ -0,0 +1,35 @@
+package com.hiaashuu.appteka.screen.installed.adapter.app
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.installed.AppsResourceProvider
+import com.hiaashuu.appteka.screen.installed.adapter.ItemListener
+
+class AppItemPresenter(
+    private val listener: ItemListener,
+    private val resourceProvider: AppsResourceProvider
+) : ItemPresenter<AppItemView, AppItem> {
+
+    override fun bindView(view: AppItemView, item: AppItem, position: Int) {
+        view.setIcon(item.icon)
+        view.setTitle(item.title)
+        view.setVersion(item.version)
+        view.setSize(resourceProvider.formatFileSize(item.size))
+        view.setUpdateTime(
+            if (item.updateTime == item.installTime) {
+                resourceProvider.installDate(item.updateTime)
+            } else {
+                resourceProvider.updateDate(item.updateTime)
+            }
+        )
+        view.setUpdatable(item.updateAppId != null)
+        if (item.isNew) view.showBadge() else view.hideBadge()
+        view.setOnClickListener { listener.onItemClick(item) }
+        view.setOnUpdateClickListener {
+            listener.onUpdateClick(
+                item.title,
+                item.updateAppId.orEmpty()
+            )
+        }
+    }
+
+}
