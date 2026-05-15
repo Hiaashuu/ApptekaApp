3f60a2b34ebbdd431d71f9c79556df89246eb5a7
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:17:11 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/chat/adapter/MsgAttachment.kt
@@ -0,0 +1,13 @@
+package com.hiaashuu.appteka.screen.chat.adapter
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class MsgAttachment(
+    val previewUrl: String,
+    val originalUrl: String,
+    val size: Long,
+    val width: Int,
+    val height: Int
+) : Parcelable
