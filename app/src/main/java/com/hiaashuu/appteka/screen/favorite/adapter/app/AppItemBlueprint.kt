82e5d5140a60f21acaa6833323abae470c10b535
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:21:00 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/favorite/adapter/app/AppItemBlueprint.kt
@@ -0,0 +1,19 @@
+package com.hiaashuu.appteka.screen.favorite.adapter.app
+
+import com.hiaashuu.appteka.util.adapter.Item
+import com.hiaashuu.appteka.util.adapter.ItemBlueprint
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.util.adapter.ViewHolderBuilder
+import com.hiaashuu.appteka.R
+
+class AppItemBlueprint(override val presenter: ItemPresenter<AppItemView, AppItem>) :
+    ItemBlueprint<AppItemView, AppItem> {
+
+    override val viewHolderProvider = ViewHolderBuilder.ViewHolderProvider(
+        layoutId = R.layout.store_item,
+        creator = { _, view -> AppItemViewHolder(view) }
+    )
+
+    override fun isRelevantItem(item: Item) = item is AppItem
+
+}
