41535f63a16a78b88069875b6bc810fa19f96e06
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:26:33 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/adapter/reviews/ReviewItemListener.kt
@@ -0,0 +1,9 @@
+package com.hiaashuu.appteka.screen.profile.adapter.reviews
+
+import com.hiaashuu.appteka.screen.profile.adapter.review.ReviewItem
+
+interface ReviewItemListener {
+
+    fun onRatingClick(item: ReviewItem)
+
+}
