08e22320a535005308ef823d4f0103f571f69ba0
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:25:55 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/adapter/downloads/DownloadsItemView.kt
@@ -0,0 +1,47 @@
+package com.hiaashuu.appteka.screen.profile.adapter.downloads
+
+import android.view.View
+import android.widget.TextView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.bind
+
+interface DownloadsItemView : ItemView {
+
+    fun setCount(count: Int)
+
+    fun setOnClickListener(listener: (() -> Unit)?)
+
+}
+
+class DownloadsItemViewHolder(view: View) : BaseItemViewHolder(view), DownloadsItemView {
+
+    private val context = view.context
+    private val subtitle: TextView = view.findViewById(R.id.subtitle)
+
+    private var clickListener: (() -> Unit)? = null
+
+    init {
+        view.setOnClickListener { clickListener?.invoke() }
+    }
+
+    override fun setCount(count: Int) {
+        this.subtitle.bind(
+            context.resources.getQuantityString(
+                R.plurals.download_apps_count,
+                count,
+                count
+            )
+        )
+    }
+
+    override fun setOnClickListener(listener: (() -> Unit)?) {
+        this.clickListener = listener
+    }
+
+    override fun onUnbind() {
+        this.clickListener = null
+    }
+
+}
