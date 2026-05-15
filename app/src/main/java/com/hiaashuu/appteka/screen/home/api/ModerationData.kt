ad652d36014c3af5a767ebc65a8e72a0b915ac1d
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:05 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/home/api/ModerationData.kt
@@ -0,0 +1,15 @@
+package com.hiaashuu.appteka.screen.home.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@GsonModel
+data class ModerationData(
+    @SerializedName("moderator")
+    val moderator: Boolean,
+    @SerializedName("count")
+    val count: Int,
+) : Parcelable
