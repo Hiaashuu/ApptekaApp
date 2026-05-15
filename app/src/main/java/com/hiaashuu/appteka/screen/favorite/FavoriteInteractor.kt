99ef4ce4c9a14f6e596578bf5638f70eba2a91e6
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:20:55 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/favorite/FavoriteInteractor.kt
@@ -0,0 +1,46 @@
+package com.hiaashuu.appteka.screen.favorite
+
+import com.hiaashuu.appteka.core.StoreApi
+import com.hiaashuu.appteka.dto.AppEntity
+import com.hiaashuu.appteka.util.SchedulersFactory
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
+import java.util.Locale
+
+interface FavoriteInteractor {
+
+    fun listApps(offsetAppId: String? = null): Observable<List<AppEntity>>
+
+    fun markFavorite(appId: String, isFavorite: Boolean): Completable
+
+}
+
+class FavoriteInteractorImpl(
+    private val api: StoreApi,
+    private val userId: Int,
+    private val locale: Locale,
+    private val schedulers: SchedulersFactory
+) : FavoriteInteractor {
+
+    override fun listApps(offsetAppId: String?): Observable<List<AppEntity>> {
+        return api
+            .getFavoriteList(
+                userId = userId,
+                appId = offsetAppId,
+                locale = locale.language
+            )
+            .map { list ->
+                list.result.files
+            }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun markFavorite(appId: String, isFavorite: Boolean): Completable {
+        return api
+            .markFavorite(appId = appId, isFavorite = isFavorite)
+            .ignoreElement()
+            .subscribeOn(schedulers.io())
+    }
+
+}
