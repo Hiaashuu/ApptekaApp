882abb0c625d4784ce9bdf4110b0911ce2850c8f
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:28:00 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/reviews/adapter/review/ReviewItem.kt
@@ -0,0 +1,22 @@
+package com.hiaashuu.appteka.screen.reviews.adapter.review
+
+import android.os.Parcelable
+import com.hiaashuu.appteka.util.adapter.Item
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class ReviewItem(
+    override val id: Long,
+    val appId: String,
+    val rateId: Int,
+    val icon: String?,
+    val title: String,
+    val version: String,
+    val rating: Float,
+    val text: String?,
+    val time: Long,
+    val showRatingMenu: Boolean,
+    var hasMore: Boolean = false,
+    var hasError: Boolean = false,
+    var hasProgress: Boolean = false,
+) : Item, Parcelable
