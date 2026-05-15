baf12c6592d11af930678a02b0f1d5cc278d2a7c
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:15:21 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/core/UserAgentProvider.kt
@@ -0,0 +1,32 @@
+package com.hiaashuu.appteka.core
+
+import android.os.Build
+import java.util.Locale
+
+interface UserAgentProvider {
+
+    fun getUserAgent(): String
+
+}
+
+class UserAgentProviderImpl(
+    private val appInfoProvider: AppInfoProvider,
+    private val locale: Locale
+) : UserAgentProvider {
+
+    override fun getUserAgent(): String {
+        return String.format(
+            Locale.ENGLISH,
+            "Appteka/%s.%d (%s; %s; sdk:%d; %s; %s-%s)",
+            appInfoProvider.getVersionName(),
+            appInfoProvider.getVersionCode(),
+            Build.MANUFACTURER,
+            Build.MODEL,
+            Build.VERSION.SDK_INT,
+            appInfoProvider.getPackageName(),
+            locale.language,
+            locale.country
+        )
+    }
+
+}
