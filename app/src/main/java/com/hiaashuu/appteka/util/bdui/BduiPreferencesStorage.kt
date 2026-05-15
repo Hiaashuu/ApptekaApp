3d58cf96576c0c839926c92e13f99a006a152f4e
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:35:39 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/bdui/BduiPreferencesStorage.kt
@@ -0,0 +1,46 @@
+package com.hiaashuu.appteka.util.bdui
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+
+interface BduiPreferencesStorage {
+
+    fun store(key: String, value: Any?)
+
+    fun load(key: String, defaultValue: Any?): Any?
+
+}
+
+class BduiPreferencesStorageImpl(
+    private val preferences: SharedPreferences,
+    private val gson: Gson
+) : BduiPreferencesStorage {
+
+    override fun store(key: String, value: Any?) {
+        val prefKey = BDUI_PREF_PREFIX + key
+        preferences.edit().apply {
+            when (value) {
+                null -> remove(prefKey)
+                is Boolean -> putBoolean(prefKey, value)
+                is Int -> putInt(prefKey, value)
+                is Long -> putLong(prefKey, value)
+                is Float -> putFloat(prefKey, value)
+                is String -> putString(prefKey, value)
+                else -> putString(prefKey, gson.toJson(value))
+            }
+        }.apply()
+    }
+
+    override fun load(key: String, defaultValue: Any?): Any? {
+        val prefKey = BDUI_PREF_PREFIX + key
+        return if (preferences.contains(prefKey)) {
+            preferences.all[prefKey]
+        } else {
+            defaultValue
+        }
+    }
+
+    companion object {
+        private const val BDUI_PREF_PREFIX = "bdui_"
+    }
+}
