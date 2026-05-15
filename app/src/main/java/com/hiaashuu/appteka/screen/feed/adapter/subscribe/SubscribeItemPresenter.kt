d60be1b45c575f23437d7cc5ac0865228681712a
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:21:59 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/feed/adapter/subscribe/SubscribeItemPresenter.kt
@@ -0,0 +1,49 @@
+package com.hiaashuu.appteka.screen.feed.adapter.subscribe
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.categories.DEFAULT_LOCALE
+import com.hiaashuu.appteka.screen.feed.FeedResourceProvider
+import com.hiaashuu.appteka.screen.feed.adapter.ItemListener
+import com.hiaashuu.appteka.user.api.UserBrief
+import java.util.Locale
+
+class SubscribeItemPresenter(
+    private val locale: Locale,
+    private val resourceProvider: FeedResourceProvider,
+    private val listener: ItemListener,
+) : ItemPresenter<SubscribeItemView, SubscribeItem> {
+
+    override fun bindView(view: SubscribeItemView, item: SubscribeItem, position: Int) {
+        with(item) {
+            if (hasMore) {
+                hasMore = false
+                hasProgress = true
+                listener.onLoadMore(this)
+            }
+        }
+
+        view.setUserName(item.user.name())
+        view.setUserIcon(item.user.icon)
+        view.setUserBadge(item.user.primaryBadge)
+        view.setPublisherName(item.publisher.name())
+        view.setPublisherIcon(item.publisher.icon)
+        view.setPublisherBadge(item.publisher.primaryBadge)
+        view.setTime(resourceProvider.formatTime(item.time))
+        if (item.hasProgress) view.showProgress() else view.hideProgress()
+        if (!item.actions.isNullOrEmpty()) view.showMenu() else view.hideMenu()
+        item.reacts.takeIf { !it.isNullOrEmpty() }
+            ?.let { view.setReactions(it) }
+            ?: view.hideReactions()
+        view.setOnPostClickListener { listener.onItemClick(item) }
+        view.setOnPublisherClickListener { listener.onUserClick(item.publisher) }
+        view.setOnMenuClickListener { listener.onMenuClick(item) }
+        view.setOnReactionClickListener { reaction -> listener.onReactionClick(item, reaction) }
+    }
+
+    private fun UserBrief.name(): String {
+        return name.takeIf { !it.isNullOrBlank() }
+            ?: icon.label?.get(locale.language)
+            ?: icon.label?.get(DEFAULT_LOCALE).orEmpty()
+    }
+
+}
