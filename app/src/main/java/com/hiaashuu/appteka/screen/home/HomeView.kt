bd10e546359dfc7bb3f61dace8aa5f51c7a5c43e
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:19 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/home/HomeView.kt
@@ -0,0 +1,358 @@
+package com.hiaashuu.appteka.screen.home
+
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.jakewharton.rxrelay3.PublishRelay
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.core.permissions.Capability
+import com.hiaashuu.appteka.core.permissions.CapabilityHintResolver
+import com.hiaashuu.appteka.util.applyBottomInsets
+import com.hiaashuu.appteka.util.applyBottomInsetsAsMargin
+import com.hiaashuu.appteka.util.applyBottomMarginForView
+import com.hiaashuu.appteka.util.applyTopInsets
+import com.hiaashuu.appteka.util.clicks
+import com.hiaashuu.appteka.util.hide
+import com.hiaashuu.appteka.util.show
+import io.reactivex.rxjava3.core.Observable
+
+interface HomeView {
+
+    fun showStoreToolbar()
+
+    fun showFeedToolbar()
+
+    fun showDiscussToolbar()
+
+    fun showProfileToolbar()
+
+    fun selectStoreTab()
+
+    fun selectDiscussTab()
+
+    fun selectProfileTab()
+
+    fun showUnreadBadge(count: Int)
+
+    fun showFeedBadge(count: Int)
+
+    fun showModerationBadge(count: Int)
+
+    fun hideUnreadBadge()
+
+    fun hideFeedBadge()
+
+    fun hideModerationBadge()
+
+    fun showUpdateBlock()
+
+    fun hideUpdateBlock()
+
+    fun showUploadButton()
+
+    fun showPostButton()
+
+    fun showCreateChatButton()
+
+    fun hideFabButtons()
+
+    fun showStatusDialog(block: Boolean, title: String?, message: String)
+
+    fun showCapabilityDenied(
+        capability: com.hiaashuu.appteka.core.permissions.Capability,
+    )
+
+    fun storeClicks(): Observable<Unit>
+
+    fun feedClicks(): Observable<Unit>
+
+    fun discussClicks(): Observable<Unit>
+
+    fun profileClicks(): Observable<Unit>
+
+    fun uploadClicks(): Observable<Unit>
+
+    fun postClicks(): Observable<Unit>
+
+    fun createChatClicks(): Observable<Unit>
+
+    fun updateClicks(): Observable<Unit>
+
+    fun laterClicks(): Observable<Unit>
+
+    fun searchClicks(): Observable<Unit>
+
+    fun categoryClicks(): Observable<Unit>
+
+    fun profileShareClicks(): Observable<Unit>
+
+    fun installedClicks(): Observable<Unit>
+
+    fun distroClicks(): Observable<Unit>
+
+    fun settingsClicks(): Observable<Unit>
+
+    fun aboutClicks(): Observable<Unit>
+
+    fun exitAppClicks(): Observable<Unit>
+
+    fun tabReselectClicks(): Observable<Unit>
+
+}
+
+class HomeViewImpl(view: View) : HomeView {
+
+    private val context = view.context
+    private val coordinator: View = view.findViewById(R.id.coordinator)
+    private val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+    private val updateBlock: View = view.findViewById(R.id.update_block)
+    private val frameLayout: View = view.findViewById(R.id.frame)
+    private val bottomNavigation: BottomNavigationView = view.findViewById(R.id.bottom_navigation)
+    private val uploadButton: FloatingActionButton = view.findViewById(R.id.fab_upload)
+    private val postButton: FloatingActionButton = view.findViewById(R.id.fab_post)
+    private val createChatButton: FloatingActionButton = view.findViewById(R.id.fab_create_chat)
+    private val updateButton: Button = view.findViewById(R.id.update)
+    private val laterButton: Button = view.findViewById(R.id.update_later)
+
+    private val storeRelay = PublishRelay.create<Unit>()
+    private val feedRelay = PublishRelay.create<Unit>()
+    private val discussRelay = PublishRelay.create<Unit>()
+    private val profileRelay = PublishRelay.create<Unit>()
+    private val uploadRelay = PublishRelay.create<Unit>()
+    private val postRelay = PublishRelay.create<Unit>()
+    private val createChatRelay = PublishRelay.create<Unit>()
+    private val updateRelay = PublishRelay.create<Unit>()
+    private val laterRelay = PublishRelay.create<Unit>()
+    private val searchRelay = PublishRelay.create<Unit>()
+    private val categoryRelay = PublishRelay.create<Unit>()
+    private val profileShareRelay = PublishRelay.create<Unit>()
+    private val installedRelay = PublishRelay.create<Unit>()
+    private val distroRelay = PublishRelay.create<Unit>()
+    private val settingsRelay = PublishRelay.create<Unit>()
+    private val aboutRelay = PublishRelay.create<Unit>()
+    private val exitAppRelay = PublishRelay.create<Unit>()
+    private val tabReselectRelay = PublishRelay.create<Unit>()
+
+    init {
+        toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.menu_search -> searchRelay.accept(Unit)
+                R.id.menu_category -> categoryRelay.accept(Unit)
+                R.id.menu_share -> profileShareRelay.accept(Unit)
+                R.id.menu_installed -> installedRelay.accept(Unit)
+                R.id.menu_distro -> distroRelay.accept(Unit)
+                R.id.menu_settings -> settingsRelay.accept(Unit)
+                R.id.menu_about -> aboutRelay.accept(Unit)
+            }
+            true
+        }
+
+        bottomNavigation.setOnItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.nav_store -> storeRelay.accept(Unit)
+                R.id.nav_feed -> feedRelay.accept(Unit)
+                R.id.nav_discuss -> discussRelay.accept(Unit)
+                R.id.nav_profile -> profileRelay.accept(Unit)
+            }
+            true
+        }
+
+        bottomNavigation.setOnItemReselectedListener { tabReselectRelay.accept(Unit) }
+
+        uploadButton.clicks(uploadRelay)
+        postButton.clicks(postRelay)
+        createChatButton.clicks(createChatRelay)
+        updateButton.clicks(updateRelay)
+        laterButton.clicks(laterRelay)
+
+        coordinator.applyTopInsets()
+        bottomNavigation.applyBottomInsets()
+        frameLayout.applyBottomMarginForView(bottomNavigation)
+        uploadButton.applyBottomInsetsAsMargin()
+        postButton.applyBottomInsetsAsMargin()
+        createChatButton.applyBottomInsetsAsMargin()
+    }
+
+    override fun showStoreToolbar() {
+        with(toolbar) {
+            title = "AppStore"
+            subtitle = "Hiaashuu"
+            menu.clear()
+            inflateMenu(R.menu.store_menu)
+            invalidateMenu()
+        }
+    }
+
+    override fun showFeedToolbar() {
+        with(toolbar) {
+            setTitle(R.string.tab_feed)
+            subtitle = ""
+            menu.clear()
+            inflateMenu(R.menu.home_menu)
+            invalidateMenu()
+        }
+    }
+
+    override fun showDiscussToolbar() {
+        with(toolbar) {
+            setTitle(R.string.tab_discuss)
+            subtitle = ""
+            menu.clear()
+            inflateMenu(R.menu.home_menu)
+            invalidateMenu()
+        }
+    }
+
+    override fun showProfileToolbar() {
+        with(toolbar) {
+            setTitle(R.string.tab_profile)
+            subtitle = ""
+            menu.clear()
+            inflateMenu(R.menu.user_menu)
+            invalidateMenu()
+        }
+    }
+
+    override fun selectStoreTab() {
+        bottomNavigation.selectedItemId = R.id.nav_store
+    }
+
+    override fun selectDiscussTab() {
+        bottomNavigation.selectedItemId = R.id.nav_discuss
+    }
+
+    override fun selectProfileTab() {
+        bottomNavigation.selectedItemId = R.id.nav_profile
+    }
+
+    override fun showUnreadBadge(count: Int) {
+        bottomNavigation.getOrCreateBadge(R.id.nav_discuss).apply {
+            number = count
+            isVisible = true
+        }
+    }
+
+    override fun showFeedBadge(count: Int) {
+        bottomNavigation.getOrCreateBadge(R.id.nav_feed).apply {
+            number = count
+            isVisible = true
+        }
+    }
+
+    override fun showModerationBadge(count: Int) {
+        bottomNavigation.getOrCreateBadge(R.id.nav_profile).apply {
+            number = count
+            isVisible = true
+        }
+    }
+
+    override fun hideUnreadBadge() {
+        bottomNavigation.getBadge(R.id.nav_discuss)?.apply {
+            clearNumber()
+            isVisible = false
+        }
+    }
+
+    override fun hideFeedBadge() {
+        bottomNavigation.getBadge(R.id.nav_feed)?.apply {
+            clearNumber()
+            isVisible = false
+        }
+    }
+
+    override fun hideModerationBadge() {
+        bottomNavigation.getBadge(R.id.nav_profile)?.apply {
+            clearNumber()
+            isVisible = false
+        }
+    }
+
+    override fun showUpdateBlock() {
+        updateBlock.show()
+    }
+
+    override fun hideUpdateBlock() {
+        updateBlock.hide()
+    }
+
+    override fun showUploadButton() {
+        uploadButton.show()
+    }
+
+    override fun showPostButton() {
+        postButton.show()
+    }
+
+    override fun showCreateChatButton() {
+        createChatButton.show()
+    }
+
+    override fun hideFabButtons() {
+        uploadButton.hide()
+        postButton.hide()
+        createChatButton.hide()
+    }
+
+    override fun showStatusDialog(block: Boolean, title: String?, message: String) {
+        MaterialAlertDialogBuilder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setCancelable(!block)
+            .setPositiveButton(R.string.ok) { _, _ ->
+                if (block) {
+                    exitAppRelay.accept(Unit)
+                }
+            }
+            .show()
+    }
+
+    override fun showCapabilityDenied(capability: Capability) {
+        val text = CapabilityHintResolver(context.resources).resolveText(capability)
+
+        Snackbar.make(coordinator, text, Snackbar.LENGTH_LONG)
+            .setAnchorView(bottomNavigation)
+            .show()
+    }
+
+    override fun storeClicks(): Observable<Unit> = storeRelay
+
+    override fun feedClicks(): Observable<Unit> = feedRelay
+
+    override fun discussClicks(): Observable<Unit> = discussRelay
+
+    override fun profileClicks(): Observable<Unit> = profileRelay
+
+    override fun uploadClicks(): Observable<Unit> = uploadRelay
+
+    override fun postClicks(): Observable<Unit> = postRelay
+
+    override fun createChatClicks(): Observable<Unit> = createChatRelay
+
+    override fun updateClicks(): Observable<Unit> = updateRelay
+
+    override fun laterClicks(): Observable<Unit> = laterRelay
+
+    override fun searchClicks(): Observable<Unit> = searchRelay
+
+    override fun categoryClicks(): Observable<Unit> = categoryRelay
+
+    override fun profileShareClicks(): Observable<Unit> = profileShareRelay
+
+    override fun installedClicks(): Observable<Unit> = installedRelay
+
+    override fun distroClicks(): Observable<Unit> = distroRelay
+
+    override fun settingsClicks(): Observable<Unit> = settingsRelay
+
+    override fun aboutClicks(): Observable<Unit> = aboutRelay
+
+    override fun exitAppClicks(): Observable<Unit> = exitAppRelay
+
+    override fun tabReselectClicks(): Observable<Unit> = tabReselectRelay
+
+}
