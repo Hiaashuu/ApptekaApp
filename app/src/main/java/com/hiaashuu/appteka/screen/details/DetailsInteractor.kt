0cf913bb322a488e1f59af196825a6e549dfc725
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:25 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/DetailsInteractor.kt
@@ -0,0 +1,131 @@
+package com.hiaashuu.appteka.screen.details
+
+import com.hiaashuu.appteka.core.StoreApi
+import com.hiaashuu.appteka.screen.details.api.CreateTopicResponse
+import com.hiaashuu.appteka.screen.details.api.DeletionResponse
+import com.hiaashuu.appteka.screen.details.api.Details
+import com.hiaashuu.appteka.screen.details.api.MarkFavoriteResponse
+import com.hiaashuu.appteka.screen.details.api.ModerationDecisionResponse
+import com.hiaashuu.appteka.screen.details.api.RequestScanResponse
+import com.hiaashuu.appteka.screen.details.api.TranslationResponse
+import com.hiaashuu.appteka.user.api.UserBrief
+import com.hiaashuu.appteka.util.SchedulersFactory
+import com.tomclaw.bananalytics.Bananalytics
+import com.tomclaw.bananalytics.api.BreadcrumbCategory
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.util.Locale
+
+interface DetailsInteractor {
+
+    fun loadDetails(appId: String?, packageName: String?): Observable<Details>
+
+    fun sendModerationDecision(
+        appId: String,
+        isApprove: Boolean
+    ): Single<ModerationDecisionResponse>
+
+    fun deleteApplication(appId: String): Single<DeletionResponse>
+
+    fun createTopic(packageName: String): Single<CreateTopicResponse>
+
+    fun markFavorite(appId: String, isFavorite: Boolean): Single<MarkFavoriteResponse>
+
+    fun getUserBrief(): Single<UserBrief>
+
+    fun translate(appId: String): Single<TranslationResponse>
+
+    fun requestSecurityScan(appId: String): Single<RequestScanResponse>
+
+}
+
+class DetailsInteractorImpl(
+    private val bananalytics: Bananalytics,
+    private val api: StoreApi,
+    private val locale: Locale,
+    private val schedulers: SchedulersFactory
+) : DetailsInteractor {
+
+    override fun loadDetails(appId: String?, packageName: String?): Observable<Details> {
+        bananalytics.leaveBreadcrumb("Load details: $appId", BreadcrumbCategory.NETWORK)
+        return api
+            .getInfo(
+                appId = appId,
+                packageName = packageName,
+                locale = locale.language,
+            )
+            .map { it.result }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun sendModerationDecision(
+        appId: String,
+        isApprove: Boolean
+    ): Single<ModerationDecisionResponse> {
+        val decision = if (isApprove) MODERATION_APPROVE else MODERATION_DENY
+        return api
+            .sendModerationDecision(
+                appId = appId,
+                decision = decision,
+            )
+            .map { it.result }
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun deleteApplication(appId: String): Single<DeletionResponse> {
+        bananalytics.leaveBreadcrumb("Delete app: $appId", BreadcrumbCategory.NETWORK)
+        return api.deleteApplication(appId = appId)
+            .map { it.result }
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun createTopic(packageName: String): Single<CreateTopicResponse> {
+        val textPlain = "text/plain".toMediaTypeOrNull()
+        return api.createTopic(
+            packageName = packageName.toRequestBody(textPlain),
+            title = null,
+            description = null,
+            avatar = null,
+        )
+            .map { it.result }
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun markFavorite(appId: String, isFavorite: Boolean): Single<MarkFavoriteResponse> {
+        return api
+            .markFavorite(
+                appId = appId,
+                isFavorite = isFavorite,
+            )
+            .map { it.result }
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun getUserBrief(): Single<UserBrief> {
+        return api
+            .getUserBrief(userId = null)
+            .map { it.result }
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun translate(appId: String): Single<TranslationResponse> {
+        return api
+            .getInfoTranslation(appId, locale.language)
+            .map { it.result }
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun requestSecurityScan(appId: String): Single<RequestScanResponse> {
+        return api
+            .requestSecurityScan(appId)
+            .map { it.result }
+            .subscribeOn(schedulers.io())
+    }
+
+}
+
+const val MODERATION_APPROVE = 1
+const val MODERATION_DENY = -1
