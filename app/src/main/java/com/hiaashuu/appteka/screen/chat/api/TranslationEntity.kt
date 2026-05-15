ca21fc8938b13538bc16c1a500e14116b972b0ba
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:17:20 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/chat/api/TranslationEntity.kt
@@ -0,0 +1,13 @@
+package com.hiaashuu.appteka.screen.chat.api
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class TranslationEntity(
+    val msgId: Int,
+    val original: String,
+    val translation: String,
+    val lang: String,
+    var translated: Boolean,
+) : Parcelable
