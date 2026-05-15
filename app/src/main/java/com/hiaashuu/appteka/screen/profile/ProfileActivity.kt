216f3c589398fc3db9d0558a02461d025e60db28
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:25:42 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/ProfileActivity.kt
@@ -0,0 +1,79 @@
+package com.hiaashuu.appteka.screen.profile
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.appComponent
+import com.hiaashuu.appteka.screen.home.createHomeActivityIntent
+
+class ProfileActivity : AppCompatActivity() {
+
+    private val deepLinkParser: ProfileDeepLinkParser by lazy {
+        appComponent.profileDeepLinkParser()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var userId: Int
+        var isShowHomeOnFinish = false
+
+        val data = intent.data
+        if (data != null) {
+            when (val deepLink = deepLinkParser.parse(data)) {
+                is ProfileDeepLink.ByUserId -> {
+                    userId = deepLink.userId
+                    isShowHomeOnFinish = true
+                }
+                is ProfileDeepLink.Invalid -> return navigateToStore()
+            }
+        } else {
+            userId = intent.getIntExtra(EXTRA_USER_ID, 0)
+            if (userId == 0) {
+                return navigateToStore()
+            }
+        }
+
+        setContentView(R.layout.profile_activity)
+
+        if (savedInstanceState == null) {
+            val fragment = createProfileFragment(userId)
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.profile_fragment, fragment)
+                .commit()
+        }
+
+        if (isShowHomeOnFinish) {
+            onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    leaveScreen()
+                }
+            })
+        }
+    }
+
+    private fun leaveScreen() {
+        navigateToStore()
+    }
+
+    private fun navigateToStore() {
+        val intent = createHomeActivityIntent(context = this).apply {
+            setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+        startActivity(intent)
+        finish()
+    }
+
+}
+
+fun createProfileActivityIntent(
+    context: Context,
+    userId: Int,
+): Intent = Intent(context, ProfileActivity::class.java)
+    .putExtra(EXTRA_USER_ID, userId)
+
+private const val EXTRA_USER_ID = "user_id"
