9088c750cd33e5d9f84bf26bbb42b282ab19ef06
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:27:09 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/di/ProfileModule.kt
@@ -0,0 +1,299 @@
+package com.hiaashuu.appteka.screen.profile.di
+
+import android.content.Context
+import android.os.Bundle
+import com.hiaashuu.appteka.util.adapter.ItemBinder
+import com.hiaashuu.appteka.util.adapter.AdapterPresenter
+import com.hiaashuu.appteka.util.adapter.SimpleAdapterPresenter
+import com.hiaashuu.appteka.util.adapter.ItemBlueprint
+import com.hiaashuu.appteka.core.StoreApi
+import com.hiaashuu.appteka.core.TimeProvider
+import com.hiaashuu.appteka.core.permissions.UserCapabilitiesProvider
+import com.hiaashuu.appteka.di.DATE_FORMATTER
+import com.hiaashuu.appteka.screen.profile.ProfileConverter
+import com.hiaashuu.appteka.screen.profile.ProfileConverterImpl
+import com.hiaashuu.appteka.screen.profile.ProfileInteractor
+import com.hiaashuu.appteka.screen.profile.ProfileInteractorImpl
+import com.hiaashuu.appteka.screen.profile.ProfilePresenter
+import com.hiaashuu.appteka.screen.profile.ProfilePresenterImpl
+import com.hiaashuu.appteka.screen.profile.adapter.app.AppItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.app.AppItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.downloads.DownloadsItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.downloads.DownloadsItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.favorites.FavoritesItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.favorites.FavoritesItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.feed.FeedItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.feed.FeedItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.header.HeaderItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.header.HeaderItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.header.HeaderResourceProvider
+import com.hiaashuu.appteka.screen.profile.adapter.header.HeaderResourceProviderImpl
+import com.hiaashuu.appteka.screen.profile.adapter.placeholder.PlaceholderItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.placeholder.PlaceholderItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.review.ReviewItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.review.ReviewItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.reviews.ReviewsItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.reviews.ReviewsItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.moderation.ModerationItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.moderation.ModerationItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.unauthorized.UnauthorizedItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.unauthorized.UnauthorizedItemPresenter
+import com.hiaashuu.appteka.screen.profile.adapter.uploads.UploadsItemBlueprint
+import com.hiaashuu.appteka.screen.profile.adapter.uploads.UploadsItemPresenter
+import com.hiaashuu.appteka.user.ModerationProvider
+import com.hiaashuu.appteka.util.PerFragment
+import com.hiaashuu.appteka.util.SchedulersFactory
+import dagger.Lazy
+import dagger.Module
+import dagger.Provides
+import dagger.multibindings.IntoSet
+import java.text.DateFormat
+import java.util.Locale
+import javax.inject.Named
+
+@Module
+class ProfileModule(
+    private val userId: Int?,
+    private val withToolbar: Boolean?,
+    private val state: Bundle?
+) {
+
+    @Provides
+    @PerFragment
+    internal fun providePresenter(
+        interactor: ProfileInteractor,
+        converter: ProfileConverter,
+        moderationProvider: ModerationProvider,
+        capabilitiesProvider: UserCapabilitiesProvider,
+        @Named(PROFILE_ADAPTER_PRESENTER) adapterPresenter: Lazy<AdapterPresenter>,
+        schedulers: SchedulersFactory
+    ): ProfilePresenter = ProfilePresenterImpl(
+        userId,
+        withToolbar,
+        interactor,
+        converter,
+        moderationProvider,
+        capabilitiesProvider,
+        adapterPresenter,
+        schedulers,
+        state
+    )
+
+    @Provides
+    @PerFragment
+    internal fun provideInteractor(
+        api: StoreApi,
+        schedulers: SchedulersFactory
+    ): ProfileInteractor = ProfileInteractorImpl(api, schedulers)
+
+    @Provides
+    @PerFragment
+    internal fun provideConverter(): ProfileConverter = ProfileConverterImpl()
+
+    @Provides
+    @Named(PROFILE_ADAPTER_PRESENTER)
+    @PerFragment
+    internal fun provideProfileAdapterPresenter(binder: ItemBinder): AdapterPresenter {
+        return SimpleAdapterPresenter(binder)
+    }
+
+    @Provides
+    @Named(UPLOADS_ADAPTER_PRESENTER)
+    @PerFragment
+    internal fun provideUploadsAdapterPresenter(binder: ItemBinder): AdapterPresenter {
+        return SimpleAdapterPresenter(binder)
+    }
+
+    @Provides
+    @Named(RATINGS_ADAPTER_PRESENTER)
+    @PerFragment
+    internal fun provideRatingsAdapterPresenter(binder: ItemBinder): AdapterPresenter {
+        return SimpleAdapterPresenter(binder)
+    }
+
+    @Provides
+    @PerFragment
+    internal fun provideItemBinder(
+        blueprintSet: Set<@JvmSuppressWildcards ItemBlueprint<*, *>>
+    ): ItemBinder {
+        return ItemBinder.Builder().apply {
+            blueprintSet.forEach { registerItem(it) }
+        }.build()
+    }
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideHeaderItemBlueprint(
+        presenter: HeaderItemPresenter
+    ): ItemBlueprint<*, *> = HeaderItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun provideHeaderResourceProvider(
+        context: Context,
+        timeProvider: TimeProvider,
+    ): HeaderResourceProvider = HeaderResourceProviderImpl(context, timeProvider)
+
+    @Provides
+    @PerFragment
+    internal fun provideHeaderItemPresenter(
+        presenter: ProfilePresenter,
+        resourceProvider: HeaderResourceProvider,
+        locale: Locale,
+    ) = HeaderItemPresenter(presenter, resourceProvider, locale)
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideAppItemBlueprint(
+        presenter: AppItemPresenter
+    ): ItemBlueprint<*, *> = AppItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun provideAppItemPresenter(
+        presenter: UploadsItemPresenter,
+    ) = AppItemPresenter(presenter)
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideUploadsItemBlueprint(
+        presenter: UploadsItemPresenter,
+        @Named(UPLOADS_ADAPTER_PRESENTER) adapterPresenter: Lazy<AdapterPresenter>,
+        binder: Lazy<ItemBinder>,
+    ): ItemBlueprint<*, *> = UploadsItemBlueprint(
+        presenter,
+        adapterPresenter,
+        binder
+    )
+
+    @Provides
+    @PerFragment
+    internal fun provideUploadsItemPresenter(
+        presenter: ProfilePresenter,
+        @Named(UPLOADS_ADAPTER_PRESENTER) adapterPresenter: Lazy<AdapterPresenter>,
+    ) = UploadsItemPresenter(
+        presenter,
+        adapterPresenter
+    )
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideRatingItemBlueprint(
+        presenter: ReviewItemPresenter
+    ): ItemBlueprint<*, *> = ReviewItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun provideRatingItemPresenter(
+        @Named(DATE_FORMATTER) dateFormatter: DateFormat,
+        presenter: ReviewsItemPresenter,
+    ) = ReviewItemPresenter(dateFormatter, presenter)
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideRatingsItemBlueprint(
+        presenter: ReviewsItemPresenter,
+        @Named(RATINGS_ADAPTER_PRESENTER) adapterPresenter: Lazy<AdapterPresenter>,
+        binder: Lazy<ItemBinder>,
+    ): ItemBlueprint<*, *> = ReviewsItemBlueprint(
+        presenter,
+        adapterPresenter,
+        binder
+    )
+
+    @Provides
+    @PerFragment
+    internal fun provideRatingsItemPresenter(
+        presenter: ProfilePresenter,
+        @Named(RATINGS_ADAPTER_PRESENTER) adapterPresenter: Lazy<AdapterPresenter>,
+    ) = ReviewsItemPresenter(
+        presenter,
+        adapterPresenter
+    )
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideFavoritesItemBlueprint(
+        presenter: FavoritesItemPresenter
+    ): ItemBlueprint<*, *> = FavoritesItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun provideFavoritesItemPresenter(
+        presenter: ProfilePresenter,
+    ) = FavoritesItemPresenter(presenter)
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideDownloadsItemBlueprint(
+        presenter: DownloadsItemPresenter
+    ): ItemBlueprint<*, *> = DownloadsItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun provideDownloadsItemPresenter(
+        presenter: ProfilePresenter,
+    ) = DownloadsItemPresenter(presenter)
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun providePlaceholderItemBlueprint(
+        presenter: PlaceholderItemPresenter
+    ): ItemBlueprint<*, *> = PlaceholderItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun providePlaceholderItemPresenter() = PlaceholderItemPresenter()
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideUnauthorizedItemBlueprint(
+        presenter: UnauthorizedItemPresenter
+    ): ItemBlueprint<*, *> = UnauthorizedItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun provideUnauthorizedItemPresenter(
+        presenter: ProfilePresenter,
+    ) = UnauthorizedItemPresenter(presenter)
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideFeedItemBlueprint(
+        presenter: FeedItemPresenter
+    ): ItemBlueprint<*, *> = FeedItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun provideFeedItemPresenter(
+        presenter: ProfilePresenter,
+    ) = FeedItemPresenter(presenter)
+
+    @Provides
+    @IntoSet
+    @PerFragment
+    internal fun provideModerationItemBlueprint(
+        presenter: ModerationItemPresenter
+    ): ItemBlueprint<*, *> = ModerationItemBlueprint(presenter)
+
+    @Provides
+    @PerFragment
+    internal fun provideModerationItemPresenter(
+        presenter: ProfilePresenter,
+    ) = ModerationItemPresenter(presenter)
+
+}
+
+const val PROFILE_ADAPTER_PRESENTER = "ProfileAdapterPresenter"
+const val UPLOADS_ADAPTER_PRESENTER = "UploadsAdapterPresenter"
+const val RATINGS_ADAPTER_PRESENTER = "RatingsAdapterPresenter"
