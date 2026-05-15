19d277e8d1e558108e8725f2ecd1fd6d12d055e8
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:14:39 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/analytics/EnvironmentProviderImpl.kt
@@ -0,0 +1,27 @@
+package com.hiaashuu.appteka.analytics
+
+import android.os.Build
+import com.hiaashuu.appteka.core.AppInfoProvider
+import com.hiaashuu.appteka.core.DeviceIdProvider
+import com.tomclaw.bananalytics.EnvironmentProvider
+import com.tomclaw.bananalytics.api.Environment
+import java.util.Locale
+
+class EnvironmentProviderImpl(
+    private val locale: Locale,
+    private val infoProvider: AppInfoProvider,
+    private val idProvider: DeviceIdProvider,
+) : EnvironmentProvider {
+
+    override fun environment() = Environment(
+        packageName = infoProvider.getPackageName(),
+        appVersion = infoProvider.getVersionCode(),
+        appVersionName = infoProvider.getVersionName(),
+        deviceId = idProvider.getDeviceId(),
+        osVersion = Build.VERSION.SDK_INT,
+        manufacturer = Build.MANUFACTURER,
+        model = Build.MODEL,
+        country = locale.country,
+        language = locale.language,
+    )
+}
