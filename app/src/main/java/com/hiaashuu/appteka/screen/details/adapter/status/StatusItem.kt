a9093199653a16ca1b96a5463160397571fb96cc
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:02 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/status/StatusItem.kt
@@ -0,0 +1,26 @@
+package com.hiaashuu.appteka.screen.details.adapter.status
+
+import android.os.Parcelable
+import com.hiaashuu.appteka.util.adapter.Item
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class StatusItem(
+    override val id: Long,
+    val type: StatusType,
+    val text: String,
+    val actionType: StatusAction,
+    val actionLabel: String?,
+) : Item, Parcelable
+
+enum class StatusType {
+    INFO,
+    WARNING,
+    ERROR
+}
+
+enum class StatusAction {
+    NONE,
+    EDIT_META,
+    UNPUBLISH
+}
