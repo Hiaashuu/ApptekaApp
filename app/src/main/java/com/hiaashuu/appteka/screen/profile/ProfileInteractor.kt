28cf032f9850aaf84a8f46ca66e05c76c177c2e2
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:27:12 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/ProfileInteractor.kt
@@ -0,0 +1,76 @@
+package com.hiaashuu.appteka.screen.profile
+
+import com.hiaashuu.appteka.core.StoreApi
+import com.hiaashuu.appteka.screen.profile.api.EliminateUserResponse
+import com.hiaashuu.appteka.screen.profile.api.ProfileResponse
+import com.hiaashuu.appteka.screen.profile.api.SubscribeResponse
+import com.hiaashuu.appteka.screen.profile.api.UnsubscribeResponse
+import com.hiaashuu.appteka.screen.profile.api.UserAppsResponse
+import com.hiaashuu.appteka.util.SchedulersFactory
+import io.reactivex.rxjava3.core.Observable
+
+interface ProfileInteractor {
+
+    fun loadProfile(userId: Int?): Observable<ProfileResponse>
+
+    fun loadUserApps(userId: Int?, offsetAppId: String?): Observable<UserAppsResponse>
+
+    fun subscribe(userId: Int): Observable<SubscribeResponse>
+
+    fun unsubscribe(userId: Int): Observable<UnsubscribeResponse>
+
+    fun eliminateUser(userId: Int): Observable<EliminateUserResponse>
+
+}
+
+class ProfileInteractorImpl(
+    private val api: StoreApi,
+    private val schedulers: SchedulersFactory
+) : ProfileInteractor {
+
+    override fun loadProfile(userId: Int?): Observable<ProfileResponse> {
+        return api
+            .getProfile(userId)
+            .map { it.result }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun loadUserApps(userId: Int?, offsetAppId: String?): Observable<UserAppsResponse> {
+        return api
+            .getUserApps(
+                userId = userId,
+                appId = offsetAppId,
+            )
+            .map { list ->
+                list.result
+            }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun subscribe(userId: Int): Observable<SubscribeResponse> {
+        return api
+            .subscribe(userId)
+            .map { it.result }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun unsubscribe(userId: Int): Observable<UnsubscribeResponse> {
+        return api
+            .unsubscribe(userId)
+            .map { it.result }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun eliminateUser(userId: Int): Observable<EliminateUserResponse> {
+        return api
+            .eliminateUser(userId)
+            .map { it.result }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+}
