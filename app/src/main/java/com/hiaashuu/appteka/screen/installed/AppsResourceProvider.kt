363a50a0f6d2c13dc474d2a2ecb6f98a40198b1a
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:23:25 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/installed/AppsResourceProvider.kt
@@ -0,0 +1,49 @@
+package com.hiaashuu.appteka.screen.installed
+
+import android.content.res.Resources
+import com.hiaashuu.appteka.R
+import com.hiaashuu.appteka.util.FileHelper
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+interface AppsResourceProvider {
+
+    fun formatAppVersion(verName: String, verCode: Int): String
+
+    fun formatFileSize(size: Long): String
+
+    fun installDate(value: Long): String
+
+    fun updateDate(value: Long): String
+
+}
+
+class AppsResourceProviderImpl(
+    val resources: Resources,
+    val locale: Locale,
+) : AppsResourceProvider {
+
+    private val dateFormat: DateFormat = SimpleDateFormat("dd.MM.yy", locale)
+
+    override fun formatAppVersion(verName: String, verCode: Int): String {
+        return resources.getString(R.string.app_version_format, verName, verCode)
+    }
+
+    override fun formatFileSize(size: Long): String {
+        return FileHelper.formatBytes(resources, size)
+    }
+
+    override fun installDate(value: Long): String {
+        return resources.getString(R.string.install_date, formatDate(value))
+    }
+
+    override fun updateDate(value: Long): String {
+        return resources.getString(R.string.update_date, formatDate(value))
+    }
+
+    private fun formatDate(value: Long): String {
+        return dateFormat.format(value)
+    }
+
+}
