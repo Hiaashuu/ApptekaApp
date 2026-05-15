64a5ef09625a1e4e3d2ad0178bc9e8a66883ea46
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:27:38 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/ratings/adapter/rating/RatingItemPresenter.kt
@@ -0,0 +1,44 @@
+package com.hiaashuu.appteka.screen.ratings.adapter.rating
+
+import com.hiaashuu.appteka.categories.DEFAULT_LOCALE
+import com.hiaashuu.appteka.screen.ratings.adapter.ItemListener
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import java.text.DateFormat
+import java.util.Locale
+
+class RatingItemPresenter(
+    private val dateFormatter: DateFormat,
+    private val locale: Locale,
+    private val listener: ItemListener,
+) : ItemPresenter<RatingItemView, RatingItem> {
+
+    override fun bindView(view: RatingItemView, item: RatingItem, position: Int) {
+        with(item) {
+            if (hasMore) {
+                hasMore = false
+                hasProgress = true
+                hasError = false
+                listener.onLoadMore(this)
+            }
+        }
+
+        item.user.icon?.let(view::setUserIcon)
+        view.setUserBadge(item.user.primaryBadge)
+        val name = item.user.name.takeIf { !it.isNullOrBlank() }
+            ?: item.user.icon?.label?.get(locale.language)
+            ?: item.user.icon?.label?.get(DEFAULT_LOCALE).orEmpty()
+        view.setUserName(name)
+        view.setRating(item.score.toFloat())
+        val date: String = dateFormatter.format(item.time)
+        view.setDate(date)
+        view.setComment(item.text)
+        if (item.showRatingMenu) {
+            view.showRatingMenu()
+        } else {
+            view.hideRatingMenu()
+        }
+        view.setOnRatingClickListener { listener.onItemClick(item) }
+        view.setOnDeleteClickListener { listener.onDeleteClick(item) }
+    }
+
+}
