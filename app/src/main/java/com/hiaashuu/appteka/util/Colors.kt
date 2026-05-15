7331ab988d3caa1f955e8267f3e9a1862aa3056b
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:34:59 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/Colors.kt
@@ -0,0 +1,38 @@
+package com.hiaashuu.appteka.util
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.util.TypedValue
+import kotlin.math.max
+
+fun darker(color: Int, factor: Float): Int {
+    val a = Color.alpha(color)
+    val r = Color.red(color)
+    val g = Color.green(color)
+    val b = Color.blue(color)
+    return Color.argb(
+        a,
+        max((r * factor).toInt(), 0),
+        max((g * factor).toInt(), 0),
+        max((b * factor).toInt(), 0)
+    )
+}
+
+fun getAttributedColor(context: Context, attr: Int): Int {
+    val set = intArrayOf(attr)
+    val typedValue = TypedValue()
+    val a = context.obtainStyledAttributes(typedValue.data, set)
+    val color = a.getColor(0, Color.WHITE)
+    a.recycle()
+    return color
+}
+
+@Suppress("DEPRECATION")
+fun getColor(color: Int, context: Context): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        context.resources.getColor(color, context.theme)
+    } else {
+        context.resources.getColor(color)
+    }
+}
