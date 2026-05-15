5a246eb33036536fa91f1853920487bb141b673a
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:34:43 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/Analytics.kt
@@ -0,0 +1,31 @@
+package com.hiaashuu.appteka.util
+
+import com.tomclaw.bananalytics.Bananalytics
+
+interface Analytics {
+
+    fun register()
+
+    fun trackEvent(name: String)
+
+    fun trackException(throwable: Throwable, context: Map<String, String> = emptyMap())
+
+}
+
+class AnalyticsImpl(
+    private val bananalytics: Bananalytics,
+) : Analytics {
+
+    override fun register() {
+        bananalytics.install()
+    }
+
+    override fun trackEvent(name: String) {
+        bananalytics.trackEvent(name)
+    }
+
+    override fun trackException(throwable: Throwable, context: Map<String, String>) {
+        bananalytics.trackException(throwable, context)
+    }
+
+}
