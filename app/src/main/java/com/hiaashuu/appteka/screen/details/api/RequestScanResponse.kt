4b7bee6a615b406c2fac7cc750e28a69ddf40892
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:43 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/api/RequestScanResponse.kt
@@ -0,0 +1,19 @@
+package com.hiaashuu.appteka.screen.details.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.hiaashuu.appteka.util.GsonModel
+import kotlinx.parcelize.Parcelize
+
+@GsonModel
+@Parcelize
+data class RequestScanResponse(
+    @SerializedName("scan_id")
+    val scanId: Int,
+    @SerializedName("file_id")
+    val fileId: Int,
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("message")
+    val message: String?,
+) : Parcelable
