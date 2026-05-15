68f771180a7f04b372b5e42c0eb46b42c9727057
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:19:00 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/details/adapter/security/SecurityItemPresenter.kt
@@ -0,0 +1,30 @@
+package com.hiaashuu.appteka.screen.details.adapter.security
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+import com.hiaashuu.appteka.screen.details.adapter.ItemListener
+
+class SecurityItemPresenter(
+    private val listener: ItemListener
+) : ItemPresenter<SecurityItemView, SecurityItem> {
+
+    override fun bindView(view: SecurityItemView, item: SecurityItem, position: Int) {
+        with(view) {
+            when (item.type) {
+                SecurityType.NOT_SCANNED -> setSecurityTypeNotScanned()
+                SecurityType.SCANNING -> setSecurityTypeScanning()
+                SecurityType.SAFE -> setSecurityTypeSafe()
+                SecurityType.SUSPICIOUS -> setSecurityTypeSuspicious()
+                SecurityType.MALWARE -> setSecurityTypeMalware()
+                SecurityType.UNKNOWN -> setSecurityTypeUnknown()
+            }
+            if (item.showAction) {
+                showActionButton(item.actionLabel.orEmpty())
+                setOnActionClickListener { listener.onRequestSecurityScan(item.appId) }
+            } else {
+                hideActionButton()
+            }
+            setSecurityText(item.text)
+        }
+    }
+
+}
