dace2a1b64ae2828cff6ecc92a614e0759c33314
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:21:51 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/feed/adapter/favorite/FavoriteItem.kt
@@ -0,0 +1,39 @@
+package com.hiaashuu.appteka.screen.feed.adapter.favorite
+
+import com.hiaashuu.appteka.categories.Category
+import com.hiaashuu.appteka.dto.Screenshot
+import com.hiaashuu.appteka.screen.feed.adapter.FeedItem
+import com.hiaashuu.appteka.screen.feed.api.Reaction
+import com.hiaashuu.appteka.user.api.UserBrief
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class FavoriteItem(
+    override val id: Long,
+    val time: Long,
+    val appId: String,
+    val packageName: String,
+    val icon: String?,
+    val title: String,
+    val verName: String,
+    val verCode: Int,
+    val size: Long,
+    val rating: Float,
+    val downloads: Int,
+    val status: Int,
+    val category: Category?,
+    val exclusive: Boolean,
+    val openSource: Boolean,
+    val description: String?,
+    val screenshots: List<Screenshot>,
+    override val user: UserBrief,
+    override val actions: List<String>?,
+    val reacts: List<Reaction>?,
+    override var hasMore: Boolean = false,
+    override var hasProgress: Boolean = false,
+) : FeedItem {
+
+    override fun getReactions(): List<Reaction>? = reacts
+
+    override fun withReactions(reactions: List<Reaction>): FeedItem = copy(reacts = reactions)
+}
