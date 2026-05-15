a2be02e0c0c30d070cdb588ca453e977dd38e499
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:36:47 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/util/ZipParcelable.kt
@@ -0,0 +1,115 @@
+package com.hiaashuu.appteka.util
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class ZipParcelable : Parcelable {
+
+    private var raw: ByteArray? = null
+    private var zip: Boolean? = null
+    private var nested: Parcelable? = null
+
+    constructor(nested: Parcelable?) {
+        this.nested = nested
+    }
+
+    private constructor(raw: ByteArray, zip: Boolean) {
+        this.raw = raw
+        this.zip = zip
+    }
+
+    private fun writeNestedParcel(nested: Parcelable, parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(nested, flags)
+    }
+
+    private fun <P : Parcelable> readNestedParcel(clazz: Class<P>, parcel: Parcel): Parcelable? {
+        @Suppress("DEPRECATION")
+        return parcel.readParcelable(clazz.classLoader)
+    }
+
+    inline fun <reified T : Parcelable> restore() = restore(T::class.java) as? T
+
+    fun <P : Parcelable> restore(clazz: Class<P>): Parcelable? {
+        if (nested != null) return nested as Parcelable
+
+        val data = raw?.takeIf { it.isNotEmpty() } ?: return null
+
+        val array = when (zip == true) {
+            true -> try {
+                data.unzip()
+            } catch (_: Throwable) {
+                return null
+            }
+
+            else -> data
+        }
+        return readNestedParcel(clazz, array.unmarshallToParcel())
+    }
+
+    override fun writeToParcel(out: Parcel, flags: Int) = with(out) {
+        when (val nested = nested) {
+            null -> writeBool(false)
+            else -> {
+                writeBool(value = true)
+                val originalArray = parcelableToByteArray { writeNestedParcel(nested, it, flags) }
+                val compressed = try {
+                    originalArray.zip()
+                } catch (_: Throwable) {
+                    null
+                }
+                if (compressed != null && compressed.size < originalArray.size) {
+                    writeBool(value = true)
+                    writeByteArrayWithSize(compressed)
+                } else {
+                    writeBool(value = false)
+                    writeByteArrayWithSize(originalArray)
+                }
+            }
+        }
+    }
+
+    private fun Parcel.writeByteArrayWithSize(array: ByteArray) {
+        writeInt(array.size)
+        writeByteArray(array)
+    }
+
+    override fun describeContents() = 0
+
+    companion object {
+
+        @JvmField
+        val CREATOR = Parcels.creator {
+            create(this) { raw, zip ->
+                ZipParcelable(raw, zip)
+            }
+        }
+
+        @JvmStatic
+        fun create(
+            parcel: Parcel,
+            creator: (ByteArray, Boolean) -> ZipParcelable
+        ): ZipParcelable = with(parcel) {
+            var zip = false
+            val data = when (readBool()) {
+                true -> {
+                    zip = readBool()
+                    val size = readInt()
+                    ByteArray(size).apply {
+                        readByteArray(this)
+                    }
+                }
+
+                else -> ByteArray(size = 0)
+            }
+            return creator(data, zip)
+        }
+    }
+}
+
+fun parcelableToByteArray(writer: (Parcel) -> Unit): ByteArray {
+    val parcel = Parcel.obtain()
+    writer(parcel)
+    val bytes = parcel.marshall()
+    parcel.recycle()
+    return bytes
+}
