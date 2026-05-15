3366b58d02ad6e0abf7e0e41c178f6a6e149e258
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:27:14 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/ProfilePresenter.kt
@@ -0,0 +1,382 @@
+package com.hiaashuu.appteka.screen.profile
+
+import android.os.Bundle
+import com.hiaashuu.appteka.core.permissions.CapabilityAction
+import com.hiaashuu.appteka.core.permissions.CapabilityPolicy
+import com.hiaashuu.appteka.core.permissions.UserCapabilitiesProvider
+import com.hiaashuu.appteka.util.adapter.AdapterPresenter
+import com.hiaashuu.appteka.util.adapter.Item
+import com.hiaashuu.appteka.dto.AppEntity
+import com.hiaashuu.appteka.dto.Badge
+import com.hiaashuu.appteka.screen.profile.adapter.ItemListener
+import com.hiaashuu.appteka.screen.profile.adapter.app.AppItem
+import com.hiaashuu.appteka.screen.profile.api.ProfileResponse
+import com.hiaashuu.appteka.user.ModerationProvider
+import com.hiaashuu.appteka.util.SchedulersFactory
+import com.hiaashuu.appteka.util.filterUnauthorizedErrors
+import com.hiaashuu.appteka.util.getParcelableArrayListCompat
+import com.hiaashuu.appteka.util.getParcelableCompat
+import com.hiaashuu.appteka.util.retryWhenNonAuthErrors
+import dagger.Lazy
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.Observables
+import io.reactivex.rxjava3.kotlin.plusAssign
+
+interface ProfilePresenter : ItemListener {
+
+    fun attachView(view: ProfileView)
+
+    fun detachView()
+
+    fun attachRouter(router: ProfileRouter)
+
+    fun detachRouter()
+
+    fun saveState(): Bundle
+
+    fun onAuthorized()
+
+    fun onResume()
+
+    fun onProfileEdited()
+
+    interface ProfileRouter {
+
+        fun openUserFilesScreen(userId: Int)
+
+        fun openDetailsScreen(appId: String, label: String?)
+
+        fun openFavoriteScreen(userId: Int)
+
+        fun openDownloadsScreen(userId: Int)
+
+        fun openReviewsScreen(userId: Int)
+
+        fun openShareUrlDialog(text: String)
+
+        fun openLoginScreen()
+
+        fun openFeedScreen(userId: Int)
+
+        fun openSubscribersScreen(userId: Int)
+
+        fun openPublishersScreen(userId: Int)
+
+        fun openModerationScreen()
+
+        fun openChangeEmailScreen()
+
+        fun openEditProfileScreen()
+
+        fun leaveScreen()
+
+    }
+
+}
+
+class ProfilePresenterImpl(
+    private val userId: Int?,
+    private val withToolbar: Boolean?,
+    private val interactor: ProfileInteractor,
+    private val converter: ProfileConverter,
+    private val moderationProvider: ModerationProvider,
+    private val capabilitiesProvider: UserCapabilitiesProvider,
+    private val adapterPresenter: Lazy<AdapterPresenter>,
+    private val schedulers: SchedulersFactory,
+    state: Bundle?
+) : ProfilePresenter {
+
+    private var view: ProfileView? = null
+    private var router: ProfilePresenter.ProfileRouter? = null
+
+    private var profile: ProfileResponse? =
+        state?.getParcelableCompat(KEY_PROFILE, ProfileResponse::class.java)
+    private var uploads: ArrayList<AppEntity>? =
+        state?.getParcelableArrayListCompat(KEY_UPLOADS, AppEntity::class.java)
+
+    private val items = ArrayList<Item>()
+
+    private val subscriptions = CompositeDisposable()
+
+    override fun attachView(view: ProfileView) {
+        this.view = view
+
+        subscriptions += view.navigationClicks().subscribe { onBackPressed() }
+        subscriptions += view.swipeRefresh().subscribe { loadProfile() }
+        subscriptions += view.shareClicks().subscribe { onShareProfile() }
+        subscriptions += view.eliminateClicks().subscribe { ready ->
+            if (ready) {
+                onEliminateUser()
+            } else {
+                view.showEliminationDialog()
+            }
+        }
+        subscriptions += view.loginClicks().subscribe {
+            router?.openLoginScreen()
+        }
+
+        if (withToolbar == true) {
+            view.showToolbar()
+        } else {
+            view.hideToolbar()
+        }
+
+        if (profile != null) {
+            bindProfile()
+        } else {
+            loadProfile()
+        }
+    }
+
+    override fun detachView() {
+        subscriptions.clear()
+        this.view = null
+    }
+
+    override fun attachRouter(router: ProfilePresenter.ProfileRouter) {
+        this.router = router
+    }
+
+    override fun detachRouter() {
+        this.router = null
+    }
+
+    override fun saveState() = Bundle().apply {
+        putParcelable(KEY_PROFILE, profile)
+        putParcelableArrayList(KEY_UPLOADS, uploads)
+    }
+
+    override fun onAuthorized() {
+        loadProfile()
+    }
+
+    override fun onResume() {
+        if (profile != null) {
+            bindProfile()
+        }
+    }
+
+    override fun onAppClick(appId: String, title: String?) {
+        router?.openDetailsScreen(appId, title)
+    }
+
+    override fun onRatingsClick() {
+        val profile = profile?.profile ?: return
+        router?.openReviewsScreen(profile.userId)
+    }
+
+    override fun onFavoritesClick() {
+        val profile = profile?.profile ?: return
+        router?.openFavoriteScreen(profile.userId)
+    }
+
+    override fun onDownloadsClick() {
+        val profile = profile?.profile ?: return
+        router?.openDownloadsScreen(profile.userId)
+    }
+
+    override fun onUploadsClick(userId: Int) {
+        router?.openUserFilesScreen(userId)
+    }
+
+    override fun onModerationClick() {
+        router?.openModerationScreen()
+    }
+
+    override fun onLoginClick() {
+        router?.openLoginScreen()
+    }
+
+    override fun onEditName(name: String?, nameRegex: String?) {
+
+        router?.openEditProfileScreen()
+    }
+
+    override fun onEditAvatar() {
+        router?.openEditProfileScreen()
+    }
+
+    override fun onEditEmail() {
+        router?.openChangeEmailScreen()
+    }
+
+    override fun onNextPage(last: AppItem, param: (List<AppItem>) -> Unit) {
+        subscriptions += interactor.loadUserApps(userId, last.appId)
+            .observeOn(schedulers.mainThread())
+            .retryWhenNonAuthErrors()
+            .subscribe(
+                { response ->
+                    val appItems = converter.convertApps(response.files)
+                    param.invoke(appItems)
+                },
+                { }
+            )
+    }
+
+    override fun onFeedClick() {
+        val profile = profile?.profile ?: return
+        router?.openFeedScreen(profile.userId)
+    }
+
+    override fun onSubscribersClick() {
+        val profile = profile?.profile ?: return
+        router?.openSubscribersScreen(profile.userId)
+    }
+
+    override fun onPublishersClick() {
+        val profile = profile?.profile ?: return
+        router?.openPublishersScreen(profile.userId)
+    }
+
+    override fun onSubscribeClick() {
+        val profile = profile?.profile ?: return
+        subscriptions += interactor.subscribe(profile.userId)
+            .observeOn(schedulers.mainThread())
+            .subscribe(
+                { loadProfile() },
+                { onChangeSubscriptionStateError(it) }
+            )
+    }
+
+    override fun onUnsubscribeClick() {
+        userId ?: return
+        subscriptions += interactor.unsubscribe(userId)
+            .observeOn(schedulers.mainThread())
+            .subscribe(
+                { loadProfile() },
+                { onChangeSubscriptionStateError(it) }
+            )
+    }
+
+    override fun onBadgeClick(badge: Badge) {
+        view?.showBadgeDetails(badge)
+    }
+
+    private fun onChangeSubscriptionStateError(ex: Throwable) {
+        ex.filterUnauthorizedErrors({ view?.showUnauthorizedError() }) {
+            view?.showSubscriptionError()
+        }
+    }
+
+    private fun onBackPressed() {
+        router?.leaveScreen()
+    }
+
+    private fun onShareProfile() {
+        val url = profile?.profile?.url ?: return
+        router?.openShareUrlDialog(url)
+    }
+
+    private fun loadProfile() {
+        subscriptions += Observables.zip(
+            interactor.loadProfile(userId),
+            interactor.loadUserApps(userId, offsetAppId = null)
+        )
+            .observeOn(schedulers.mainThread())
+            .retryWhenNonAuthErrors()
+            .doOnSubscribe {
+                view?.hideMenu()
+                view?.hideError()
+                view?.showProgress()
+            }
+            .subscribe(
+                { onProfileLoaded(it.first, it.second.files) },
+                { onLoadingError(it) }
+            )
+    }
+
+    private fun onProfileLoaded(profile: ProfileResponse?, uploads: List<AppEntity>) {
+        this.profile = profile
+        this.uploads = ArrayList(uploads)
+        bindProfile()
+    }
+
+    private fun onLoadingError(ex: Throwable) {
+        ex.filterUnauthorizedErrors({
+            this.profile = null
+            this.uploads = null
+            bindUnauthorized()
+        }) {
+            view?.hideMenu()
+            view?.showContent()
+            view?.showError()
+        }
+    }
+
+    override fun onProfileEdited() {
+        loadProfile()
+    }
+
+    private fun onEliminateUser() {
+        val userId = userId ?: return
+        subscriptions += interactor.eliminateUser(userId)
+            .observeOn(schedulers.mainThread())
+            .doOnSubscribe {
+                view?.hideError()
+                view?.showProgress()
+            }
+            .doOnTerminate {
+                view?.showContent()
+            }
+            .subscribe(
+                { response ->
+                    view?.showContent()
+                    view?.showEliminationDone(
+                        filesCount = response.filesCount,
+                        messagesCount = response.msgsCount,
+                        ratingsCount = response.ratingsCount,
+                    )
+                },
+                { view?.showEliminationFailed() }
+            )
+    }
+
+    private fun bindProfile() {
+        val profile = this.profile ?: return
+        val isSelf = userId == null
+
+        val capabilities = capabilitiesProvider.getCapabilities()
+        items.clear()
+        items += converter.convertProfile(
+            profile.profile,
+            uploads,
+            moderation = moderationProvider.getModerationData(),
+            isSelf = isSelf,
+            userCapabilities = capabilities,
+        )
+
+        bindItems()
+        bindMenu(
+            canEliminate = CapabilityPolicy.isAllowed(
+                action = CapabilityAction.USER_ELIMINATE,
+                capabilities = capabilities,
+            )
+        )
+
+        view?.contentUpdated()
+        view?.showContent()
+    }
+
+    private fun bindUnauthorized() {
+        items.clear()
+        items += converter.unauthorizedProfile()
+
+        bindItems()
+        view?.hideMenu()
+
+        view?.contentUpdated()
+        view?.showContent()
+    }
+
+    private fun bindMenu(canEliminate: Boolean) {
+        view?.showMenu(canEliminate)
+    }
+
+    private fun bindItems() {
+        adapterPresenter.get().onDataSourceChanged(items)
+    }
+
+}
+
+private const val KEY_PROFILE = "profile"
+private const val KEY_UPLOADS = "uploads"
