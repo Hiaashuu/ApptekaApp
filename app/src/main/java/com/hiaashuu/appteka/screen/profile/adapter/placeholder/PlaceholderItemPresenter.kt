a3c953d17114378fbaad0fa85e85deac077c036d
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:26:24 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/profile/adapter/placeholder/PlaceholderItemPresenter.kt
@@ -0,0 +1,9 @@
+package com.hiaashuu.appteka.screen.profile.adapter.placeholder
+
+import com.hiaashuu.appteka.util.adapter.ItemPresenter
+
+class PlaceholderItemPresenter : ItemPresenter<PlaceholderItemView, PlaceholderItem> {
+
+    override fun bindView(view: PlaceholderItemView, item: PlaceholderItem, position: Int) {}
+
+}
