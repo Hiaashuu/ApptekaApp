8d558df60773bb7d54966d1441a2d4680e309ec7
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:29:30 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/topics/TopicsPresenter.kt
@@ -0,0 +1,234 @@
+package com.hiaashuu.appteka.screen.topics
+
+import android.os.Bundle
+import com.hiaashuu.appteka.util.adapter.AdapterPresenter
+import com.hiaashuu.appteka.util.adapter.Item
+import com.hiaashuu.appteka.dto.TopicEntity
+import com.hiaashuu.appteka.screen.topics.adapter.ItemListener
+import com.hiaashuu.appteka.screen.topics.adapter.topic.TopicItem
+import com.hiaashuu.appteka.util.SchedulersFactory
+import com.hiaashuu.appteka.util.filterUnauthorizedErrors
+import com.hiaashuu.appteka.util.getParcelableArrayListCompat
+import com.hiaashuu.appteka.util.retryWhenNonAuthErrors
+import dagger.Lazy
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.plusAssign
+
+interface TopicsPresenter : ItemListener {
+
+    fun attachView(view: TopicsView)
+
+    fun detachView()
+
+    fun attachRouter(router: TopicsRouter)
+
+    fun detachRouter()
+
+    fun saveState(): Bundle
+
+    fun scrollToTop()
+
+    fun invalidate()
+
+    interface TopicsRouter {
+
+        fun showChatScreen(entity: TopicEntity)
+
+        fun openLoginScreen()
+
+    }
+
+}
+
+class TopicsPresenterImpl(
+    private val converter: TopicConverter,
+    private val preferences: TopicsPreferencesProvider,
+    private val topicsInteractor: TopicsInteractor,
+    private val adapterPresenter: Lazy<AdapterPresenter>,
+    private val schedulers: SchedulersFactory,
+    state: Bundle?
+) : TopicsPresenter {
+
+    private var view: TopicsView? = null
+    private var router: TopicsPresenter.TopicsRouter? = null
+
+    private val subscriptions = CompositeDisposable()
+
+    private var entities: List<TopicEntity>? =
+        state?.getParcelableArrayListCompat(KEY_TOPICS, TopicEntity::class.java)
+    private var isError: Boolean = state?.getBoolean(KEY_ERROR) == true
+    private var hasMore: Boolean = state?.getBoolean(KEY_HAS_MORE) == true
+    private val showOriginal: MutableSet<Int> =
+        state?.getIntegerArrayList(KEY_SHOW_ORIGINAL)?.toMutableSet() ?: mutableSetOf()
+
+    override fun attachView(view: TopicsView) {
+        this.view = view
+
+        subscriptions += view.getStartedClicks().subscribe {
+            preferences.setIntroShown()
+            loadTopics()
+        }
+
+        subscriptions += view.retryButtonClicks().subscribe {
+            loadTopics()
+        }
+
+        subscriptions += view.refreshClicks().subscribe {
+            invalidateTopics()
+        }
+
+        subscriptions += view.pinTopicClicks().subscribe { topicId ->
+            pinTopic(topicId)
+        }
+
+        subscriptions += view.translateTopicClicks().subscribe { topicId ->
+            if (topicId in showOriginal) {
+                showOriginal.remove(topicId)
+            } else {
+                showOriginal.add(topicId)
+            }
+            bindEntities()
+        }
+
+        subscriptions += view.loginClicks().subscribe {
+            router?.openLoginScreen()
+        }
+
+        if (preferences.isShowIntro()) {
+            view.showIntro()
+        } else {
+            if (isError) {
+                onError()
+            } else {
+                entities?.let { bindEntities() } ?: loadTopics()
+            }
+        }
+    }
+
+    override fun detachView() {
+        subscriptions.clear()
+        this.view = null
+    }
+
+    override fun attachRouter(router: TopicsPresenter.TopicsRouter) {
+        this.router = router
+    }
+
+    override fun detachRouter() {
+        this.router = null
+    }
+
+    override fun saveState() = Bundle().apply {
+        putParcelableArrayList(KEY_TOPICS, entities?.let { ArrayList(entities.orEmpty()) })
+        putBoolean(KEY_ERROR, isError)
+        putBoolean(KEY_HAS_MORE, hasMore)
+        putIntegerArrayList(KEY_SHOW_ORIGINAL, ArrayList(showOriginal))
+    }
+
+    override fun scrollToTop() {
+        view?.scrollToTop()
+    }
+
+    override fun invalidate() {
+        if (!preferences.isShowIntro()) {
+            invalidateTopics()
+        }
+    }
+
+    private fun invalidateTopics() {
+        entities = null
+        isError = false
+        loadTopics()
+    }
+
+    private fun loadTopics() {
+        subscriptions += topicsInteractor.listTopics()
+            .observeOn(schedulers.mainThread())
+            .doOnSubscribe {
+                entities = null
+                if (view?.isPullRefreshing() == false) view?.showProgress()
+            }
+            .subscribe(
+                { onLoaded(it.topics, it.hasMore) },
+                { onError() }
+            )
+    }
+
+    private fun loadTopics(offset: Int) {
+        subscriptions += topicsInteractor.listTopics(offset)
+            .observeOn(schedulers.mainThread())
+            .retryWhenNonAuthErrors()
+            .subscribe(
+                { onLoaded(it.topics, it.hasMore) },
+                { onError() }
+            )
+    }
+
+    private fun onLoaded(entities: List<TopicEntity>, hasMore: Boolean) {
+        isError = false
+        this.entities = (this.entities ?: emptyList()).plus(entities)
+        this.hasMore = hasMore
+        bindEntities()
+    }
+
+    private fun bindEntities() {
+        val entities = this.entities ?: emptyList()
+
+        val items = entities
+            .map { converter.convert(it, translated = it.topicId !in showOriginal) }
+            .apply { if (isNotEmpty()) last().hasMore = hasMore }
+
+        adapterPresenter.get().onDataSourceChanged(items)
+        view?.let {
+            it.contentUpdated()
+            it.stopPullRefreshing()
+            it.showContent()
+        }
+    }
+
+    private fun onError() {
+        this.isError = true
+        view?.stopPullRefreshing()
+        view?.showError()
+    }
+
+    private fun pinTopic(topicId: Int) {
+        subscriptions += topicsInteractor.pinTopic(topicId)
+            .flatMap { topicsInteractor.listTopics() }
+            .observeOn(schedulers.mainThread())
+            .subscribe(
+                { response ->
+                    entities = response.topics
+                    hasMore = response.hasMore
+                    bindEntities()
+                },
+                { ex ->
+                    ex.filterUnauthorizedErrors(
+                        authError = { view?.showUnauthorizedError() },
+                        other = { view?.showPinFailed() }
+                    )
+                }
+            )
+    }
+
+    override fun onItemClick(item: Item) {
+        val entity = entities?.find { it.topicId.toLong() == item.id } ?: return
+        router?.showChatScreen(entity)
+    }
+
+    override fun onItemLongClick(item: Item) {
+        if (item !is TopicItem) return
+        view?.showMessageDialog(item.id.toInt(), item.isPinned, item.hasTranslation, item.translated)
+    }
+
+    override fun onLoadMore(item: Item) {
+        val offset = entities?.size ?: return
+        loadTopics(offset)
+    }
+
+}
+
+private const val KEY_TOPICS = "topics"
+private const val KEY_ERROR = "error"
+private const val KEY_HAS_MORE = "has_more"
+private const val KEY_SHOW_ORIGINAL = "show_original"
