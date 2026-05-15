0b33b7407bdd2dd35dbaca519f36d107a7f13d7e
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:27:34 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/ratings/RatingsInteractor.kt
@@ -0,0 +1,61 @@
+package com.hiaashuu.appteka.screen.ratings
+
+import com.hiaashuu.appteka.core.StoreApi
+import com.hiaashuu.appteka.screen.details.api.RatingEntity
+import com.hiaashuu.appteka.screen.ratings.api.DeleteRatingResponse
+import com.hiaashuu.appteka.user.api.UserBrief
+import com.hiaashuu.appteka.util.SchedulersFactory
+import io.reactivex.rxjava3.core.Observable
+
+data class UserBriefWrapper(
+    val userBrief: UserBrief?
+)
+
+interface RatingsInteractor {
+
+    fun listRatings(offsetRateId: Int?): Observable<List<RatingEntity>>
+
+    fun deleteRating(rateId: Int): Observable<DeleteRatingResponse>
+
+    fun getUserBrief(): Observable<UserBriefWrapper>
+
+}
+
+class RatingsInteractorImpl(
+    private val api: StoreApi,
+    private val appId: String,
+    private val schedulers: SchedulersFactory
+) : RatingsInteractor {
+
+    override fun listRatings(offsetRateId: Int?): Observable<List<RatingEntity>> {
+        return api
+            .getRatings(
+                appId = appId,
+                rateId = offsetRateId,
+                count = null,
+            )
+            .map { list ->
+                list.result.entries
+            }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun deleteRating(rateId: Int): Observable<DeleteRatingResponse> {
+        return api
+            .deleteRating(rateId)
+            .map { it.result }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+    override fun getUserBrief(): Observable<UserBriefWrapper> {
+        return api
+            .getUserBrief(userId = null)
+            .map { UserBriefWrapper(it.result) }
+            .onErrorReturn { UserBriefWrapper(userBrief = null) }
+            .toObservable()
+            .subscribeOn(schedulers.io())
+    }
+
+}
