3786a72775dcee96624dea0765feed33b8cf1380
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:30:51 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/upload/adapter/other_versions/OtherVersionsItemView.kt
@@ -0,0 +1,43 @@
+package com.hiaashuu.appteka.screen.upload.adapter.other_versions
+
+import android.view.View
+import android.widget.TextView
+import com.hiaashuu.appteka.util.adapter.BaseItemViewHolder
+import com.hiaashuu.appteka.util.adapter.ItemView
+import com.hiaashuu.appteka.R
+
+interface OtherVersionsItemView : ItemView {
+
+    fun setVersionsCount(value: Int)
+
+    fun setOnClickListener(listener: (() -> Unit)?)
+
+}
+
+@Suppress("DEPRECATION")
+class OtherVersionsItemViewHolder(view: View) : BaseItemViewHolder(view), OtherVersionsItemView {
+
+    private val resources = view.resources
+    private val otherVersionsText: TextView = view.findViewById(R.id.other_versions_text)
+    private val otherVersionsButton: View = view.findViewById(R.id.other_versions_button)
+
+    private var clickListener: (() -> Unit)? = null
+
+    init {
+        otherVersionsButton.setOnClickListener { clickListener?.invoke() }
+    }
+
+    override fun setVersionsCount(value: Int) {
+        otherVersionsText.text =
+            resources.getQuantityString(R.plurals.other_versions_text, value, value)
+    }
+
+    override fun setOnClickListener(listener: (() -> Unit)?) {
+        this.clickListener = listener
+    }
+
+    override fun onUnbind() {
+        this.clickListener = null
+    }
+
+}
