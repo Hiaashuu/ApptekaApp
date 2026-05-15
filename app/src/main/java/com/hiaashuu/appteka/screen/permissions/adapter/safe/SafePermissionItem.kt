d0a01f29f7e0785570ec1e98406c82d93b541c5e
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-15 18:24:20 +0530

    Initial commit via CodXFuse

---
+++ app/src/main/java/com/hiaashuu/appteka/screen/permissions/adapter/safe/SafePermissionItem.kt
@@ -0,0 +1,12 @@
+package com.hiaashuu.appteka.screen.permissions.adapter.safe
+
+import android.os.Parcelable
+import com.hiaashuu.appteka.util.adapter.Item
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class SafePermissionItem(
+    override val id: Long,
+    val description: String?,
+    val permission: String,
+) : Item, Parcelable
